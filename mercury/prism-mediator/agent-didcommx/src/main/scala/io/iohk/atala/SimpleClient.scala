9b04c7b82fff7287e258af67561f379e8064f2d1
Shailesh <shailesh.patil@iohk.io>
2022-08-17 17:16:36 +0100

    updated the endpoints added read client

--- mercury/prism-mediator/agent-didcommx/src/main/scala/io/iohk/atala/SimpleClient.scala
+++ mercury/prism-mediator/agent-didcommx/src/main/scala/io/iohk/atala/SimpleClient.scala
@@ -1,12 +1,13 @@
 package io.iohk.atala
 
 import zhttp.service.{ChannelFactory, Client, EventLoopGroup}
-import zio._
+import zio.*
 import zhttp.http.Method
 import zhttp.http.HttpData
 import io.iohk.atala.mercury.AgentService
 import io.iohk.atala.mercury.Agent
-
+import io.iohk.atala.mercury.model.EncryptedMessage
+import io.iohk.atala.mercury.protocol.mailbox.Mailbox.ReadMessage
 object SimpleClient extends ZIOAppDefault {
   val env = ChannelFactory.auto ++ EventLoopGroup.auto()
 
@@ -64,6 +65,44 @@ object SimpleClient extends ZIOAppDefault {
     AgentService.alice,
     // AgentService.bob,
     // AgentService.mediator
+  )
+
+  Unsafe.unsafe { Runtime.default.unsafe.run(app).getOrThrowFiberFailure() }
+
+}
+
+@main def AgentClientBob() = {
+
+  def makeReadMessage(from: Agent, mediator: Agent) =
+    ReadMessage(from = from.id, to = mediator.id, expires_time = None)
+
+  val program = for {
+    _ <- Console.printLine("\n#### Bob Sending type Readmessages ####")
+    messageCreated <- ZIO.succeed(makeReadMessage(Agent.Bob, Agent.Mediator))
+    bob <- ZIO.service[AgentService[Agent.Bob.type]]
+
+    // ##########################################
+    encryptedMsg <- bob.packEncrypted(messageCreated.asMessage, to = Agent.Mediator.id)
+    _ <- Console.printLine("EncryptedMsg: " + encryptedMsg.string)
+    _ <- Console.printLine("Sending bytes ...")
+    base64EncodedString = encryptedMsg.base64
+    _ <- Console.printLine(base64EncodedString)
+    // HTTP
+    res <- Client.request(
+      url = "http://localhost:8080/message",
+      method = Method.POST,
+      // headers = Headers.empty,
+      content = HttpData.fromChunk(Chunk.fromArray(base64EncodedString.getBytes)),
+      // ssl = ClientSSLOptions.DefaultSSL,
+    )
+    data <- res.bodyAsString
+    _ <- Console.printLine(data)
+  } yield ()
+
+  val env = ChannelFactory.auto ++ EventLoopGroup.auto()
+  val app = program.provide(
+    env,
+    AgentService.bob,
   )
 
   Unsafe.unsafe { Runtime.default.unsafe.run(app).getOrThrowFiberFailure() }
--- mercury/prism-mediator/mediator/src/main/scala/io/iohk/atala/mercury/mediator/Endpoints.scala
+++ mercury/prism-mediator/mediator/src/main/scala/io/iohk/atala/mercury/mediator/Endpoints.scala
@@ -15,6 +15,8 @@ import io.iohk.atala.mercury.mediator.*
 import io.iohk.atala.mercury.model.DidId
 import io.iohk.atala.mercury.{Agent, AgentService, DidComm}
 import io.iohk.atala.mercury.protocol.invitation.*
+import io.iohk.atala.mercury.protocol.mailbox.Mailbox.`type`
+import io.iohk.atala.mercury.protocol.routing.ForwardMessage
 
 import scala.jdk.CollectionConverters.*
 import java.util.Base64
@@ -22,13 +24,19 @@ import java.util.Base64
 
 object Endpoints {
 
+<<<<<<< Updated upstream
+=======
+  type DID
+  // val messages = mutable.Map[ConnectionId, List[String]]()
+>>>>>>> Stashed changes
   val messages = mutable.Map[DidId, List[String]]()
 
   val retrieveMessages: PublicEndpoint[String, Unit, List[String], Any] = endpoint.get
     .tag("mediator")
     .summary("Retrieve Messages")
     .in("messages")
-    .in(path[String]("connectionId"))
+    .in(header[String]("did"))
+    .description("did in the header")
     .out(jsonBody[List[String]])
 
   val retrieveMessagesServerEndpoint: ZServerEndpoint[Any, Any] =
@@ -44,6 +52,7 @@ object Endpoints {
   val registerMediatorServerEndpoint: ZServerEndpoint[Any, Any] =
     registerMediator.serverLogicSuccess(_ => ZIO.succeed(()))
 
+<<<<<<< Updated upstream
   val sendMessageServerEndpoint: ZServerEndpoint[DidComm, Any] = {
     val sendMessage: PublicEndpoint[String, ErrorInfo, Unit, Any] =
       endpoint.post
@@ -53,6 +62,47 @@ object Endpoints {
         .in(stringBody)
         .errorOut(httpErrors)
     sendMessage.serverLogicSuccess { (base64EncodedString: String) => MediatorProgram.program(base64EncodedString) }
+=======
+  val sendMessage: PublicEndpoint[String, ErrorInfo, String, Any] =
+    endpoint.post
+      .tag("mediator")
+      .summary("Mediator service endpoint for sending message")
+      .in("message")
+      .in(stringBody)
+      .out(jsonBody[String])
+      .errorOut(httpErrors)
+
+  val sendMessageServerEndpoint: ZServerEndpoint[DidComm, Any] = {
+    sendMessage.serverLogicSuccess { (base64EncodedString: String) =>
+      for {
+        _ <- Console.printLine(s"received message ...\n $base64EncodedString \n")
+        msgInMediator <- DidComm.unpackBase64(base64EncodedString)
+        _ <- Console.printLine("msgInMediator: ")
+        _ <- Console.printLine(msgInMediator.getMessage)
+        result <- ZIO.succeed(messageProcessing(msgInMediator.getMessage))
+
+      } yield (result)
+
+    }
+  }
+
+  def messageProcessing(message: org.didcommx.didcomm.message.Message): String = {
+    val typeOfMessage = message.getType
+    val recipient = message.getTo.asScala.toList.head
+    val did = DidId(recipient)
+    typeOfMessage match {
+      case "https://didcomm.org/routing/2.0/forward" => {
+        val msg = message.getAttachments().get(0).getData().toJSONObject().get("json").toString()
+        val msgList: List[String] =
+          messages.getOrElse(did, List.empty[String]) :+ msg
+        messages += (did -> msgList)
+        "Message Forwarded"
+      }
+      case "https://atalaprism.io/mercury/mailbox/1.0/ReadMessages" =>
+        messages.getOrElse(did, List.empty[String]).toString()
+      case _ => "Unknown Message Type"
+    }
+>>>>>>> Stashed changes
   }
 
   val createInvitation: PublicEndpoint[CreateInvitation, ErrorInfo, CreateInvitationResponse, Any] = {
