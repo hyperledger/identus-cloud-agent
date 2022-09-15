45a18c9a1bb3c857ffebf1878547702faab7d756
patlo-iog <108713642+patlo-iog@users.noreply.github.com>
2022-09-16 02:07:30 +0700

    [ATL-1759] feat(castor): castor services scaffolding (#24)
    * feat(castor): create empty scala project
    
    * feat(castor): bootstrap castor subprojects
    
    * feat(castor): add castor http server subproject
    
    * feat(castor): add guardrail openapi codegen
    
    * feat(castor): update gitignore
    
    * feat(castor): use OpenAPI-Generator for server codegen
    
    * feat(castor): update dependencies for openapi codegen
    
    * feat(castor): restructor openapi directory
    
    * feat(castor): add sample http-server
    
    * feat(castor): reorganize actorsystem layer
    
    * feat(castor): add template for ApiService generated file
    
    * feat(castor): add AkkaZioSupport
    
    * feat(castor): add mock resonse and adjust openapi spec
    
    * feat(castor): add worker subproject
    
    * feat(castor): add DIDApi api group
    
    * feat(castor): add DIDOperations api group
    
    * feat(castor): add EventConsumer for worker process
    
    * feat(castor): re-organize structure to account for grpc module
    
    * feat(castor): add grpc dependencies
    
    * feat(castor): remove models subproject
    
    * feat(castor): add grpc server
    
    * feat(castor): format sbt definitions
    
    * feat(castor): add grpc services
    
    * feat(castor): fix openapi spec unwanted edits
    
    * feat(castor): add local docker-compose db and migrations
    
    * feat(castor): add repository module
    
    * feat(castor): initializing transactorLayer type can fail
    
    * feat(castor): change doobie Transactor to use hikari pool

---
+++ castor/service/api-server/src/main/scala/io/iohk/atala/castor/apiserver/http/service/AkkaZioSupport.scala
@@ -0,0 +1,15 @@
+package io.iohk.atala.castor.apiserver.http.service
+
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives.*
+import zio.*
+
+trait AkkaZioSupport {
+
+  protected def onZioSuccess[A](z: UIO[A])(f: A => Route)(using runtime: Runtime[Any]): Route = {
+    Unsafe.unsafe { implicit unsafe =>
+      onSuccess(runtime.unsafe.runToFuture(z))(f)
+    }
+  }
+
+}
