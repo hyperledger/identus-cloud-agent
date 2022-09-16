6580156fde797d35cc1bc11e16237ff7db3b5e68
Pat Losoponkul <pat.losoponkul@iohk.io>
2022-09-17 02:44:50 +0700

    feat(castor): customize sttp template to work in scala3

--- castor/client/scala/project/Dependencies.scala
+++ castor/client/scala/project/Dependencies.scala
@@ -4,17 +4,23 @@ object Dependencies {
 
   object Versions {
     val sttp = "3.7.6"
+    val circe = "0.14.3"
   }
 
   private lazy val scalaPbProto = "com.thesamet.scalapb" %% "scalapb-runtime" % scalapb.compiler.Version.scalapbVersion % "protobuf"
   private lazy val scalaPbGrpc = "com.thesamet.scalapb" %% "scalapb-runtime-grpc" % scalapb.compiler.Version.scalapbVersion
 
+  private lazy val circe = "io.circe" %% "circe-core" % Versions.circe
+  private lazy val circeGeneric = "io.circe" %% "circe-generic" % Versions.circe
+  private lazy val circeParser = "io.circe" %% "circe-parser" % Versions.circe
+
   private lazy val sttpCore = "com.softwaremill.sttp.client3" %% "core" % Versions.sttp
   private lazy val sttpCirce = "com.softwaremill.sttp.client3" %% "circe" % Versions.sttp
 
+  private lazy val circeDependencies: Seq[ModuleID] = Seq(circe, circeGeneric, circeParser)
   private lazy val httpDependencies: Seq[ModuleID] = Seq(sttpCore, sttpCirce)
   private lazy val grpcDependencies: Seq[ModuleID] = Seq(scalaPbProto, scalaPbGrpc)
 
-  lazy val clientDependencies: Seq[ModuleID] = httpDependencies ++ grpcDependencies
+  lazy val clientDependencies: Seq[ModuleID] = circeDependencies ++ httpDependencies ++ grpcDependencies
 
 }
