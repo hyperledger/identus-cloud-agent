b56e9cfb7de57c518f6b3e96e51b7bef75d7ff8f
Bassam Riman <bassam.riman@iohk.io>
2022-09-28 14:27:05 -0400

    [ATL-1342] feat(pollux): Added JSON Schema

--- pollux/vc-jwt/src/main/scala/io/iohk/atala/pollux/vc/jwt/JWTDemo.scala
+++ pollux/vc-jwt/src/main/scala/io/iohk/atala/pollux/vc/jwt/JWTDemo.scala
@@ -6,6 +6,12 @@ import pdi.jwt.{JwtAlgorithm, JwtCirce}
 import java.security.*
 import java.security.spec.*
 import java.time.Instant
+import net.reactivecore.cjs.Loader
+
+import net.reactivecore.cjs.{DocumentValidator, Loader, Result}
+import net.reactivecore.cjs.resolver.Downloader
+import cats.implicits._
+import io.circe.Json
 
 @main def jwtDemo(): Unit =
   val keyGen = KeyPairGenerator.getInstance("EC")
