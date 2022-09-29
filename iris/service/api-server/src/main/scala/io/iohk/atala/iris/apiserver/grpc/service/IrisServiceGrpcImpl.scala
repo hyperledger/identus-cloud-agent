e50918ebc9ac70744bf153460c06f6f56925b5b5
Ilya Peresadin <ilya.peresadin@iohk.io>
2022-09-29 11:01:28 +0300

    [ATL-1774] fix(iris): Rearrange folder structure a little bit, add AtalaObject proto

--- iris/service/api-server/src/main/scala/io/iohk/atala/iris/apiserver/grpc/service/IrisServiceGrpcImpl.scala
+++ iris/service/api-server/src/main/scala/io/iohk/atala/iris/apiserver/grpc/service/IrisServiceGrpcImpl.scala
@@ -3,8 +3,9 @@ package io.iohk.atala.iris.apiserver.grpc.service
 import com.google.protobuf.ByteString
 import io.iohk.atala.iris.core.service.PublishingService
 import io.iohk.atala.iris.core.worker.PublishingScheduler
+import io.iohk.atala.iris.proto.did_operations.{CreateDid, DocumentDefinition}
+import io.iohk.atala.iris.proto.dlt.IrisOperation
 import io.iohk.atala.iris.proto.service.*
-import io.iohk.atala.iris.proto.did_operations.{CreateDid, DocumentDefinition};
 import zio.*
 
 import scala.concurrent.Future
