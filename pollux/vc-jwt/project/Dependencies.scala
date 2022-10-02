e3ee7abdc364388cb996fcfc00d3c794b89f6ee6
Bassam <84336050+CryptoKnightIOG@users.noreply.github.com>
2022-10-02 19:15:56 -0400

    [ATL-1342] feat(pollux): Added JSON Schema (#44)

--- pollux/vc-jwt/project/Dependencies.scala
+++ pollux/vc-jwt/project/Dependencies.scala
@@ -6,17 +6,23 @@ object Dependencies {
     val jwtCirceVersion = "9.1.1"
   }
 
-  private lazy val coreJwtCirce = "io.circe" %% "circe-core" % Versions.circeVersion
-  private lazy val genericJwtCirce = "io.circe" %% "circe-generic" % Versions.circeVersion
-  private lazy val parserJwtCirce = "io.circe" %% "circe-parser" % Versions.circeVersion
+  private lazy val coreJwtCirce = ("io.circe" %% "circe-core" % Versions.circeVersion).cross(CrossVersion.for3Use2_13)
+  private lazy val genericJwtCirce =
+    ("io.circe" %% "circe-generic" % Versions.circeVersion).cross(CrossVersion.for3Use2_13)
+  private lazy val parserJwtCirce =
+    ("io.circe" %% "circe-parser" % Versions.circeVersion).cross(CrossVersion.for3Use2_13)
 
-  private lazy val jwtCirce = "com.github.jwt-scala" %% "jwt-circe" % Versions.jwtCirceVersion
+  private lazy val circeJsonSchema =
+    ("net.reactivecore" %% "circe-json-schema" % "0.3.0").cross(CrossVersion.for3Use2_13)
+
+  private lazy val jwtCirce =
+    ("com.github.jwt-scala" %% "jwt-circe" % Versions.jwtCirceVersion).cross(CrossVersion.for3Use2_13)
 
   private lazy val test = "org.scalameta" %% "munit" % "0.7.29" % Test
 
   // Dependency Modules
   private lazy val circeDependencies: Seq[ModuleID] = Seq(coreJwtCirce, genericJwtCirce, parserJwtCirce)
-  private lazy val baseDependencies: Seq[ModuleID] = circeDependencies :+ jwtCirce :+ test
+  private lazy val baseDependencies: Seq[ModuleID] = circeDependencies :+ jwtCirce :+ circeJsonSchema :+ test
 
   // Project Dependencies
   lazy val polluxVcJwtDependencies: Seq[ModuleID] = baseDependencies
