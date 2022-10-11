04818f1c7f2faf8abcb727fdbf64e325b8788737
Pat Losoponkul <pat.losoponkul@iohk.io>
2022-10-11 23:34:48 +0700

    chore: pr diff cleanup

--- castor/lib/core/src/main/scala/io/iohk/atala/castor/core/model/did/EllipticCurve.scala
+++ castor/lib/core/src/main/scala/io/iohk/atala/castor/core/model/did/EllipticCurve.scala
@@ -1,5 +1,8 @@
 package io.iohk.atala.castor.core.model.did
 
+// EC Name is used in JWK https://w3c-ccg.github.io/security-vocab/#publicKeyJwk
+// It MUST match the curve name in https://www.iana.org/assignments/jose/jose.xhtml
+// in the "JSON Web Key Elliptic Curve" section
 enum EllipticCurve(val name: String) {
   case SECP256K1 extends EllipticCurve("secp256k1")
 }
