3b9aee79151c93e46bc46bf78d14d18ad661a4e8
FabioPinheiro <fabiomgpinheiro@gmail.com>
2022-10-25 16:30:45 +0100

    Imp UnpackMessageImp

--- mercury/mercury-library/agent-didcommx/src/main/scala/io/iohk/atala/mercury/model/package.scala
+++ mercury/mercury-library/agent-didcommx/src/main/scala/io/iohk/atala/mercury/model/package.scala
@@ -1,5 +1,7 @@
 package io.iohk.atala.mercury.model
 
+import scala.jdk.CollectionConverters.*
+
 import org.didcommx.didcomm.model._
 
 final case class EncryptedMessageImp(private val msg: PackEncryptedResult) extends EncryptedMessage {
@@ -11,9 +13,20 @@ final case class SignedMessageImp(private val msg: PackSignedResult) extends Sig
 }
 
 final case class UnpackMessageImp(private val msg: UnpackResult) extends UnpackMessage {
-  def message: Message = {
+  def message: Message = { // FIXME TODO
     val aux = msg.getMessage
-    aux
-    ???
+    Message(
+      piuri = aux.getType(),
+      from = Some(DidId(aux.getFrom())), // FIXME some ... and none
+      to = aux.getTo().asScala.toSeq.map(e => DidId(e)).headOption, // FIXME to need to be a Seq
+      body = Map.empty, // FIXME TODO
+      id = aux.getId(),
+      createdTime = aux.getCreatedTime(),
+      expiresTimePlus = aux.getExpiresTime(),
+      attachments = Seq.empty, // FIXME aux.getAttachments(),
+      thid = Option(aux.getThid()).filter(!_.isEmpty()),
+      pthid = Option(aux.getPthid()).filter(!_.isEmpty()),
+      ack = Option(aux.getAck()).toSeq.filter(!_.isEmpty()),
+    )
   }
 }
