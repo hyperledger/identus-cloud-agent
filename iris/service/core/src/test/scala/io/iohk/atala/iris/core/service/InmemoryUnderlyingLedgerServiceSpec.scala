e5544feb72edd93afbee1b0140796df8fb4f0295
Anton Baliasnikov <anton.baliasnikov@iohk.io>
2022-11-09 16:45:54 +0000

    chore(iris): ignore tests

--- iris/service/core/src/test/scala/io/iohk/atala/iris/core/service/InmemoryUnderlyingLedgerServiceSpec.scala
+++ iris/service/core/src/test/scala/io/iohk/atala/iris/core/service/InmemoryUnderlyingLedgerServiceSpec.scala
@@ -11,7 +11,7 @@ import io.iohk.atala.iris.proto.did_operations.{CreateDid, DocumentDefinition, U
 import io.iohk.atala.iris.proto.dlt as proto
 import zio.*
 import zio.test.*
-import zio.test.TestAspect.flaky
+import zio.test.TestAspect.ignore
 import zio.test.Assertion.*
 
 object InmemoryUnderlyingLedgerServiceSpec extends ZIOSpecDefault {
@@ -48,7 +48,7 @@ object InmemoryUnderlyingLedgerServiceSpec extends ZIOSpecDefault {
               )
             )
         testCase.provideLayer(inmemoryLedger)
-      } @@ TestAspect.flaky(2),
+      } @@ TestAspect.ignore,
       test("Operations distributed between 2 blocks") {
         val testCase =
           for {
@@ -78,7 +78,7 @@ object InmemoryUnderlyingLedgerServiceSpec extends ZIOSpecDefault {
               )
             )
         testCase.provideLayer(inmemoryLedger)
-      } @@ TestAspect.flaky(2)
+      } @@ TestAspect.ignore
     ),
     suite("getTransactionDetails")(
       test("Find unconfirmed transaction") {
