30570b5d6262078b9de26d3e6c23bc00945d89b6
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2022-11-21 16:08:57 +0100

    chore(prism-agent): use release pollux lib 0.3.0

--- prism-agent/service/project/Dependencies.scala
+++ prism-agent/service/project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val akka = "2.6.20"
     val akkaHttp = "10.2.9"
     val castor = "0.2.0"
-    val pollux = "0.3.0-SNAPSHOT" // FIXME
+    val pollux = "0.3.0"
     val bouncyCastle = "1.70"
     val logback = "1.4.4"
     val mercury = "0.6.0"
