78b807bdc1a61ac7d271d8367e3e023674c6da36
Pat Losoponkul <pat.losoponkul@iohk.io>
2022-11-23 18:53:37 +0700

    feat(castor): add service to domain model and implement validation

--- castor/lib/core/src/main/scala/io/iohk/atala/castor/core/util/DIDOperationValidator.scala
+++ castor/lib/core/src/main/scala/io/iohk/atala/castor/core/util/DIDOperationValidator.scala
@@ -80,7 +80,7 @@ class DIDOperationValidator(config: Config) {
 
   private def validateKeyIdRegex(operation: PrismDIDOperation): Either[DIDOperationError, Unit] = {
     operation match {
-      case PrismDIDOperation.Create(publicKeys, internalKeys) =>
+      case PrismDIDOperation.Create(publicKeys, internalKeys, _) =>
         val ids = publicKeys.map(_.id) ++ internalKeys.map(_.id)
         val invalidIds = ids.filterNot(id => KEY_ID_RE.pattern.matcher(id).matches())
         if (invalidIds.isEmpty) Right(())
