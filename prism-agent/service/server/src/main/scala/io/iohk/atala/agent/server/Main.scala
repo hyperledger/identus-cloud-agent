bb716500010458c258558d8a71274798de75c03e
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2022-11-23 10:59:18 +0100

    chore(prism-agent): execute connect db migrations at startup

--- prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/Main.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/Main.scala
@@ -6,6 +6,7 @@ import org.didcommx.didcomm.DIDComm
 import io.iohk.atala.resolvers.UniversalDidResolver
 import io.iohk.atala.castor.sql.repository.{Migrations => CastorMigrations}
 import io.iohk.atala.pollux.sql.repository.{Migrations => PolluxMigrations}
+import io.iohk.atala.connect.sql.repository.{Migrations => ConnectMigrations}
 
 object Main extends ZIOAppDefault {
   def agentLayer(peer: PeerDID): ZLayer[Any, Nothing, AgentServiceAny] = ZLayer.succeed(
@@ -54,6 +55,9 @@ object Main extends ZIOAppDefault {
       _ <- ZIO
         .serviceWithZIO[PolluxMigrations](_.migrate)
         .provide(RepoModule.polluxDbConfigLayer >>> PolluxMigrations.layer)
+      _ <- ZIO
+        .serviceWithZIO[ConnectMigrations](_.migrate)
+        .provide(RepoModule.connectDbConfigLayer >>> ConnectMigrations.layer)
 
       agentDID <- for {
         peer <- ZIO.succeed(PeerDID.makePeerDid(serviceEndpoint = Some(s"http://localhost:$didCommServicePort")))
