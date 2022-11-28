56cc1c38caa313846c61253527accb456ccc476a
Pat Losoponkul <pat.losoponkul@iohk.io>
2022-11-28 11:57:36 +0700

    feat(connect): bump up shared version to use 0.2.0

--- connect/lib/project/Dependencies.scala
+++ connect/lib/project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val iris = "0.1.0"
     val mercury = "0.7.0"
     val flyway = "9.7.0"
-    val shared = "0.1.0"
+    val shared = "0.2.0"
   }
 
   private lazy val zio = "dev.zio" %% "zio" % Versions.zio
