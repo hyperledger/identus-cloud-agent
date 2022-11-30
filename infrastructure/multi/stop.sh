7ee59bcf1058e6dea79186593b458d81981aba6b
David <david.poltorak@iohk.io>
2022-11-30 16:55:33 +0000

    feat(infra): add helper script to run I/H/V in one command

---
+++ infrastructure/multi/stop.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Set working directory
+cd ${SCRIPT_DIR}
+
+Help()
+{
+   # Display Help
+   echo "Run an instance of the ATALA `bulding-block` stack locally"
+   echo
+   echo "Syntax: run.sh [-n/--name NAME|-p/--port PORT|-h/--help]"
+   echo "options:"
+   echo "-n/--name          Name of this instance - defaults to dev."
+   echo "-p/--port          Port to run this instance on - defaults to 80."
+   echo "-h/--help          Print this help text."
+   echo
+}
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -n|--name)
+      NAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -p|--port)
+      PORT="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -h|--help)
+      Help
+      exit
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      Help
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 "$1"
+fi
+
+if [ -z ${NAME+x} ];
+then
+    NAME="dev"
+fi
+
+if [ -z ${PORT+x} ];
+then
+    PORT="80"
+fi
+
+if [ -z ${BACKGROUND+x} ];
+then
+    BACKGROUND=""
+fi
+
+echo "NAME            = ${NAME}"
+echo "PORT            = ${PORT}"
+
+echo "--------------------------------------"
+echo "Stopping stack using docker-compose"
+echo "--------------------------------------"
+
+../local/stop.sh -n issuer
+../local/stop.sh -n holder
+../local/stop.sh -n verifier
