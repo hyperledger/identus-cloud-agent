c8c501e921ce75f5e0afdb656eaf4e9385e9cd81
FabioPinheiro <fabiomgpinheiro@gmail.com>
2022-11-30 15:24:44 +0000

    feat(mercury): Update protocol-present-proof to v3 and add our extensions

---
+++ mercury/mercury-library/protocol-present-proof/src/main/scala/io/iohk/atala/mercury/protocol/presentproof/ProofType.scala
@@ -0,0 +1,16 @@
+package io.iohk.atala.mercury.protocol.presentproof
+
+import io.circe._
+import io.circe.generic.semiauto._
+import io.circe.syntax._
+import io.iohk.atala.mercury.model.DidId
+
+case class ProofType(
+    schema: String, // Schema ID EX: https://schema.org/Person
+    requiredFields: Option[Seq[String]], // ["email"]
+    trustIssuers: Option[Seq[DidId]] // ["did:prism:123321123"]
+)
+object ProofType {
+  given Encoder[ProofType] = deriveEncoder[ProofType]
+  given Decoder[ProofType] = deriveDecoder[ProofType]
+}
