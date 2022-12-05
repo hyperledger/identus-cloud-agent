d98be3cf7b6fb769ec115c2ef750cd6e50921a68
Anton Baliasnikov <anton.baliasnikov@iohk.io>
2022-12-05 10:43:06 +0000

    test: test

--- tests/e2e-tests/src/test/kotlin/runners/E2eTestsRunner.kt
+++ tests/e2e-tests/src/test/kotlin/runners/E2eTestsRunner.kt
@@ -4,7 +4,6 @@ import io.cucumber.junit.CucumberOptions
 import net.serenitybdd.cucumber.CucumberWithSerenity
 import org.junit.runner.RunWith
 
-@RunWith(CucumberWithSerenity::class)
 @CucumberOptions(
     features = [
         "src/test/resources/features"
@@ -16,4 +15,5 @@ import org.junit.runner.RunWith
         "json:target/serenity-reports/cucumber_report.json"
     ],
 )
+@RunWith(CucumberWithSerenity::class)
 class E2eTestsRunner
