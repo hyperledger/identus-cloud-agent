e87c3e0ffd4233c6ce27b481cba792cb9602d3f2
Anton Baliasnikov <anton.baliasnikov@iohk.io>
2022-12-06 11:11:49 +0000

    test: test

--- infrastructure/local/update_env.sh
+++ infrastructure/local/update_env.sh
@@ -3,18 +3,12 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ENV_FILE="${SCRIPT_DIR}/.env"
 
-pip install ${SCRIPT_DIR}/../utils/python/github-helpers
+pip install ${SCRIPT_DIR}/../utils/python/github-helpers > /dev/null 2>&1
 
 MERCURY_MEDIATOR_VERSION=$(github get-latest-package-version --package mercury-mediator --package-type container)
 IRIS_SERVICE_VERSION=$(github get-latest-package-version --package iris-service --package-type container)
 PRISM_AGENT_VERSION=$(github get-latest-package-version --package prism-agent --package-type container)
 
-ls -la ${ENV_FILE}
-
-echo MERCURY_MEDIATOR_VERSION=${MERCURY_MEDIATOR_VERSION}
-echo IRIS_SERVICE_VERSION=${IRIS_SERVICE_VERSION}
-echo PRISM_AGENT_VERSION=${PRISM_AGENT_VERSION}
-
-sed -i '' -e "s/MERCURY_MEDIATOR_VERSION=.*/MERCURY_MEDIATOR_VERSION=${MERCURY_MEDIATOR_VERSION}/" ${ENV_FILE}
-sed -i '' -e "s/IRIS_SERVICE_VERSION=.*/IRIS_SERVICE_VERSION=${IRIS_SERVICE_VERSION}/" ${ENV_FILE}
-sed -i '' -e "s/PRISM_AGENT_VERSION=.*/PRISM_AGENT_VERSION=${PRISM_AGENT_VERSION}/" ${ENV_FILE}
+sed -i.bak "s/MERCURY_MEDIATOR_VERSION=.*/MERCURY_MEDIATOR_VERSION=${MERCURY_MEDIATOR_VERSION}/" ${ENV_FILE} && rm -f ${ENV_FILE}.bak
+sed -i.bak "s/IRIS_SERVICE_VERSION=.*/IRIS_SERVICE_VERSION=${IRIS_SERVICE_VERSION}/" ${ENV_FILE} && rm -f ${ENV_FILE}.bak
+sed -i.bak "s/PRISM_AGENT_VERSION=.*/PRISM_AGENT_VERSION=${PRISM_AGENT_VERSION}/" ${ENV_FILE} && rm -f ${ENV_FILE}.bak
