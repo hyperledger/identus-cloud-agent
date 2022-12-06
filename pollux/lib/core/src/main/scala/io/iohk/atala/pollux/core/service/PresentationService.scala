47f4c7451dfe0cc32afdaf6966c17e4ddca593a4
FabioPinheiro <fabiomgpinheiro@gmail.com>
2022-12-06 14:56:52 +0000

    Add field maybeIssuanceDate in W3cPresentationPayload

--- pollux/lib/core/src/main/scala/io/iohk/atala/pollux/core/service/PresentationService.scala
+++ pollux/lib/core/src/main/scala/io/iohk/atala/pollux/core/service/PresentationService.scala
@@ -221,6 +221,7 @@ private class PresentationServiceImpl(
         verifiableCredential = verifiableCredentials,
         holder = prover.did.value,
         verifier = Vector("https://example.edu/issuers/565049"), // TODO Fix this
+        maybeIssuanceDate = None,
         maybeExpirationDate = Some(Instant.parse("2010-01-12T00:00:00Z"))
       )
 
