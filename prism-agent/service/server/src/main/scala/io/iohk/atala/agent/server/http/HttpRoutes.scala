92fd76b5bfd8672f6bf92a6b23b375376ac1d42a
Pat Losoponkul <pat.losoponkul@iohk.io>
2022-12-07 13:07:22 +0700

    feat(prism-agent): deprecate unused DID related endpoints

--- prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/HttpRoutes.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/HttpRoutes.scala
@@ -5,7 +5,6 @@ import akka.http.scaladsl.server.Directives.*
 import io.iohk.atala.agent.openapi.api.{
   DIDApi,
   DIDAuthenticationApi,
-  DIDOperationsApi,
   DIDRegistrarApi,
   IssueCredentialsProtocolApi,
   ConnectionsManagementApi
@@ -16,18 +15,16 @@ import akka.http.scaladsl.server.Route
 object HttpRoutes {
 
   def routes: URIO[
-    DIDApi & DIDOperationsApi & DIDAuthenticationApi & DIDRegistrarApi & IssueCredentialsProtocolApi &
-      ConnectionsManagementApi,
+    DIDApi & DIDAuthenticationApi & DIDRegistrarApi & IssueCredentialsProtocolApi & ConnectionsManagementApi,
     Route
   ] =
     for {
       didApi <- ZIO.service[DIDApi]
-      didOperationsApi <- ZIO.service[DIDOperationsApi]
       didAuthApi <- ZIO.service[DIDAuthenticationApi]
       disRegistrarApi <- ZIO.service[DIDRegistrarApi]
       issueCredentialsProtocolApi <- ZIO.service[IssueCredentialsProtocolApi]
       connectionsManagementApi <- ZIO.service[ConnectionsManagementApi]
-    } yield didApi.route ~ didOperationsApi.route ~ didAuthApi.route ~ disRegistrarApi.route ~ issueCredentialsProtocolApi.route ~ connectionsManagementApi.route ~ additionalRoute
+    } yield didApi.route ~ didAuthApi.route ~ disRegistrarApi.route ~ issueCredentialsProtocolApi.route ~ connectionsManagementApi.route ~ additionalRoute
 
   private def additionalRoute: Route = {
     // swagger-ui expects this particular header when resolving relative $ref
--- prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/service/DIDApiServiceImpl.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/service/DIDApiServiceImpl.scala
@@ -13,64 +13,11 @@ import io.iohk.atala.agent.server.http.model.{HttpServiceError, OASDomainModelHe
 import io.iohk.atala.castor.core.model.error.DIDOperationError
 import zio.*
 
-// TODO: replace with actual implementation
 class DIDApiServiceImpl(service: DIDService)(using runtime: Runtime[Any])
     extends DIDApiService,
       AkkaZioSupport,
       OASDomainModelHelper,
       OASErrorModelHelper {
-
-  private val mockDID = DID(
-    id = "did:prism:1:mainnet:abcdef123456",
-    controller = None,
-    verificationMethod = None,
-    authentication = Some(
-      Seq(
-        VerificationMethod(
-          id = "did:prism:1:mainnet:abcdef123456#key-1",
-          `type` = "JsonWebKey2020",
-          controller = "did:prism:1:mainnet:abcdef123456",
-          publicKeyJwk = PublicKeyJwk(
-            crv = Some("P-256"),
-            x = Some("38M1FDts7Oea7urmseiugGW7tWc3mLpJh6rKe7xINZ8"),
-            y = Some("nDQW6XZ7b_u2Sy9slofYLlG03sOEoug3I0aAPQ0exs4"),
-            kty = "EC",
-            kid = Some("_TKzHv2jFIyvdTGF1Dsgwngfdg3SH6TpDv0Ta1aOEkw")
-          )
-        )
-      )
-    ),
-    assertionMethod = None,
-    keyAgreement = None,
-    capabilityInvocation = None,
-    service = None
-  )
-
-  private val mockDIDResponse = DIDResponse(
-    did = mockDID,
-    metadata = DIDDocumentMetadata(deactivated = false)
-  )
-
-  private val mockDIDOperationResponse = DIDOperationResponse(
-    scheduledOperation = DidOperationSubmission(
-      id = "0123456789abcdef",
-      didRef = "did:example:123"
-    )
-  )
-
-  override def createDid(createDIDRequest: CreateDIDRequest)(implicit
-      toEntityMarshallerDIDOperationResponse: ToEntityMarshaller[DIDOperationResponse],
-      toEntityMarshallerErrorResponse: ToEntityMarshaller[ErrorResponse]
-  ): Route = {
-    onZioSuccess(ZIO.unit) { _ => createDid202(mockDIDOperationResponse) }
-  }
-
-  override def deactivateDID(didRef: String, deactivateDIDRequest: DeactivateDIDRequest)(implicit
-      toEntityMarshallerDIDOperationResponse: ToEntityMarshaller[DIDOperationResponse],
-      toEntityMarshallerErrorResponse: ToEntityMarshaller[ErrorResponse]
-  ): Route = {
-    onZioSuccess(ZIO.unit) { _ => deactivateDID202(mockDIDOperationResponse) }
-  }
 
   override def getDid(didRef: String)(implicit
       toEntityMarshallerDIDResponse: ToEntityMarshaller[DIDResponse],
@@ -81,20 +28,6 @@ class DIDApiServiceImpl(service: DIDService)(using runtime: Runtime[Any])
       case Left(error)     => complete(error.status -> error)
       case Right(response) => getDid200(response)
     }
-  }
-
-  override def recoverDid(didRef: String, recoverDIDRequest: RecoverDIDRequest)(implicit
-      toEntityMarshallerDIDOperationResponse: ToEntityMarshaller[DIDOperationResponse],
-      toEntityMarshallerErrorResponse: ToEntityMarshaller[ErrorResponse]
-  ): Route = {
-    onZioSuccess(ZIO.unit) { _ => recoverDid202(mockDIDOperationResponse) }
-  }
-
-  override def updateDid(didRef: String, updateDIDRequest: UpdateDIDRequest)(implicit
-      toEntityMarshallerDIDOperationResponse: ToEntityMarshaller[DIDOperationResponse],
-      toEntityMarshallerErrorResponse: ToEntityMarshaller[ErrorResponse]
-  ): Route = {
-    onZioSuccess(ZIO.unit) { _ => updateDid202(mockDIDOperationResponse) }
   }
 
 }
