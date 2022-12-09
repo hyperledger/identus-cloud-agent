6b362c512bed9d23066fb4a5c841ef3f91b0e43a
yshyn-iohk <yurii.shynbuiev@iohk.io>
2022-12-09 13:47:40 +0700

    fix(connect): bump mercury version to 1.10.1

--- connect/lib/project/Dependencies.scala
+++ connect/lib/project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val doobie = "1.0.0-RC2"
     val zioCatsInterop = "3.3.0"
     val iris = "0.1.0"
-    val mercury = "0.10.0"
+    val mercury = "0.10.1"
     val flyway = "9.8.3"
     val shared = "0.2.0"
   }
