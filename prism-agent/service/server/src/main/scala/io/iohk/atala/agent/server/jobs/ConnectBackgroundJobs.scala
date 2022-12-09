b419140524d88ca6240988e1d03a28c0dff4af73
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2022-12-09 11:43:40 +0100

    feat(prism-agent): send DIDComm Connect messages using scoped PeerDID loaded from storage

--- prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/jobs/ConnectBackgroundJobs.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/jobs/ConnectBackgroundJobs.scala
@@ -13,6 +13,13 @@ import io.iohk.atala.mercury.model.error._
 import io.iohk.atala.mercury.protocol.issuecredential._
 import java.io.IOException
 import io.iohk.atala.connect.core.model.error.ConnectionServiceError
+import io.iohk.atala.mercury.AgentServiceAny
+import org.didcommx.didcomm.DIDComm
+import io.iohk.atala.mercury.PeerDID
+import com.nimbusds.jose.jwk.OctetKeyPair
+import io.iohk.atala.resolvers.UniversalDidResolver
+import io.iohk.atala.agent.walletapi.service.ManagedDIDService
+import io.iohk.atala.agent.walletapi.model.error.DIDSecretStorageError
 
 object ConnectBackgroundJobs {
 
@@ -28,25 +35,47 @@ object ConnectBackgroundJobs {
 
   private[this] def performExchange(
       record: ConnectionRecord
-  ): URIO[DidComm & ConnectionService, Unit] = {
+  ): URIO[ConnectionService & ManagedDIDService, Unit] = {
     import Role._
     import ProtocolState._
     val exchange = record match {
       case ConnectionRecord(id, _, _, _, _, Invitee, ConnectionRequestPending, _, Some(request), _) =>
         (for {
-          didComm <- ZIO.service[DidComm]
-          _ <- sendMessage(request.makeMessage)
+          managedDidService <- ZIO.service[ManagedDIDService]
+          peerDID <- managedDidService.getPeerDID(request.from)
+          didCommAgent = ZLayer.succeed(
+            AgentServiceAny(
+              new DIDComm(UniversalDidResolver, peerDID.getSecretResolverInMemory),
+              peerDID.did
+            )
+          )
+          _ <- sendMessage(request.makeMessage).provide(didCommAgent)
           connectionService <- ZIO.service[ConnectionService]
           _ <- connectionService.markConnectionRequestSent(id)
-        } yield ()): ZIO[DidComm & ConnectionService, ConnectionServiceError | MercuryException, Unit]
+        } yield ()): ZIO[
+          ConnectionService & ManagedDIDService,
+          ConnectionServiceError | MercuryException | DIDSecretStorageError,
+          Unit
+        ]
 
       case ConnectionRecord(id, _, _, _, _, Inviter, ConnectionResponsePending, _, _, Some(response)) =>
         (for {
-          didComm <- ZIO.service[DidComm]
-          _ <- sendMessage(response.makeMessage)
+          managedDidService <- ZIO.service[ManagedDIDService]
+          peerDID <- managedDidService.getPeerDID(response.from)
+          didCommAgent = ZLayer.succeed(
+            AgentServiceAny(
+              new DIDComm(UniversalDidResolver, peerDID.getSecretResolverInMemory),
+              peerDID.did
+            )
+          )
+          _ <- sendMessage(response.makeMessage).provide(didCommAgent)
           connectionService <- ZIO.service[ConnectionService]
           _ <- connectionService.markConnectionResponseSent(id)
-        } yield ()): ZIO[DidComm & ConnectionService, ConnectionServiceError | MercuryException, Unit]
+        } yield ()): ZIO[
+          ConnectionService & ManagedDIDService,
+          ConnectionServiceError | MercuryException | DIDSecretStorageError,
+          Unit
+        ]
 
       case _ => ZIO.unit
     }
@@ -57,9 +86,11 @@ object ConnectBackgroundJobs {
           ZIO.logErrorCause(s"DIDComm communication error processing record: ${record.id}", Cause.fail(ex))
         case ex: ConnectionServiceError =>
           ZIO.logErrorCause(s"Connection service error processing record: ${record.id} ", Cause.fail(ex))
+        case ex: DIDSecretStorageError =>
+          ZIO.logErrorCause(s"DID secret storafe error processing record: ${record.id} ", Cause.fail(ex))
       }
       .catchAllDefect { case throwable =>
-        ZIO.logErrorCause(s"Conection protocol defect processing record: ${record.id}", Cause.fail(throwable))
+        ZIO.logErrorCause(s"Connection protocol defect processing record: ${record.id}", Cause.fail(throwable))
       }
   }
 
