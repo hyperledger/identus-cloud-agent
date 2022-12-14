4424de14917988cc6f8e406624bf7cf20db27455
bvoiturier <benjamin.voiturier@iohk.io>
2022-12-14 20:24:11 +0100

    feat(prism-agent): fix DB connection pool duplication issue by providing ZLayer globally (#256)

--- prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/MainInMemory.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/MainInMemory.scala
@@ -5,12 +5,16 @@ import io.iohk.atala.mercury.*
 import io.iohk.atala.resolvers.UniversalDidResolver
 import org.didcommx.didcomm.DIDComm
 import zio.*
+import io.iohk.atala.pollux.service.SchemaRegistryServiceInMemory
+import io.iohk.atala.pollux.service.VerificationPolicyServiceInMemory
 
 object MainInMemory extends ZIOAppDefault {
 
   override def run: ZIO[Any, Throwable, Unit] =
     for {
-      _ <- Modules.zioApp.fork
+      _ <- Modules.zioApp
+        .provide(SchemaRegistryServiceInMemory.layer, VerificationPolicyServiceInMemory.layer, SystemModule.configLayer)
+        .fork
       _ <- ZIO.never
     } yield ()
 
