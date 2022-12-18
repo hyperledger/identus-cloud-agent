25c6111dad9e6052bb8534ec2a5336f69677c16f
atala-dev <atala.dev@iohk.io>
2022-12-18 00:18:10 +0000

    Update bcpkix-jdk15on to 1.72 in main

--- prism-agent/service/project/Dependencies.scala
+++ prism-agent/service/project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val castor = "0.5.1"
     val pollux = "0.12.0"
     val connect = "0.5.0"
-    val bouncyCastle = "1.70"
+    val bouncyCastle = "1.72"
     val logback = "1.4.5"
     val mercury = "0.13.0"
     val zioJson = "0.4.2"
@@ -50,7 +50,7 @@ object Dependencies {
 
   // Added here to make prism-crypto works.
   // Once migrated to apollo, re-evaluate if this should be removed.
-  private lazy val bouncyBcpkix = "org.bouncycastle" % "bcpkix-jdk15on" % Versions.bouncyCastle
+  private lazy val bouncyBcpkix = "org.bouncycastle" % "bcpkix-jdk18on" % Versions.bouncyCastle
   private lazy val bouncyBcprov = "org.bouncycastle" % "bcprov-jdk15on" % Versions.bouncyCastle
 
   private lazy val logback = "ch.qos.logback" % "logback-classic" % Versions.logback
