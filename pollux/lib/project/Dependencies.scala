a31f379321bbf9dc43eb851365e880572cb29bde
bvoiturier <benjamin.voiturier@iohk.io>
2022-12-19 16:11:38 +0100

    feat(pollux): bump mercury version (#271)

--- pollux/lib/project/Dependencies.scala
+++ pollux/lib/project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val prismSdk = "v1.4.1" // scala-steward:off
     val iris = "0.1.0"
     val shared = "0.2.0"
-    val mercury = "0.12.0"
+    val mercury = "0.15.0"
     val flyway = "9.8.3"
   }
 
