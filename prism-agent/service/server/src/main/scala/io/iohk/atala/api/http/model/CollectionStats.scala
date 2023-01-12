3a41dcbcf78730791f313c7edcc3f9e7be932b9e
yshyn-iohk <yurii.shynbuiev@iohk.io>
2023-01-12 22:27:06 +0700

    chore(prism-agent): refactoring

--- prism-agent/service/server/src/main/scala/io/iohk/atala/api/http/model/CollectionStats.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/api/http/model/CollectionStats.scala
@@ -1,3 +1,3 @@
 package io.iohk.atala.api.http.model
 
-case class CollectionStats(totalCount: Int, filteredCount: Int)
+case class CollectionStats(totalCount: Long, filteredCount: Long)
---
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/pollux/service/JdbcSchemaRegistryService.scala
@@ -0,0 +1,108 @@
+package io.iohk.atala.pollux.service
+import doobie.util.transactor.Transactor
+import io.iohk.atala.api.http.model.{CollectionStats, Order, Pagination}
+import io.iohk.atala.pollux.schema.model.{VerifiableCredentialSchema, VerifiableCredentialSchemaInput, VerifiableCredentialSchemaPage}
+import zio.Task
+import zio.*
+import zio.interop.catz.*
+import zio.interop.catz.implicits.*
+import doobie.*
+import doobie.hikari.HikariTransactor
+import doobie.implicits.*
+import doobie.util.ExecutionContexts
+import doobie.util.transactor.Transactor
+import cats.Functor
+import cats.effect.std.Dispatcher
+import cats.effect.{Async, Resource}
+import cats.syntax.functor.*
+//import io.iohk.atala.pollux.dal.VerifiableCredentialSchema as VCS
+//import io.iohk.atala.pollux.dal.VerifiableCredentialSchema.sql
+//import io.iohk.atala.pollux.dal.VerifiableCredentialSchema.sql.*
+//import io.iohk.atala.pollux.dal.VerifiableCredentialSchema.sql.idiom.*
+import io.iohk.atala.pollux.sql.model.VerifiableCredentialSchema as VCS
+import io.iohk.atala.pollux.sql.model.VerifiableCredentialSchema.sql
+import io.iohk.atala.pollux.sql.model.VerifiableCredentialSchema.sql.*
+
+import io.getquill.*
+import io.getquill.idiom.*
+
+import java.util.UUID
+import scala.util.Try
+
+class JdbcSchemaRegistryService(tx: Transactor[Task]) extends SchemaRegistryService {
+
+  import JdbcSchemaRegistryService.given_Conversion_VerifiableCredentialSchema_VCS
+  import JdbcSchemaRegistryService.given_Conversion_VCS_VerifiableCredentialSchema
+  override def createSchema(in: VerifiableCredentialSchemaInput): Task[VerifiableCredentialSchema] = {
+    for {
+      vcs <- zio.ZIO.fromTry(Try(VerifiableCredentialSchema(in)))
+      vcs_dto: VCS = vcs.convert
+      action <- sql.insert(vcs_dto).transact(tx)
+    } yield vcs
+  }
+
+  override def getSchemaById(id: UUID): Task[Option[VerifiableCredentialSchema]] = {
+    for {
+      vcsOption <- sql.findBy(id).transact(tx).map(_.headOption)
+      result = vcsOption.map[VerifiableCredentialSchema](dto => dto.convert)
+    } yield result
+  }
+
+  override def lookupSchemas(
+      filter: VerifiableCredentialSchema.Filter,
+      pagination: Pagination,
+      order: Option[Order]
+  ): Task[(VerifiableCredentialSchemaPage, CollectionStats)] = {
+    val lookupQuery = sql.lookup(
+      authorOpt = filter.author,
+      nameOpt = filter.name,
+      versionOpt = filter.version,
+      attributeOpt = filter.attribute,
+      tagOpt = filter.tags,
+      offsetOpt = Some(pagination.offset),
+      limitOpt = Some(pagination.limit)
+    )
+    val lookupQueryCount = lookupQuery.size
+    val totalCountQuery = sql.totalCount
+
+    for {
+      dtoList <- sql.run(lookupQuery).transact(tx)
+      count <- sql.run(lookupQueryCount).transact(tx)
+      totalCount <- totalCountQuery.transact(tx)
+      vcsList = dtoList.map[VerifiableCredentialSchema](dto => dto.convert)
+    } yield (VerifiableCredentialSchemaPage(contents = vcsList), CollectionStats(totalCount, count))
+  }
+
+}
+
+object JdbcSchemaRegistryService {
+
+  val layer: URLayer[Transactor[Task], JdbcSchemaRegistryService] =
+    ZLayer.fromFunction(new JdbcSchemaRegistryService(_))
+
+  given scala.Conversion[VerifiableCredentialSchema, VCS] = vcs =>
+    VCS(
+      id = vcs.id,
+      name = vcs.name,
+      version = vcs.version,
+      tags = vcs.tags,
+      description = vcs.description,
+      attributes = vcs.attributes,
+      author = vcs.author,
+      authored = vcs.authored.toOffsetDateTime // TODO: Fix after merge
+    )
+
+  given scala.Conversion[VCS, VerifiableCredentialSchema] = vcs =>
+    VerifiableCredentialSchema(
+      id = vcs.id,
+      name = vcs.name,
+      version = vcs.version,
+      tags = vcs.tags,
+      description = vcs.description,
+      attributes = vcs.attributes,
+      author = vcs.author,
+      authored = vcs.authored.toZonedDateTime, // TODO: Fix after merge
+      proof = None
+    )
+
+}
