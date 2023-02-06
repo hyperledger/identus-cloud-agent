8ae5b848005c4038655c557af9796a013b9bd340
FabioPinheiro <fabiomgpinheiro@gmail.com>
2023-02-06 15:02:40 +0000

    fix

--- connect/lib/core/src/main/scala/io/iohk/atala/connect/core/service/ConnectionService.scala
+++ connect/lib/core/src/main/scala/io/iohk/atala/connect/core/service/ConnectionService.scala
@@ -39,6 +39,6 @@ trait ConnectionService {
 
   def deleteConnectionRecord(recordId: UUID): IO[ConnectionServiceError, Int]
 
-  def updateAfterFail(recordId: UUID, failReason: Option[String]): IO[ConnectionServiceError, Int]
+  def reportProcessingFailure(recordId: UUID, failReason: Option[String]): IO[ConnectionServiceError, Int]
 
 }
