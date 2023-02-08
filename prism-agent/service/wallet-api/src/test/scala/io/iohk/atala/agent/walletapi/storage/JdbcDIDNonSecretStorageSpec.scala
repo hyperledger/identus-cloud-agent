ce3fbf12701f43e3d27a5a70e8251c069793cf2f
Pat Losoponkul <pat.losoponkul@iohk.io>
2023-02-08 22:41:06 +0700

    test(prism-agent): add DIDLineage crud tests

--- prism-agent/service/wallet-api/src/test/scala/io/iohk/atala/agent/walletapi/storage/JdbcDIDNonSecretStorageSpec.scala
+++ prism-agent/service/wallet-api/src/test/scala/io/iohk/atala/agent/walletapi/storage/JdbcDIDNonSecretStorageSpec.scala
@@ -8,6 +8,8 @@ import zio.test.Assertion.*
 import io.iohk.atala.castor.core.model.did.PrismDID
 import io.iohk.atala.agent.walletapi.model.ManagedDIDState
 import scala.collection.compat.immutable.ArraySeq
+import io.iohk.atala.agent.walletapi.sql.JdbcDIDSecretStorage
+import io.iohk.atala.castor.core.model.did.ScheduledDIDOperationStatus
 
 object JdbcDIDNonSecretStorageSpec extends ZIOSpecDefault, StorageSpecHelper, PostgresTestContainerSupport {
 
@@ -16,13 +18,15 @@ object JdbcDIDNonSecretStorageSpec extends ZIOSpecDefault, StorageSpecHelper, Po
       suite("JdbcDIDNonSecretStorageSpec")(
         listDIDStateSpec,
         setDIDStateSpec,
-        getDIDStateSpec
+        getDIDStateSpec,
+        listDIDLineageSpec,
+        setDIDLineageStatusSpec
       ) @@ TestAspect.before(DBTestUtils.runMigrationAgentDB)
 
     testSuite.provideSomeLayer(
-      pgContainerLayer >+> transactorLayer >+> JdbcDIDNonSecretStorage.layer
+      pgContainerLayer >+> transactorLayer >+> (JdbcDIDSecretStorage.layer ++ JdbcDIDNonSecretStorage.layer)
     )
-  } @@ TestAspect.tag("dev")
+  }
 
   private val listDIDStateSpec = suite("listManagedDIDState")(
     test("initialize with empty list") {
@@ -84,6 +88,101 @@ object JdbcDIDNonSecretStorageSpec extends ZIOSpecDefault, StorageSpecHelper, Po
           } yield readState
         }
       } yield assert(readStates.flatten)(equalTo(states))
+    }
+  )
+
+  private val listDIDLineageSpec = {
+    val initDIDLineage = {
+      for {
+        storage <- ZIO.service[DIDNonSecretStorage]
+        did1 <- initializeDIDStateAndKeys(Nil).map(_._1)
+        did2 <- initializeDIDStateAndKeys(Nil).map(_._1)
+        input = Seq(
+          (did1, Array.fill[Byte](32)(0), ScheduledDIDOperationStatus.Pending),
+          (did1, Array.fill[Byte](32)(1), ScheduledDIDOperationStatus.Confirmed),
+          (did2, Array.fill[Byte](32)(2), ScheduledDIDOperationStatus.Pending),
+          (did2, Array.fill[Byte](32)(3), ScheduledDIDOperationStatus.Confirmed),
+        )
+        _ <- ZIO.foreach(input) { case (did, hash, status) =>
+          storage.insertDIDUpdateLineage(did, updateLineage(operationId = hash, status = status))
+        }
+      } yield (did1, did2)
+    }
+
+    suite("listDIDUpdateLineage")(
+      test("initialize with empty lineage") {
+        for {
+          storage <- ZIO.service[DIDNonSecretStorage]
+          lineage <- storage.listUpdateLineage(None, None)
+        } yield assert(lineage)(isEmpty)
+      },
+      test("list all lineage") {
+        for {
+          _ <- initDIDLineage
+          lineage <- ZIO.serviceWithZIO[DIDNonSecretStorage](_.listUpdateLineage(None, None))
+        } yield assert(lineage.map(_.operationId))(
+          hasSameElements(
+            Seq(
+              ArraySeq.fill(32)(0),
+              ArraySeq.fill(32)(1),
+              ArraySeq.fill(32)(2),
+              ArraySeq.fill(32)(3),
+            )
+          )
+        )
+      },
+      test("list lineage filtered by did") {
+        for {
+          did1 <- initDIDLineage.map(_._1)
+          lineage <- ZIO.serviceWithZIO[DIDNonSecretStorage](_.listUpdateLineage(Some(did1), None))
+        } yield assert(lineage.map(_.operationId))(
+          hasSameElements(Seq(ArraySeq.fill(32)(0), ArraySeq.fill(32)(1)))
+        )
+      },
+      test("list lineage filtered by status") {
+        for {
+          did1 <- initDIDLineage.map(_._1)
+          lineage <- ZIO.serviceWithZIO[DIDNonSecretStorage](
+            _.listUpdateLineage(None, Some(ScheduledDIDOperationStatus.Pending))
+          )
+        } yield assert(lineage.map(_.operationId))(
+          hasSameElements(Seq(ArraySeq.fill(32)(0), ArraySeq.fill(32)(2)))
+        )
+      },
+      test("list lineage filtered by did and status") {
+        for {
+          did1 <- initDIDLineage.map(_._1)
+          lineage <- ZIO.serviceWithZIO[DIDNonSecretStorage](
+            _.listUpdateLineage(Some(did1), Some(ScheduledDIDOperationStatus.Pending))
+          )
+        } yield assert(lineage.map(_.operationId))(
+          hasSameElements(Seq(ArraySeq.fill(32)(0)))
+        )
+      }
+    )
+  }
+
+  private val setDIDLineageStatusSpec = suite("setDIDLineageStatus")(
+    test("do not fail on setting non-existing did lineage") {
+      for {
+        storage <- ZIO.service[DIDNonSecretStorage]
+        _ <- storage.setDIDUpdateLineageStatus(Array.fill(32)(0), ScheduledDIDOperationStatus.Pending)
+      } yield assertCompletes
+    },
+    test("set status for existing did") {
+      val operationId = Array.fill[Byte](32)(42)
+      for {
+        storage <- ZIO.service[DIDNonSecretStorage]
+        did <- initializeDIDStateAndKeys(Nil).map(_._1)
+        _ <- storage.insertDIDUpdateLineage(
+          did,
+          updateLineage(operationId = operationId, status = ScheduledDIDOperationStatus.Pending)
+        )
+        status1 <- storage.listUpdateLineage(Some(did), None).map(_.head.status)
+        _ <- storage.setDIDUpdateLineageStatus(operationId, ScheduledDIDOperationStatus.Confirmed)
+        status2 <- storage.listUpdateLineage(Some(did), None).map(_.head.status)
+      } yield assert(status1)(equalTo(ScheduledDIDOperationStatus.Pending))
+        && assert(status2)(equalTo(ScheduledDIDOperationStatus.Confirmed))
     }
   )
 
