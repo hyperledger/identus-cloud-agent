74e1d21ff7ea2d454e4e67a41ae3598ccb3aec66
patlo-iog <108713642+patlo-iog@users.noreply.github.com>
2023-02-09 18:53:14 +0700

    test(prism-agent): add wallet-api and key-management tests (#362)
    * WIP: add test containers
    
    * WIP: make test container migration work
    
    * WIP: add sample for JdbcDIDSecretStorage
    
    * test(prism-agent): add DIDSecretStorage getKey tests
    
    * test(prism-agent): add DIDSecretStorage insertKey tests
    
    * test(prism-agent): add removeKey tests
    
    * test(prism-agent): add removeDIDSecret tests
    
    * fix(prism-agent): remove InMemoryStorage implementation
    
    * test(prism-agent): add ManagedDIDState crud tests
    
    * test(prism-agent): add DIDLineage crud tests
    
    * build(prism-agent): use scala 3.2.2
    
    * Revert "build(prism-agent): use scala 3.2.2"
    
    This reverts commit 6b7c183d437776e46660909192c2e9e58ae80e8d.
    
    * test(prism-agent): improve coverage on createAndStoreDID and publishStoredDID
    
    * test(prism-agent): add updateManagedDID tests
    
    * test(prism-agent): add deactivateManagedDID tests
    
    * test(prism-agent): add DID update lineage assertion
    
    * chore: docuemnt some logger config

---
+++ prism-agent/service/wallet-api/src/test/scala/io/iohk/atala/test/container/DBTestUtils.scala
@@ -0,0 +1,37 @@
+package io.iohk.atala.test.container
+
+import com.dimafeng.testcontainers.PostgreSQLContainer
+import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.output.MigrateResult
+import zio.*
+import zio.test.{TestAspect, TestAspectAtLeastR, TestAspectPoly}
+
+object DBTestUtils {
+  def runMigrationAgentDB: RIO[PostgreSQLContainer, MigrateResult] = runMigrationPgContainer(
+    "public",
+    "filesystem:../server/src/main/resources/sql/agent"
+  )
+
+  def runMigrationPgContainer(schema: String, paths: String*): RIO[PostgreSQLContainer, MigrateResult] =
+    for {
+      pg <- ZIO.service[PostgreSQLContainer]
+      result <- runMigration(pg.jdbcUrl, pg.username, pg.password, schema, paths: _*)
+    } yield result
+
+  def runMigration(
+      url: String,
+      username: String,
+      password: String,
+      schema: String,
+      locations: String*
+  ): Task[MigrateResult] =
+    ZIO.attemptBlocking {
+      Flyway
+        .configure()
+        .dataSource(url, username, password)
+        .schemas(schema)
+        .locations(locations: _*)
+        .load()
+        .migrate()
+    }
+}
