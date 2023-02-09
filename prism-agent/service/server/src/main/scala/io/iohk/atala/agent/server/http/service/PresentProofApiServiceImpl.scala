f62636de503bcfff9a95c9d6d44e38d55feebb87
FabioPinheiro <fabiomgpinheiro@gmail.com>
2023-02-09 13:06:22 +0000

    one sbt project

--- prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/service/PresentProofApiServiceImpl.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/service/PresentProofApiServiceImpl.scala
@@ -57,7 +57,7 @@ class PresentProofApiServiceImpl(presentationService: PresentationService, conne
               trustIssuers = Some(e.trustIssuers.map(DidId(_)))
             )
           },
-          options = requestPresentationInput.options.map(x => Options(x.challenge, x.domain))
+          options = None, // FIXME
         )
         .mapError(HttpServiceError.DomainError[PresentationError].apply)
         .mapError(_.toOAS)
