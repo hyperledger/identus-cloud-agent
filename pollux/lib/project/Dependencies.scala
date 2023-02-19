1ae10c8af0caa89f94ed773d0a25105028a6a309
atala-dev <atala.dev@iohk.io>
2023-02-19 00:20:26 +0000

    Update testcontainers-scala-postgresql to 0.40.12 in main

--- pollux/lib/project/Dependencies.scala
+++ pollux/lib/project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val mercury = "0.19.0"
     val castor = "0.8.0"
     val flyway = "9.8.3"
-    val testContainersScalaPostgresql = "0.40.11"
+    val testContainersScalaPostgresql = "0.40.12"
     val quill = "4.6.0"
     val logback = "1.4.5"
     val munit = "1.0.0-M6" // "0.7.29"
