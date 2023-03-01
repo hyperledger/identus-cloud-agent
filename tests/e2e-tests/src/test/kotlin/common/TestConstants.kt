1387c55ac1a70d68f30f2cc28ccb5b8615412cb2
Anton Baliasnikov <anton.baliasnikov@iohk.io>
2023-03-01 14:45:34 +0000

    test: test

--- tests/e2e-tests/src/test/kotlin/common/TestConstants.kt
+++ tests/e2e-tests/src/test/kotlin/common/TestConstants.kt
@@ -9,7 +9,7 @@ import java.util.UUID
 object TestConstants {
     val CREDENTIAL_SCHEMAS = CredentialSchemas
     val RANDOM_CONSTAND_UUID = UUID.randomUUID().toString()
-    val DID_UPDATE_PUBLISH_MAX_WAIT_5_MIN = Duration.ofSeconds(60L)
+    val DID_UPDATE_PUBLISH_MAX_WAIT_5_MIN = Duration.ofSeconds(600L)
     var PRISM_DID_AUTH_KEY = PublicKey("auth_key1", Purpose.AUTHENTICATION)
     val PRISM_DID_ASSERTION_KEY = PublicKey("assertion_key1", Purpose.ASSERTION_METHOD)
     val PRISM_DID_UPDATE_NEW_AUTH_KEY = PublicKey("new_auth_key", Purpose.AUTHENTICATION)
