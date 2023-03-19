c4aff4d106880cf3e972a01984a4facd4e68a2e7
atala-dev <atala.dev@iohk.io>
2023-03-19 00:22:00 +0000

    Update pollux-core to 0.42.0 in main

--- prism-agent/service/project/Dependencies.scala
+++ prism-agent/service/project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val akka = "2.6.20"
     val akkaHttp = "10.2.9"
     val castor = "0.8.1"
-    val pollux = "0.41.0"
+    val pollux = "0.42.0"
     val connect = "0.12.0"
     val bouncyCastle = "1.70"
     val logback = "1.4.6"
