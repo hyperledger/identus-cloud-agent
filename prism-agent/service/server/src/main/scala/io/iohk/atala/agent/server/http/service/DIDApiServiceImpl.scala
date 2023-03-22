1cbb729c05da51d98163a9c99d035f2814cc9fb1
patlo-iog <108713642+patlo-iog@users.noreply.github.com>
2023-03-22 17:43:58 +0700

    feat(prism-agent): add universal-resolver compatible endpoint (#455)
    * feat(prism-agent): add new endpoint def
    
    * new uni resolver endpoknt
    
    * add buildResolutionResp to DIDApiServiceImpl
    
    * add contentType to ResolutionMetadata
    
    * pr cleanup
    
    * docs: improve OAS doc on the response
    
    * docs: correct the content-type header in OAS
    
    * fix(prism-agent): unify resolution endpoint
    
    * fix e2e tests

--- prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/service/DIDApiServiceImpl.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/service/DIDApiServiceImpl.scala
@@ -12,6 +12,10 @@ import io.iohk.atala.agent.openapi.model.*
 import io.iohk.atala.agent.server.http.model.{HttpServiceError, OASDomainModelHelper, OASErrorModelHelper}
 import io.iohk.atala.castor.core.model.error.DIDOperationError
 import zio.*
+import io.iohk.atala.agent.server.http.model.{OASModelPatches, CustomMediaTypes}
+import io.iohk.atala.castor.core.model.did.w3c.DIDResolutionErrorRepr
+import spray.json.{CompactPrinter, JsonWriter, RootJsonFormat}
+import akka.http.scaladsl.marshalling.Marshaller
 
 class DIDApiServiceImpl(service: DIDService)(using runtime: Runtime[Any])
     extends DIDApiService,
@@ -19,17 +23,61 @@ class DIDApiServiceImpl(service: DIDService)(using runtime: Runtime[Any])
       OASDomainModelHelper,
       OASErrorModelHelper {
 
-  override def getDid(didRef: String)(implicit
-      toEntityMarshallerDIDResponse: ToEntityMarshaller[DIDResponse],
-      toEntityMarshallerErrorResponse: ToEntityMarshaller[ErrorResponse]
+  override def getDid(didRef: String, accept: Option[String])(implicit
+      toEntityMarshallerDIDResolutionResult: ToEntityMarshaller[OASModelPatches.DIDResolutionResult]
   ): Route = {
-    val result = makeW3CResolver(service)(didRef).mapError(HttpServiceError.DomainError.apply)
-    onZioSuccess(result.mapBoth(_.toOAS, _.toOAS).either) {
-      case Left(error)     => complete(error.status -> error)
-      case Right(response) => getDid200(response)
-    }
+    val result = for {
+      result <- makeW3CResolver(service)(didRef).either
+      resolutionResult = result.fold(_.toOAS, _.toOAS)
+      resolutionError = result.swap.toOption
+    } yield buildHttpBindingResponse(resolutionResult, resolutionError)
+
+    onZioSuccess(result)(identity)
   }
 
+  // Return response dynamically based on "Content-Type" negotiation
+  // according to https://w3c-ccg.github.io/did-resolution/#bindings-https
+  private def buildHttpBindingResponse(
+      resolutionResult: OASModelPatches.DIDResolutionResult,
+      resolutionError: Option[DIDResolutionErrorRepr]
+  ): Route = {
+    import io.iohk.atala.agent.server.http.marshaller.JsonSupport.{optionFormat, given}
+    import DIDResolutionErrorRepr.*
+
+    val jsonLdMarshaller: ToEntityMarshaller[OASModelPatches.DIDResolutionResult] = {
+      val writer = optionFormat[OASModelPatches.DIDDocument]
+      Marshaller.StringMarshaller
+        .wrap(CustomMediaTypes.`application/did+ld+json`)(CompactPrinter)
+        .compose(writer.write)
+        .compose(_.didDocument)
+    }
+    val resolutionResultMarshaller: ToEntityMarshaller[OASModelPatches.DIDResolutionResult] = {
+      val writer = summon[RootJsonFormat[OASModelPatches.DIDResolutionResult]]
+      Marshaller.StringMarshaller
+        .wrap(CustomMediaTypes.`application/ld+json;did-resolution`)(CompactPrinter)
+        .compose(writer.write)
+        .compose { result =>
+          result.copy(didResolutionMetadata =
+            result.didResolutionMetadata.copy(contentType = Some(CustomMediaTypes.`application/did+ld+json`.value))
+          )
+        }
+    }
+
+    given ToEntityMarshaller[OASModelPatches.DIDResolutionResult] =
+      Marshaller.oneOf(resolutionResultMarshaller, jsonLdMarshaller)
+
+    val isDeactivated = resolutionResult.didDocumentMetadata.deactivated.getOrElse(false)
+    resolutionError match {
+      case None if !isDeactivated           => complete(200 -> resolutionResult)
+      case None                             => complete(410 -> resolutionResult)
+      case Some(InvalidDID)                 => complete(400 -> resolutionResult)
+      case Some(InvalidDIDUrl)              => complete(400 -> resolutionResult)
+      case Some(NotFound)                   => complete(404 -> resolutionResult)
+      case Some(RepresentationNotSupported) => complete(406 -> resolutionResult)
+      case Some(InternalError)              => complete(500 -> resolutionResult)
+      case Some(_)                          => complete(500 -> resolutionResult)
+    }
+  }
 }
 
 object DIDApiServiceImpl {
