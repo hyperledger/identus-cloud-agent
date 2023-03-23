50087394766a7e4f36303b22b9fe22b8441e4c69
Pat Losoponkul <pat.losoponkul@iohk.io>
2023-03-23 13:37:14 +0700

    fix(castor): make errorMessage part of resolutionError

--- castor/lib/core/src/main/scala/io/iohk/atala/castor/core/model/did/w3c/DIDMetadata.scala
+++ castor/lib/core/src/main/scala/io/iohk/atala/castor/core/model/did/w3c/DIDMetadata.scala
@@ -3,15 +3,15 @@ package io.iohk.atala.castor.core.model.did.w3c
 import java.time.Instant
 
 // errors are based on https://www.w3.org/TR/did-spec-registries/#error
-enum DIDResolutionErrorRepr(val value: String) {
-  case InvalidDID(msg: String) extends DIDResolutionErrorRepr("invalidDid")
-  case InvalidDIDUrl(msg: String) extends DIDResolutionErrorRepr("invalidDidUrl")
-  case NotFound extends DIDResolutionErrorRepr("notFound")
-  case RepresentationNotSupported extends DIDResolutionErrorRepr("representationNotSupported")
-  case InternalError(msg: String) extends DIDResolutionErrorRepr("internalError")
-  case InvalidPublicKeyLength extends DIDResolutionErrorRepr("invalidPublicKeyLength")
-  case InvalidPublicKeyType extends DIDResolutionErrorRepr("invalidPublicKeyType")
-  case UnsupportedPublicKeyType extends DIDResolutionErrorRepr("unsupportedPublicKeyType")
+enum DIDResolutionErrorRepr(val value: String, val errorMessage: Option[String]) {
+  case InvalidDID(msg: String) extends DIDResolutionErrorRepr("invalidDid", Some(msg))
+  case InvalidDIDUrl(msg: String) extends DIDResolutionErrorRepr("invalidDidUrl", Some(msg))
+  case NotFound extends DIDResolutionErrorRepr("notFound", None)
+  case RepresentationNotSupported extends DIDResolutionErrorRepr("representationNotSupported", None)
+  case InternalError(msg: String) extends DIDResolutionErrorRepr("internalError", Some(msg))
+  case InvalidPublicKeyLength extends DIDResolutionErrorRepr("invalidPublicKeyLength", None)
+  case InvalidPublicKeyType extends DIDResolutionErrorRepr("invalidPublicKeyType", None)
+  case UnsupportedPublicKeyType extends DIDResolutionErrorRepr("unsupportedPublicKeyType", None)
 }
 
 final case class DIDDocumentMetadataRepr(deactivated: Boolean, canonicalId: String, versionId: String)
