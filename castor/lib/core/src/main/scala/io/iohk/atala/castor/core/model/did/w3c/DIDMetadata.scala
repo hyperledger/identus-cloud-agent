c31b6d92027e96057f6bb225458fafe631b99fcb
Pat Losoponkul <pat.losoponkul@iohk.io>
2023-03-23 11:53:07 +0700

    fix(castor): return error detail when resolving using w3c resolver

--- castor/lib/core/src/main/scala/io/iohk/atala/castor/core/model/did/w3c/DIDMetadata.scala
+++ castor/lib/core/src/main/scala/io/iohk/atala/castor/core/model/did/w3c/DIDMetadata.scala
@@ -4,11 +4,11 @@ import java.time.Instant
 
 // errors are based on https://www.w3.org/TR/did-spec-registries/#error
 enum DIDResolutionErrorRepr(val value: String) {
-  case InvalidDID extends DIDResolutionErrorRepr("invalidDid")
-  case InvalidDIDUrl extends DIDResolutionErrorRepr("invalidDidUrl")
+  case InvalidDID(msg: String) extends DIDResolutionErrorRepr("invalidDid")
+  case InvalidDIDUrl(msg: String) extends DIDResolutionErrorRepr("invalidDidUrl")
   case NotFound extends DIDResolutionErrorRepr("notFound")
   case RepresentationNotSupported extends DIDResolutionErrorRepr("representationNotSupported")
-  case InternalError extends DIDResolutionErrorRepr("internalError")
+  case InternalError(msg: String) extends DIDResolutionErrorRepr("internalError")
   case InvalidPublicKeyLength extends DIDResolutionErrorRepr("invalidPublicKeyLength")
   case InvalidPublicKeyType extends DIDResolutionErrorRepr("invalidPublicKeyType")
   case UnsupportedPublicKeyType extends DIDResolutionErrorRepr("unsupportedPublicKeyType")
