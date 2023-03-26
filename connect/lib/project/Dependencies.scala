e644dd0cddb424d6ecbc8908ba8e6fa70f7b0218
atala-dev <atala.dev@iohk.io>
2023-03-26 00:19:38 +0000

    Revert commit(s) c663ca8a, 11169036, 32486615, 4cca5f8c, ffb2eb95, c19cf609, 1ec48a49, a77c626d, 25c9baa4, ef61af21, 88b8575b, c4aff4d1, f1d1f49e, 800831df, 7e7f0e54, 1397f442, 334e38b2, 60793bbd, 8a3b44b9, 85aa0d4e, 9a1c088a, 29cf7611, 53ab15be, dd0723ab, 114dab7b, 8d717102, 144f8a9b, 0c9a14bf, 65bf3773, 338296f6, 551a120b, d616086e, 3552589e, fef9b1af

--- connect/lib/project/Dependencies.scala
+++ connect/lib/project/Dependencies.scala
@@ -2,15 +2,15 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val zio = "2.0.10"
+    val zio = "2.0.4"
     val doobie = "1.0.0-RC2"
-    val zioCatsInterop = "23.0.0.2"
+    val zioCatsInterop = "3.3.0"
     val iris = "0.1.0"
     val mercury = "0.20.0"
-    val flyway = "9.16.0"
+    val flyway = "9.8.3"
     val shared = "0.2.0"
-    val testContainersScalaPostgresql = "0.40.12"
-    val logback = "1.4.6"
+    val testContainersScalaPostgresql = "0.40.11"
+    val logback = "1.4.5"
   }
 
   private lazy val zio = "dev.zio" %% "zio" % Versions.zio
