5d33c8144997cccbefdca6f59adcb3a3981b5d37
atala-dev <atala.dev@iohk.io>
2023-03-26 00:19:45 +0000

    Update jwt-circe to 9.2.0 in main

--- pollux/lib/project/Dependencies_VC_JWT.scala
+++ pollux/lib/project/Dependencies_VC_JWT.scala
@@ -5,7 +5,7 @@ object Dependencies_VC_JWT {
   object Versions {
     val zio = "2.0.4"
     val circeVersion = "0.14.3"
-    val jwtCirceVersion = "9.1.2"
+    val jwtCirceVersion = "9.2.0"
     val zioPreludeVersion = "1.0.0-RC16"
     val castor = Dependencies.Versions.castor
   }
