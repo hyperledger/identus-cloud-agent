34dd8e8f23f2e271954d0838d3ec2295e09c9ab4
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-03-31 12:27:00 +0200

    chore(prism-agent): add connection controller layer

--- prism-agent/service/server/src/main/scala/io/iohk/atala/connect/controller/ConnectionControllerImpl.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/connect/controller/ConnectionControllerImpl.scala
@@ -1,16 +1,16 @@
 package io.iohk.atala.connect.controller
 
-import io.iohk.atala.agent.server.config.AgentConfig
+import io.iohk.atala.agent.server.config.{AgentConfig, AppConfig}
 import io.iohk.atala.agent.walletapi.service.ManagedDIDService
 import io.iohk.atala.api.http.{ErrorResponse, RequestContext}
 import io.iohk.atala.connect.controller.http.{Connection, CreateConnectionRequest}
 import io.iohk.atala.connect.core.service.ConnectionService
-import zio.IO
+import zio.{IO, URLayer, ZLayer}
 
 class ConnectionControllerImpl(
     service: ConnectionService,
     managedDIDService: ManagedDIDService,
-    agentConfig: AgentConfig
+    appConfig: AppConfig
 ) extends ConnectionController {
 
   override def createConnection(request: CreateConnectionRequest)(implicit
@@ -18,11 +18,16 @@ class ConnectionControllerImpl(
   ): IO[ErrorResponse, Connection] = {
     val result = for {
       pairwiseDid <- managedDIDService
-        .createAndStorePeerDID(agentConfig.didCommServiceEndpointUrl)
+        .createAndStorePeerDID(appConfig.agent.didCommServiceEndpointUrl)
       connection <- service.createConnectionInvitation(request.label, pairwiseDid.did)
     } yield Connection.fromDomain(connection)
 
     result.mapError(_ => ErrorResponse(500, "", "", None, ""))
   }
 
+}
+
+object ConnectionControllerImpl {
+  val layer: URLayer[ConnectionService & ManagedDIDService & AppConfig, ConnectionController] =
+    ZLayer.fromFunction(ConnectionControllerImpl(_, _, _))
 }
