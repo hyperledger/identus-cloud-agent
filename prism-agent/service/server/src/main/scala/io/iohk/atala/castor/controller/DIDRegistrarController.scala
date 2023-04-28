421f626034807dca2d06df4d73a8800677b65985
Pat Losoponkul <pat.losoponkul@iohk.io>
2023-04-28 13:01:18 +0700

    move impl to same file

--- prism-agent/service/server/src/main/scala/io/iohk/atala/castor/controller/DIDRegistrarController.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/castor/controller/DIDRegistrarController.scala
@@ -4,10 +4,43 @@ import io.iohk.atala.api.http.{ErrorResponse, RequestContext}
 import io.iohk.atala.api.http.model.PaginationInput
 import io.iohk.atala.castor.controller.http.ManagedDIDPage
 import zio.*
+import io.iohk.atala.agent.walletapi.service.ManagedDIDService
+import io.iohk.atala.agent.server.http.model.HttpServiceError
+import io.iohk.atala.api.http.model.CollectionStats
+import io.iohk.atala.api.util.PaginationUtils
 
 trait DIDRegistrarController {
   def listManagedDid(
       requestContext: RequestContext,
       paginationInput: PaginationInput
   ): IO[ErrorResponse, ManagedDIDPage]
+}
+
+class DIDRegistrarControllerImpl(service: ManagedDIDService) extends DIDRegistrarController {
+  override def listManagedDid(
+      requestContext: RequestContext,
+      paginationInput: PaginationInput
+  ): IO[ErrorResponse, ManagedDIDPage] = {
+    val uri = requestContext.request.uri
+    val pagination = paginationInput.toPagination
+    val result = for {
+      pageResult <- service
+        .listManagedDIDPage(offset = pagination.offset, limit = pagination.limit)
+        .mapError(HttpServiceError.DomainError.apply)
+      (items, totalCount) = pageResult
+      stats = CollectionStats(totalCount = totalCount, filteredCount = totalCount)
+    } yield ManagedDIDPage(
+      self = uri.toString(),
+      pageOf = PaginationUtils.composePageOfUri(uri).toString,
+      next = PaginationUtils.composeNextUri(uri, items, pagination, stats).map(_.toString),
+      previous = PaginationUtils.composePreviousUri(uri, items, pagination, stats).map(_.toString),
+      contents = items.map(i => i),
+    )
+
+    ???
+  }
+}
+
+object DIDRegistrarControllerImpl {
+  val layer: URLayer[ManagedDIDService, DIDRegistrarController] = ZLayer.fromFunction(DIDRegistrarControllerImpl(_))
 }
