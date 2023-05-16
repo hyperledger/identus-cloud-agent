b41f6aeeecc540af7dd8cc0348180aec7618f186
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-05-16 13:38:36 +0200

    chore(prism-agent): clean-up unused on-chain VC publication code

--- prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/jobs/BackgroundJobs.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/jobs/BackgroundJobs.scala
@@ -723,35 +723,6 @@ object BackgroundJobs {
     } yield agent
   }
 
-  val publishCredentialsToDlt = {
-    for {
-      credentialService <- ZIO.service[CredentialService]
-      _ <- performPublishCredentialsToDlt(credentialService)
-    } yield ()
-
-  }
-
-  private[this] def performPublishCredentialsToDlt(credentialService: CredentialService) = {
-    val res: ZIO[Any, CredentialServiceError, Unit] = for {
-      records <- credentialService.getIssueCredentialRecordsByStates(
-        ignoreWithZeroRetries = true,
-        IssueCredentialRecord.ProtocolState.CredentialPending
-      )
-      // NOTE: the line below is a potentially slow operation, because <createCredentialPayloadFromRecord> makes a database SELECT call,
-      // so calling this function n times will make n database SELECT calls, while it can be optimized to get
-      // all data in one query, this function here has to be refactored as well. Consider doing this if this job is too slow
-      credentials <- ZIO.foreach(records) { record =>
-        credentialService.createCredentialPayloadFromRecord(record, credentialService.createIssuer, Instant.now())
-      }
-      // FIXME: issuer here should come from castor not from credential service, this needs to be done before going to prod
-      publishedBatchData <- credentialService.publishCredentialBatch(credentials, credentialService.createIssuer)
-      _ <- credentialService.markCredentialRecordsAsPublishQueued(publishedBatchData.credentialsAnsProofs)
-      // publishedBatchData gives back irisOperationId, which should be persisted to track the status
-    } yield ()
-
-    ZIO.unit
-  }
-
   val syncDIDPublicationStateFromDlt =
     for {
       managedDidService <- ZIO.service[ManagedDIDService]
