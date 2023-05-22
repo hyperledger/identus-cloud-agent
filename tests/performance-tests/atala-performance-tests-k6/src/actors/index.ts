309ff46fdd9e28304b75a2c2b3eb0ed6acbc90c0
Anton Baliasnikov <anton.baliasnikov@iohk.io>
2023-05-22 17:15:04 +0100

    test: add initial k6 performance scenarios

---
+++ tests/performance-tests/atala-performance-tests-k6/src/actors/index.ts
@@ -0,0 +1,5 @@
+import { Actor } from "./Actor";
+import { Holder } from "./Holder";
+import { Issuer } from "./Issuer";
+import { Verifier } from "./Verifier";
+export { Actor, Holder, Issuer, Verifier };
