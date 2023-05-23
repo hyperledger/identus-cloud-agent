76da3df19f92ef2958d86020af85eb800b964216
abalias <Anton.baliasnikov@iohk.io>
2023-05-23 08:31:09 +0100

    test: add initial k6 performance scenarios (#532)

---
+++ tests/performance-tests/atala-performance-tests-k6/src/actors/Actor.ts
@@ -0,0 +1,27 @@
+import { ConnectionService } from "../common/ConnectionService";
+import { CredentialsService } from "../common/CredentialsService";
+import { ProofsService } from "../common/ProofsService";
+import { DidService } from "../common/DidService";
+
+/**
+ * Base class for all SSI actors
+ */
+export class Actor {
+  connectionService: ConnectionService;
+  credentialsService: CredentialsService;
+  proofsService: ProofsService;
+  didService: DidService;
+  did: string | undefined;
+
+  /**
+   * Constructs a new instance of the Actor class.
+   * @param {string} url - The URL for the services.
+   * @param {string} apiKey - The API key for authentication.
+   */
+  constructor(url: string, apiKey: string) {
+    this.connectionService = new ConnectionService(url, apiKey);
+    this.credentialsService = new CredentialsService(url, apiKey);
+    this.didService = new DidService(url, apiKey);
+    this.proofsService = new ProofsService(url, apiKey);
+  }
+}
