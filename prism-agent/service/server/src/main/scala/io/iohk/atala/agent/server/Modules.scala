395f9c3ac87d19a04f053712aa1eb45257ae3e74
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-05-26 15:44:28 +0200

    chore(prism-agent): finalise main branch merge

--- prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/Modules.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/Modules.scala
@@ -14,12 +14,7 @@ import io.iohk.atala.agent.walletapi.crypto.Apollo
 import io.iohk.atala.agent.walletapi.model.error.DIDSecretStorageError
 import io.iohk.atala.agent.walletapi.service.ManagedDIDService
 import io.iohk.atala.agent.walletapi.sql.{JdbcDIDNonSecretStorage, JdbcDIDSecretStorage}
-import io.iohk.atala.castor.controller.{
-  DIDController,
-  DIDRegistrarController,
-  DIDRegistrarServerEndpoints,
-  DIDServerEndpoints
-}
+import io.iohk.atala.castor.controller.{DIDController, DIDRegistrarController, DIDRegistrarServerEndpoints, DIDServerEndpoints}
 import io.iohk.atala.castor.core.service.{DIDService, DIDServiceImpl}
 import io.iohk.atala.castor.core.util.DIDOperationValidator
 import io.iohk.atala.connect.controller.{ConnectionController, ConnectionControllerImpl, ConnectionServerEndpoints}
@@ -44,21 +39,12 @@ import io.iohk.atala.pollux.core.repository.{CredentialRepository, PresentationR
 import io.iohk.atala.pollux.core.service.*
 import io.iohk.atala.pollux.credentialschema.controller.*
 import io.iohk.atala.pollux.credentialschema.{SchemaRegistryServerEndpoints, VerificationPolicyServerEndpoints}
-import io.iohk.atala.pollux.sql.repository.{
-  JdbcCredentialRepository,
-  JdbcCredentialSchemaRepository,
-  JdbcPresentationRepository,
-  JdbcVerificationPolicyRepository,
-  DbConfig as PolluxDbConfig
-}
+import io.iohk.atala.pollux.sql.repository.{JdbcCredentialRepository, JdbcCredentialSchemaRepository, JdbcPresentationRepository, JdbcVerificationPolicyRepository, DbConfig as PolluxDbConfig}
 import io.iohk.atala.pollux.vc.jwt.{PrismDidResolver, DidResolver as JwtDidResolver}
-import io.iohk.atala.presentproof.controller.{
-  PresentProofController,
-  PresentProofEndpoints,
-  PresentProofServerEndpoints
-}
+import io.iohk.atala.presentproof.controller.{PresentProofController, PresentProofEndpoints, PresentProofServerEndpoints}
 import io.iohk.atala.prism.protos.node_api.NodeServiceGrpc
 import io.iohk.atala.resolvers.{DIDResolver, UniversalDidResolver}
+import io.iohk.atala.system.controller.{SystemController, SystemServerEndpoints}
 import org.didcommx.didcomm.DIDComm
 import org.didcommx.didcomm.model.UnpackParams
 import org.didcommx.didcomm.secret.{Secret, SecretResolver}
@@ -72,15 +58,6 @@ import zio.stream.ZStream
 
 import java.io.IOException
 import java.util.concurrent.Executors
-import io.iohk.atala.mercury.protocol.trustping.TrustPing
-import io.iohk.atala.castor.controller.{
-  DIDController,
-  DIDRegistrarController,
-  DIDRegistrarServerEndpoints,
-  DIDServerEndpoints
-}
-import io.iohk.atala.agent.walletapi.crypto.Apollo
-import io.iohk.atala.system.controller.{SystemController, SystemServerEndpoints}
 
 object Modules {
 
