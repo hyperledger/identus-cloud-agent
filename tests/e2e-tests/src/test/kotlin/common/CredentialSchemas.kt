53607435659d5606fe3f603430b2aeafb6a1d719
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-06-07 13:59:09 +0200

    test(prism-agent): fix e2e tests

--- tests/e2e-tests/src/test/kotlin/common/CredentialSchemas.kt
+++ tests/e2e-tests/src/test/kotlin/common/CredentialSchemas.kt
@@ -8,11 +8,11 @@ object CredentialSchemas {
 
     val CREDENTIAL_SCHEMA_TYPE = "https://w3c-ccg.github.io/vc-json-schemas/schema/2.0/schema.json"
 
-    val SCHEMA_TYPE = "https://json-schema.org/draft/2019-09/schema"
+    val SCHEMA_TYPE = "https://json-schema.org/draft/2020-12/schema"
 
     val JSON_SCHEMA = """
         {
-          "${"$"}id": "student-schema-1.0",
+          "${"$"}id": "https://example.com/student-schema-1.0",
           "${"$"}schema": "$SCHEMA_TYPE",
           "description": "Student schema",
           "type": "object",
@@ -27,9 +27,9 @@ object CredentialSchemas {
         }
     """.trimIndent()
 
-    fun generate_with_name_suffix(suffix: String): CredentialSchema {
+    fun generate_with_name_suffix_and_author(suffix: String, author: String): CredentialSchema {
         return CredentialSchema(
-            author = "did:prism:agent",
+            author = author,
             name = "${UUID.randomUUID()} $suffix",
             description = "Simple student credentials schema",
             type = CREDENTIAL_SCHEMA_TYPE,
