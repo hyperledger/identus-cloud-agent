d2f86b233dc29efd1c36a2f36b05b3f8664e7f0c
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-06-16 17:04:00 +0200

    feat(prism-agent): add event notification service 'in-memory' implementation

---
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/notification/EventNotificationServiceInMemoryImpl.scala
@@ -0,0 +1,18 @@
+package io.iohk.atala.agent.notification
+import zio.{IO, Queue, ULayer, URLayer, ZIO, ZLayer}
+
+class EventNotificationServiceInMemoryImpl(queue: Queue[Event]) extends EventNotificationService {
+
+  override def notify(event: Event): IO[EventNotificationService.Error, Unit] =
+    queue.offer(event).unit
+
+  override def subscribe(topic: String): IO[EventNotificationService.Error, EventConsumer] =
+    ZIO.succeed(new EventConsumer {
+      override def poll(count: Int): IO[EventConsumer.Error, Seq[Event]] = queue.takeBetween(1, count)
+    })
+}
+
+object EventNotificationServiceInMemoryImpl {
+  val layer: URLayer[Queue[Event], EventNotificationServiceInMemoryImpl] =
+    ZLayer.fromFunction(new EventNotificationServiceInMemoryImpl(_))
+}
