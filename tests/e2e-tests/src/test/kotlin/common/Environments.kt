41cf5858e9c9f7459805d42924325723110a604c
Anton Baliasnikov <anton.baliasnikov@iohk.io>
2023-06-20 11:52:15 +0100

    test: add different auth support per cloud agent in e2e

--- tests/e2e-tests/src/test/kotlin/common/Environments.kt
+++ tests/e2e-tests/src/test/kotlin/common/Environments.kt
@@ -3,9 +3,12 @@ package common
 object Environments {
     val AGENT_AUTH_REQUIRED: Boolean = System.getenv("AGENT_AUTH_REQUIRED").toBoolean()
     val AGENT_AUTH_HEADER = System.getenv("AGENT_AUTH_HEADER") ?: "apikey"
-    val AGENT_AUTH_KEY = System.getenv("AGENT_AUTH_KEY") ?: ""
+    val ACME_AUTH_KEY = System.getenv("ACME_AUTH_KEY") ?: ""
     val ACME_AGENT_URL = System.getenv("ACME_AGENT_URL") ?: "http://localhost:8080/prism-agent"
     val BOB_AGENT_URL = System.getenv("BOB_AGENT_URL") ?: "http://localhost:8090/prism-agent"
+    val BOB_AUTH_KEY = System.getenv("BOB_AUTH_KEY") ?: ""
     val MALLORY_AGENT_URL = System.getenv("MALLORY_AGENT_URL") ?: "http://localhost:8070/prism-agent"
+    val MALLORY_AUTH_KEY = System.getenv("MALLORY_AUTH_KEY") ?: ""
     val FABER_AGENT_URL = System.getenv("FABER_AGENT_URL") ?: "http://localhost:8070/prism-agent"
+    val FABER_AUTH_KEY = System.getenv("FABER_AUTH_KEY") ?: ""
 }
