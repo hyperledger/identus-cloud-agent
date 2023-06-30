44da4ab39fd011aa639ad8d387d0b19a4001d3cc
Anton Baliasnikov <anton.baliasnikov@iohk.io>
2023-06-30 10:34:27 +0100

    chore: [skip ci]

--- tests/performance-tests/atala-performance-tests-k6/src/tests/dids/create-prism-did-test.ts
+++ tests/performance-tests/atala-performance-tests-k6/src/tests/dids/create-prism-did-test.ts
@@ -1,24 +1,23 @@
 import { Options } from 'k6/options';
 import { Issuer } from '../../actors';
 
-// export let options: Options = {
-//     stages: [
-//       { duration: '1m', target: 5 },
-//     ],
-//     thresholds: {
-//       http_req_failed: [{
-//         threshold: 'rate<=0.05',
-//         abortOnFail: true,
-//       }],
-//       http_req_duration: ['p(95)<=100'],
-//       checks: ['rate>=0.99'],
-//     },
-//   };
-
 export let options: Options = {
-    vus: 1,
-    iterations: 3000
-  };
+  scenarios: {
+    smoke: {
+      executor: 'constant-vus',
+      vus: 3,
+      duration: "1m",
+    },
+  },
+  thresholds: {
+    http_req_failed: [{
+      threshold: 'rate==0',
+      abortOnFail: true,
+    }],
+    http_req_duration: ['p(95)<=500'],
+    checks: ['rate==1'],
+  },
+};
 
 const issuer = new Issuer();
 
