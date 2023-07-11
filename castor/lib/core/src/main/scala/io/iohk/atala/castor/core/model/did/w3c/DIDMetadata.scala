d50da7e7c5e494d325f531a65d276d843c372203
Pat Losoponkul <pat.losoponkul@iohk.io>
2023-07-11 15:54:34 +0700

    finish rebase to main

--- castor/lib/core/src/main/scala/io/iohk/atala/castor/core/model/did/w3c/DIDMetadata.scala
+++ castor/lib/core/src/main/scala/io/iohk/atala/castor/core/model/did/w3c/DIDMetadata.scala
@@ -12,4 +12,5 @@ enum DIDResolutionErrorRepr(val value: String, val errorMessage: Option[String])
   case UnsupportedPublicKeyType extends DIDResolutionErrorRepr("unsupportedPublicKeyType", None)
 }
 
+// TODO: add `updated` and `crated` timestamp to metadata
 final case class DIDDocumentMetadataRepr(deactivated: Boolean, canonicalId: Option[String], versionId: String)
