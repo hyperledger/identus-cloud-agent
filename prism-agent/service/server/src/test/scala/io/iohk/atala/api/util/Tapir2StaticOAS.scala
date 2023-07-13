47b273a71cec2e92be3093a86ccf83451c359ca6
bvoiturier <benjamin.voiturier@iohk.io>
2023-07-13 18:48:22 +0200

    chore(prism-agent): solve Tapir <> static OAS desync issue (#591)
    * chore(prism-agent): add executable utility class to generate static OAS spec file from Tapir endpoints
    
    * chore(prism-agent): add missing Mockito dependency
    
    * chore(prism-agent): allow API version in generated OAS to be specified as command-line arg

---
+++ prism-agent/service/server/src/test/scala/io/iohk/atala/api/util/Tapir2StaticOAS.scala
@@ -0,0 +1,62 @@
+package io.iohk.atala.api.util
+
+import io.iohk.atala.castor.controller.{
+  DIDController,
+  DIDRegistrarController,
+  DIDRegistrarServerEndpoints,
+  DIDServerEndpoints
+}
+import io.iohk.atala.connect.controller.{ConnectionController, ConnectionServerEndpoints}
+import io.iohk.atala.issue.controller.{IssueController, IssueServerEndpoints}
+import io.iohk.atala.pollux.credentialschema.controller.{CredentialSchemaController, VerificationPolicyController}
+import io.iohk.atala.pollux.credentialschema.{SchemaRegistryServerEndpoints, VerificationPolicyServerEndpoints}
+import io.iohk.atala.presentproof.controller.{PresentProofController, PresentProofServerEndpoints}
+import io.iohk.atala.system.controller.{SystemController, SystemServerEndpoints}
+import org.scalatestplus.mockito.MockitoSugar.*
+import sttp.tapir.docs.openapi.OpenAPIDocsInterpreter
+import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Path}
+import scala.util.Using
+
+object Tapir2StaticOAS extends ZIOAppDefault {
+
+  @main override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
+    val effect = for {
+      args <- getArgs
+      allSchemaRegistryEndpoints <- SchemaRegistryServerEndpoints.all
+      allVerificationPolicyEndpoints <- VerificationPolicyServerEndpoints.all
+      allConnectionEndpoints <- ConnectionServerEndpoints.all
+      allIssueEndpoints <- IssueServerEndpoints.all
+      allDIDEndpoints <- DIDServerEndpoints.all
+      allDIDRegistrarEndpoints <- DIDRegistrarServerEndpoints.all
+      allPresentProofEndpoints <- PresentProofServerEndpoints.all
+      allSystemEndpoints <- SystemServerEndpoints.all
+      allEndpoints = allSchemaRegistryEndpoints ++
+        allVerificationPolicyEndpoints ++
+        allConnectionEndpoints ++
+        allDIDEndpoints ++
+        allDIDRegistrarEndpoints ++
+        allIssueEndpoints ++
+        allPresentProofEndpoints ++
+        allSystemEndpoints
+    } yield {
+      import sttp.apispec.openapi.circe.yaml.*
+      val yaml = OpenAPIDocsInterpreter().toOpenAPI(allEndpoints.map(_.endpoint), "Prism Agent", args(1)).toYaml
+      val path = Path.of(args.head)
+      Using(Files.newBufferedWriter(path, StandardCharsets.UTF_8)) { writer => writer.write(yaml) }
+    }
+    effect.provideSomeLayer(
+      ZLayer.succeed(mock[ConnectionController]) ++
+        ZLayer.succeed(mock[CredentialSchemaController]) ++
+        ZLayer.succeed(mock[VerificationPolicyController]) ++
+        ZLayer.succeed(mock[DIDRegistrarController]) ++
+        ZLayer.succeed(mock[PresentProofController]) ++
+        ZLayer.succeed(mock[IssueController]) ++
+        ZLayer.succeed(mock[DIDController]) ++
+        ZLayer.succeed(mock[SystemController])
+    )
+  }
+
+}
