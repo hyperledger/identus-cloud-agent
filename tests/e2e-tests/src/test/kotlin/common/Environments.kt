5553dd55e95eb1e97047a44b94f5d422ac36c6ac
Anton Baliasnikov <anton.baliasnikov@iohk.io>
2023-07-13 22:32:26 +0100

    chore: revert change in verifiers port

--- tests/e2e-tests/src/test/kotlin/common/Environments.kt
+++ tests/e2e-tests/src/test/kotlin/common/Environments.kt
@@ -11,7 +11,7 @@ object Environments {
     val BOB_AUTH_KEY = System.getenv("BOB_AUTH_KEY") ?: ""
     val BOB_AGENT_WEBHOOK_HOST = System.getenv("BOB_AGENT_WEBHOOK_HOST") ?: "0.0.0.0"
     val BOB_AGENT_WEBHOOK_PORT = (System.getenv("BOB_AGENT_WEBHOOK_PORT") ?: "9956").toInt()
-    val FABER_AGENT_URL = System.getenv("FABER_AGENT_URL") ?: "http://localhost:8100/prism-agent"
+    val FABER_AGENT_URL = System.getenv("FABER_AGENT_URL") ?: "http://localhost:8070/prism-agent"
     val FABER_AUTH_KEY = System.getenv("FABER_AUTH_KEY") ?: ""
     val FABER_AGENT_WEBHOOK_HOST = System.getenv("FABER_AGENT_WEBHOOK_HOST") ?: "0.0.0.0"
     val FABER_AGENT_WEBHOOK_PORT = (System.getenv("FABER_AGENT_WEBHOOK_PORT") ?: "9957").toInt()
