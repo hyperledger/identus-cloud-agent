e1f4e5450b5fcfe284dd50a8cd7225b251584d53
Pat Losoponkul <pat.losoponkul@iohk.io>
2023-07-25 11:44:43 +0700

    refactor: isolate JdbcDIDSecretStorage

--- prism-agent/service/wallet-api/src/main/scala/io/iohk/atala/agent/walletapi/sql/JdbcDIDSecretStorage.scala
+++ prism-agent/service/wallet-api/src/main/scala/io/iohk/atala/agent/walletapi/sql/JdbcDIDSecretStorage.scala
@@ -5,6 +5,8 @@ import doobie.*
 import doobie.implicits.*
 import io.iohk.atala.agent.walletapi.storage.DIDSecretStorage
 import io.iohk.atala.mercury.model.DidId
+import io.iohk.atala.shared.models.WalletAccessContext
+import io.iohk.atala.shared.models.WalletId
 import java.time.Instant
 import java.util.UUID
 import zio.*
@@ -28,37 +30,45 @@ class JdbcDIDSecretStorage(xa: Transactor[Task]) extends DIDSecretStorage {
   given octetKeyPairGet: Get[OctetKeyPair] = Get[String].map(OctetKeyPair.parse)
   given octetKeyPairPut: Put[OctetKeyPair] = Put[String].contramap(_.toJSONString)
 
-  override def getKey(did: DidId, keyId: String): Task[Option[OctetKeyPair]] = {
-    val cxnIO = sql"""
+  override def getKey(did: DidId, keyId: String): RIO[WalletAccessContext, Option[OctetKeyPair]] = {
+    val cxnIO = (walletId: WalletId) =>
+      sql"""
         | SELECT
         |   key_pair
-        | FROM public.did_secret_storage
+        | FROM public.peer_did_rand_key
         | WHERE
         |   did = $did
         |   AND key_id = $keyId
+        |   AND wallet_id = $walletId
         """.stripMargin
-      .query[OctetKeyPair]
-      .option
+        .query[OctetKeyPair]
+        .option
 
-    cxnIO.transact(xa)
+    ZIO.serviceWithZIO[WalletAccessContext](ctx => cxnIO(ctx.walletId).transact(xa))
   }
 
-  override def insertKey(did: DidId, keyId: String, keyPair: OctetKeyPair): Task[Int] = {
-    val cxnIO = (now: InstantAsBigInt) => sql"""
-        | INSERT INTO public.did_secret_storage(
+  override def insertKey(did: DidId, keyId: String, keyPair: OctetKeyPair): RIO[WalletAccessContext, Int] = {
+    val cxnIO = (now: InstantAsBigInt, walletId: WalletId) => sql"""
+        | INSERT INTO public.peer_did_rand_key(
         |   did,
         |   created_at,
         |   key_id,
-        |   key_pair
+        |   key_pair,
+        |   wallet_id
         | ) values (
         |   ${did},
         |   ${now},
         |   ${keyId},
-        |   ${keyPair}
+        |   ${keyPair},
+        |   ${walletId}
         | )
         """.stripMargin.update
 
-    Clock.instant.flatMap(i => cxnIO(InstantAsBigInt(i)).run.transact(xa))
+    for {
+      now <- Clock.instant
+      walletId <- ZIO.serviceWith[WalletAccessContext](_.walletId)
+      n <- cxnIO(InstantAsBigInt(now), walletId).run.transact(xa)
+    } yield n
   }
 
 }
