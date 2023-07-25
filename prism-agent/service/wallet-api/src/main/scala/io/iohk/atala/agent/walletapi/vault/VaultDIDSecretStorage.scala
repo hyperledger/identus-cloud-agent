ed52954ed8a101cbe3c1091add05a47ab4f869a4
Pat Losoponkul <pat.losoponkul@iohk.io>
2023-07-25 14:18:08 +0700

    refactor: isolate vault path

--- prism-agent/service/wallet-api/src/main/scala/io/iohk/atala/agent/walletapi/vault/VaultDIDSecretStorage.scala
+++ prism-agent/service/wallet-api/src/main/scala/io/iohk/atala/agent/walletapi/vault/VaultDIDSecretStorage.scala
@@ -3,18 +3,18 @@ package io.iohk.atala.agent.walletapi.vault
 import com.nimbusds.jose.jwk.OctetKeyPair
 import io.iohk.atala.agent.walletapi.storage.DIDSecretStorage
 import io.iohk.atala.mercury.model.DidId
+import io.iohk.atala.shared.models.WalletAccessContext
+import scala.util.Failure
 import scala.util.Try
 import zio.*
-import scala.util.Failure
 
 class VaultDIDSecretStorage(vaultKV: VaultKVClient) extends DIDSecretStorage {
 
-  private val WALLET_PATH_PREFIX = "secret/default" // static <tenant-id> in signle-tenant mode
-
-  override def insertKey(did: DidId, keyId: String, keyPair: OctetKeyPair): Task[Int] = {
-    val path = s"$WALLET_PATH_PREFIX/peer-dids/${did.value}/$keyId"
+  override def insertKey(did: DidId, keyId: String, keyPair: OctetKeyPair): RIO[WalletAccessContext, Int] = {
     val kv = encodeOctetKeyPair(keyPair)
     for {
+      wallet <- ZIO.service[WalletAccessContext]
+      path = peerDidKeyPath(wallet)(did, keyId)
       alreadyExist <- vaultKV.get(path).map(_.isDefined)
       _ <- vaultKV
         .set(path, kv)
@@ -23,12 +23,19 @@ class VaultDIDSecretStorage(vaultKV: VaultKVClient) extends DIDSecretStorage {
     } yield 1
   }
 
-  override def getKey(did: DidId, keyId: String): Task[Option[OctetKeyPair]] = {
-    val path = s"$WALLET_PATH_PREFIX/peer-dids/${did.value}/$keyId"
-    vaultKV.get(path).flatMap {
-      case Some(kv) => ZIO.fromTry(decodeOctetKeyPair(kv)).asSome
-      case None     => ZIO.none
-    }
+  override def getKey(did: DidId, keyId: String): RIO[WalletAccessContext, Option[OctetKeyPair]] = {
+    for {
+      wallet <- ZIO.service[WalletAccessContext]
+      path = peerDidKeyPath(wallet)(did, keyId)
+      keyPair <- vaultKV.get(path).flatMap {
+        case Some(kv) => ZIO.fromTry(decodeOctetKeyPair(kv)).asSome
+        case None     => ZIO.none
+      }
+    } yield keyPair
+  }
+
+  private def peerDidKeyPath(wallet: WalletAccessContext)(did: DidId, keyId: String): String = {
+    s"secret/${wallet.walletId.toInt}/dids/peer/${did.value}/keys/$keyId"
   }
 
   private def encodeOctetKeyPair(keyPair: OctetKeyPair): Map[String, String] = {
