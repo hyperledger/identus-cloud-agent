d214cd728499e19b80866388483b989a8536e3ca
Pat Losoponkul <pat.losoponkul@iohk.io>
2023-08-02 19:08:11 +0700

    fix: use uuid postgres col type

--- prism-agent/service/wallet-api/src/main/scala/io/iohk/atala/agent/walletapi/sql/package.scala
+++ prism-agent/service/wallet-api/src/main/scala/io/iohk/atala/agent/walletapi/sql/package.scala
@@ -101,8 +101,8 @@ package object sql {
   given arraySeqByteGet: Get[ArraySeq[Byte]] = Get[Array[Byte]].map(ArraySeq.from)
   given arraySeqBytePut: Put[ArraySeq[Byte]] = Put[Array[Byte]].contramap(_.toArray)
 
-  given walletIdGet: Get[WalletId] = Get[String].map(s => WalletId.fromUUID(UUID.fromString(s)))
-  given walletIdPut: Put[WalletId] = Put[String].contramap(_.toUUID.toString())
+  given walletIdGet: Get[WalletId] = Get[UUID].map(WalletId.fromUUID)
+  given walletIdPut: Put[WalletId] = Put[UUID].contramap(_.toUUID)
 
   final case class DIDStateRow(
       did: PrismDID,
