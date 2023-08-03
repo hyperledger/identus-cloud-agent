c8b45532287126d05b255b6a2579ff46cc78a338
Shota Jolbordi <shota.jolbordi@iohk.io>
2023-08-03 17:03:28 +0400

    Add customMetricsAspect

--- shared/src/main/scala/io/iohk/atala/shared/utils/aspects/CustomMetricsAspect.scala
+++ shared/src/main/scala/io/iohk/atala/shared/utils/aspects/CustomMetricsAspect.scala
@@ -1,22 +1,42 @@
 package io.iohk.atala.shared.utils.aspects
-import java.util.concurrent.TimeUnit
+
 import zio.*
+import scala.collection.mutable.{Map => MutMap}
 import zio.metrics.*
 
 object CustomMetricsAspect {
+  private val checkpoints: MutMap[String, Long] = MutMap.empty
+  private def currTime = ZIO.succeed(java.lang.System.nanoTime)
 
-  def attachDurationGaugeMetric(name: String): ZIOAspect[Nothing, Any, Nothing, Any, Nothing, Any] =
+  def recordTimeAfter(key: String): ZIOAspect[Nothing, Any, Nothing, Any, Nothing, Any] =
     new ZIOAspect[Nothing, Any, Nothing, Any, Nothing, Any] {
       override def apply[R, E, A](
           zio: ZIO[R, E, A]
       )(implicit trace: Trace): ZIO[R, E, A] =
-        def currTime = Clock.currentTime(TimeUnit.MILLISECONDS)
-
         for {
-          timeBefore <- currTime
           res <- zio
           timeAfter <- currTime
-          _ <- ZIO.succeed((timeAfter - timeBefore).toDouble) @@ Metric.gauge(name)
+          _ = checkpoints.update(key, timeAfter)
         } yield res
     }
+
+  def createGaugeAfter(key: String, metricsKey: String, tags: Set[MetricLabel] = Set.empty): ZIOAspect[Nothing, Any, Nothing, Any, Nothing, Any] =
+    new ZIOAspect[Nothing, Any, Nothing, Any, Nothing, Any] {
+      override def apply[R, E, A](
+          zio: ZIO[R, E, A]
+      )(implicit trace: Trace): ZIO[R, E, A] = {
+        for {
+          res <- zio
+          timeAfter <- currTime
+          timeBefore = checkpoints.get(key)
+          metricsZio = timeBefore.map(before => timeAfter - before) match {
+            case Some(value) =>
+              ZIO.succeed(value.toDouble) @@ Metric.gauge(metricsKey).tagged(tags)
+            case None => ZIO.unit
+          }
+          _ <- metricsZio
+        } yield res
+      }
+    }
+
 }
