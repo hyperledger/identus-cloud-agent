bec1f5a50fe1eb34f0b92609fee287345546310b
Pat Losoponkul <pat.losoponkul@iohk.io>
2023-08-29 15:39:04 +0700

    feat: integrate auth api to issue endpoints

--- prism-agent/service/server/src/test/scala/io/iohk/atala/issue/controller/IssueControllerTestTools.scala
+++ prism-agent/service/server/src/test/scala/io/iohk/atala/issue/controller/IssueControllerTestTools.scala
@@ -6,6 +6,8 @@ import io.iohk.atala.agent.server.config.AppConfig
 import io.iohk.atala.api.http.ErrorResponse
 import io.iohk.atala.connect.core.repository.ConnectionRepositoryInMemory
 import io.iohk.atala.connect.core.service.ConnectionServiceImpl
+import io.iohk.atala.iam.authentication.Authenticator
+import io.iohk.atala.iam.authentication.DefaultEntityAuthenticator
 import io.iohk.atala.iris.proto.service.IrisServiceGrpc
 import io.iohk.atala.issue.controller.http.{
   CreateIssueCredentialRecordRequest,
@@ -87,7 +89,8 @@ trait IssueControllerTestTools extends PostgresTestContainerSupport {
   val testEnvironmentLayer = zio.test.testEnvironment ++
     pgContainerLayer ++
     contextAwareTransactorLayer ++
-    controllerLayer
+    controllerLayer ++
+    DefaultEntityAuthenticator.layer
 
   val issueUriBase = uri"http://test.com/issue-credentials/"
 
@@ -96,9 +99,8 @@ trait IssueControllerTestTools extends PostgresTestContainerSupport {
       .defaultHandlers(ErrorResponse.failureResponseHandler)
   }
 
-  def httpBackend(controller: IssueController) = {
-    val mockWalletAccessContext = WalletAccessContext(WalletId.random) // FIXME
-    val issueEndpoints = IssueServerEndpoints(controller, mockWalletAccessContext)
+  def httpBackend(controller: IssueController, authenticator: Authenticator) = {
+    val issueEndpoints = IssueServerEndpoints(controller, authenticator)
 
     val backend =
       TapirStubInterpreter(
