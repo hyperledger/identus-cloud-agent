e1bfc3324fe10a014fa1bd080deccd2f3225e618
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-08-31 12:04:13 +0200

    fix(prism-agent): add foreign key on entity to wallet table
    Signed-off-by: Benjamin Voiturier <benjamin.voiturier@iohk.io>

--- prism-agent/service/wallet-api/src/main/resources/sql/agent/V8__init_entity_repository_tables.sql
+++ prism-agent/service/wallet-api/src/main/resources/sql/agent/V8__init_entity_repository_tables.sql
@@ -1,11 +1,10 @@
 CREATE TABLE public.entity
 (
     id         UUID PRIMARY KEY default gen_random_uuid(),
-    name       VARCHAR(128)             NOT NULL,
---     wallet_id  UUID REFERENCES public.wallet ("wallet_id") NOT NULL,
-    wallet_id  UUID                     NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
+    name       VARCHAR(128)                                NOT NULL,
+    wallet_id  UUID REFERENCES public.wallet ("wallet_id") NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE                    NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE                    NOT NULL
 );
 
 CREATE INDEX entity_wallet_id_index ON public.entity (wallet_id);
