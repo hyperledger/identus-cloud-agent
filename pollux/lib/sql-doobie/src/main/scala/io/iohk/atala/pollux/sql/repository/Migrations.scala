803965482e2634d488d2f4f364b041917be514a5
patlo-iog <pat.losoponkul@iohk.io>
2023-09-15 19:00:11 +0700

    fix(prism-agent): define db app user privileges before app starts (#722)
    Signed-off-by: Pat Losoponkul <pat.losoponkul@iohk.io>

--- pollux/lib/sql-doobie/src/main/scala/io/iohk/atala/pollux/sql/repository/Migrations.scala
+++ pollux/lib/sql-doobie/src/main/scala/io/iohk/atala/pollux/sql/repository/Migrations.scala
@@ -1,8 +1,12 @@
 package io.iohk.atala.pollux.sql.repository
 
+import doobie.*
+import doobie.implicits.*
+import doobie.util.transactor.Transactor
 import io.iohk.atala.shared.db.DbConfig
 import org.flywaydb.core.Flyway
 import zio.*
+import zio.interop.catz.*
 
 final case class Migrations(config: DbConfig) {
 
@@ -31,4 +35,19 @@ final case class Migrations(config: DbConfig) {
 object Migrations {
   val layer: URLayer[DbConfig, Migrations] =
     ZLayer.fromFunction(Migrations.apply _)
+
+  def initDbPrivileges(appUser: String): RIO[Transactor[Task], Unit] = {
+    val cxnIO = for {
+      _ <- doobie.free.connection.createStatement.map { stm =>
+        stm.execute(s"""
+          | ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "$appUser"
+          """.stripMargin)
+      }
+    } yield ()
+
+    for {
+      xa <- ZIO.service[Transactor[Task]]
+      _ <- cxnIO.transact(xa)
+    } yield ()
+  }
 }
