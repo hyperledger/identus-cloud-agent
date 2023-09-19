767ea3481356dffebc661a13e03eda6e5f598119
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-09-19 08:42:21 +0200

    fix(prism-agent): temporarily disable FK and RLS policy on peer_did_rand_key table
    Signed-off-by: Benjamin Voiturier <benjamin.voiturier@iohk.io>

--- prism-agent/service/wallet-api/src/main/resources/sql/agent/V10__create_peer_did_table.sql
+++ prism-agent/service/wallet-api/src/main/resources/sql/agent/V10__create_peer_did_table.sql
@@ -8,8 +8,8 @@ CREATE TABLE public.peer_did
 ALTER TABLE public.peer_did
     ENABLE ROW LEVEL SECURITY;
 
-ALTER TABLE public.peer_did_rand_key
-    ADD CONSTRAINT fk_did FOREIGN KEY ("did") REFERENCES public.peer_did ("did") ON DELETE RESTRICT;
+-- ALTER TABLE public.peer_did_rand_key
+--     ADD CONSTRAINT fk_did FOREIGN KEY ("did") REFERENCES public.peer_did ("did") ON DELETE RESTRICT;
 
 CREATE POLICY peer_did_wallet_isolation
     ON public.peer_did
@@ -20,11 +20,11 @@ DROP POLICY peer_did_rand_key_wallet_isolation ON public.peer_did_rand_key;
 ALTER TABLE public.peer_did_rand_key
     DROP COLUMN "wallet_id";
 
-CREATE POLICY peer_did_rand_key_wallet_isolation
-    ON public.peer_did_rand_key
-    USING (
-    EXISTS (SELECT 1
-            FROM public.peer_did AS s
-            WHERE s.wallet_id = current_setting('app.current_wallet_id')::UUID
-              AND s.did = public.peer_did_rand_key.did)
-    );
+-- CREATE POLICY peer_did_rand_key_wallet_isolation
+--     ON public.peer_did_rand_key
+--     USING (
+--     EXISTS (SELECT 1
+--             FROM public.peer_did AS s
+--             WHERE s.wallet_id = current_setting('app.current_wallet_id')::UUID
+--               AND s.did = public.peer_did_rand_key.did)
+--     );
