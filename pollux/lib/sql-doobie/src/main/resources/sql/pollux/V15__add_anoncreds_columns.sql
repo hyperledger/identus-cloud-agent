ecc68be8b3bcdd964767fbbd2448b97544216c74
FabioPinheiro <fabiomgpinheiro@gmail.com>
2023-09-25 12:45:59 +0100

    Add columns for amoncred for presentation exchange

--- pollux/lib/sql-doobie/src/main/resources/sql/pollux/V15__add_anoncreds_columns.sql
+++ pollux/lib/sql-doobie/src/main/resources/sql/pollux/V15__add_anoncreds_columns.sql
@@ -1,3 +1,4 @@
+-- issue_credential_records
 ALTER TABLE public.issue_credential_records
     ADD COLUMN "credential_format"              VARCHAR(9),
     ADD COLUMN "credential_definition_id"       UUID,
@@ -7,4 +8,16 @@ UPDATE public.issue_credential_records
 SET "credential_format" = 'JWT';
 
 ALTER TABLE public.issue_credential_records
+    ALTER COLUMN "credential_format" SET NOT NULL;
+
+
+
+-- presentation_records
+ALTER TABLE public.presentation_records
+    ADD COLUMN "credential_format"              VARCHAR(9);
+
+UPDATE public.presentation_records
+SET "credential_format" = 'JWT';
+
+ALTER TABLE public.presentation_records
     ALTER COLUMN "credential_format" SET NOT NULL;
