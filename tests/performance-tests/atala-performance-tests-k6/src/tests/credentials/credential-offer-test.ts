09034feba9d0e0fd706f330e32610102331b1c67
David Poltorak <david.poltorak@iohk.io>
2023-09-29 15:01:26 +0100

    fix: create schema before trying to issue credential offer in perf test
    Signed-off-by: David Poltorak <david.poltorak@iohk.io>

--- tests/performance-tests/atala-performance-tests-k6/src/tests/credentials/credential-offer-test.ts
+++ tests/performance-tests/atala-performance-tests-k6/src/tests/credentials/credential-offer-test.ts
@@ -1,8 +1,9 @@
 import { group } from 'k6';
 import { Options } from 'k6/options';
 import { Issuer, Holder } from '../../actors';
-import { Connection } from '@input-output-hk/prism-typescript-client';
+import { Connection, CredentialSchemaResponse } from '@input-output-hk/prism-typescript-client';
 import { defaultScenarios, defaultThresholds } from '../../scenarios/default';
+import { Schema } from 'copy-webpack-plugin';
 export let options: Options = {
   scenarios: {
     ...defaultScenarios
@@ -31,21 +32,28 @@ export function setup() {
     holder.finalizeConnectionWithIssuer();
   });
 
+  group("Issuer creates credential schema", function () {
+    issuer.createCredentialSchema();
+  });
+
   return {
       issuerDid: issuer.did,
       holderDid: holder.did,
+      issuerSchema: issuer.schema,
       connectionWithHolder: issuer.connectionWithHolder!,
       connectionWithIssuer: holder.connectionWithIssuer!
     };
 }
 
-export default (data: { issuerDid: string; holderDid: string; connectionWithHolder: Connection, connectionWithIssuer: Connection }) => {
+export default (data: { issuerDid: string; holderDid: string; issuerSchema: CredentialSchemaResponse, connectionWithHolder: Connection, connectionWithIssuer: Connection }) => {
 
   // This is the only way to pass data from setup to default
   issuer.did = data.issuerDid;
+  issuer.schema = data.issuerSchema
   holder.did = data.holderDid;
   issuer.connectionWithHolder = data.connectionWithHolder;
   holder.connectionWithIssuer = data.connectionWithIssuer;
+
 
   group('Issuer creates credential offer', function () {
     issuer.createCredentialOffer();
