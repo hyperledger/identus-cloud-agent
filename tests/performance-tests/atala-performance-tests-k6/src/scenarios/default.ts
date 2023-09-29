c4f859fe9c1d9e66a8ba9aee3592f9f6db2c4b74
David Poltorak <david.poltorak@iohk.io>
2023-09-29 15:36:55 +0100

    fix: change smoke workload and thresholds
    Signed-off-by: David Poltorak <david.poltorak@iohk.io>

--- tests/performance-tests/atala-performance-tests-k6/src/scenarios/default.ts
+++ tests/performance-tests/atala-performance-tests-k6/src/scenarios/default.ts
@@ -4,10 +4,9 @@ import { Scenario, Threshold } from "k6/options";
 export const defaultScenarios: { [name: string]: Scenario } = {
   smoke: {
     // a simple test to ensure performance tests work and requests don't fail
-    executor: "shared-iterations",
-    vus: 1,
-    iterations: 1,
-    // duration: "2m",
+    executor: "constant-vus",
+    vus: 3,
+    duration: "10s",
     tags: { scenario_label: __ENV.SCENARIO_LABEL || "defaultScenarioLabel" }, // add label for filtering in observability platform
   },
 };
@@ -22,8 +21,8 @@ export const defaultThresholds: { [name: string]: Threshold[] } = {
   ],
   http_req_duration: [
     // fail if any requests take longer than 500ms on smoke test
-    { threshold: "p(95)<500", abortOnFail: true }, // 95% of requests should complete within 500ms
-    { threshold: "p(99)<1500", abortOnFail: true }, // 99% of requests should complete within 1500ms
+    { threshold: "p(95)<1000", abortOnFail: true }, // 95% of requests should complete within 500ms
+    { threshold: "p(99)<2000", abortOnFail: true }, // 99% of requests should complete within 1500ms
   ],
   checks: ["rate==1"], // fail if any checks fail (the checks are defined in test code which is executed)
 };
