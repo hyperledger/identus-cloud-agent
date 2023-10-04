e6f3751f0f5840812ac6fa3efbc0cce9ea62459d
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-10-04 12:06:44 +0200

    test(prism-agent): declare 'credentialFormat' field in e2e tests
    Signed-off-by: Benjamin Voiturier <benjamin.voiturier@iohk.io>

--- tests/e2e-tests/src/main/kotlin/api_models/Credential.kt
+++ tests/e2e-tests/src/main/kotlin/api_models/Credential.kt
@@ -19,6 +19,7 @@ data class Credential(
     var jwtCredential: String = "",
     var issuingDID: String = "",
     var connectionId: String = "",
+    var credentialFormat: String = "",
     var metaRetries: Int = 0,
 ): JsonEncoded
 
