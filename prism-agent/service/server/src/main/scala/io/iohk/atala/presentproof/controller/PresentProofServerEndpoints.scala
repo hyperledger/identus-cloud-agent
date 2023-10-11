9f4b0a2c8a1334deaad4ff3d3b0871e40516fa46
Pat Losoponkul <pat.losoponkul@iohk.io>
2023-10-11 12:09:59 +0700

    feat: migrate presentproof endpoints to authz
    Signed-off-by: Pat Losoponkul <pat.losoponkul@iohk.io>

--- prism-agent/service/server/src/main/scala/io/iohk/atala/presentproof/controller/PresentProofServerEndpoints.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/presentproof/controller/PresentProofServerEndpoints.scala
@@ -4,7 +4,9 @@ import io.iohk.atala.agent.walletapi.model.BaseEntity
 import io.iohk.atala.api.http.RequestContext
 import io.iohk.atala.api.http.model.PaginationInput
 import io.iohk.atala.iam.authentication.Authenticator
+import io.iohk.atala.iam.authentication.Authorizer
 import io.iohk.atala.iam.authentication.DefaultAuthenticator
+import io.iohk.atala.iam.authentication.SecurityLogic
 import io.iohk.atala.iam.authentication.apikey.ApiKeyEndpointSecurityLogic
 import io.iohk.atala.presentproof.controller.PresentProofEndpoints.{
   getAllPresentations,
@@ -21,49 +23,49 @@ import java.util.UUID
 
 class PresentProofServerEndpoints(
     presentProofController: PresentProofController,
-    authenticator: Authenticator[BaseEntity]
+    authenticator: Authenticator[BaseEntity] & Authorizer[BaseEntity]
 ) {
   private val requestPresentationEndpoint: ZServerEndpoint[Any, Any] =
     requestPresentation
-      .zServerSecurityLogic(ApiKeyEndpointSecurityLogic.securityLogic(_)(authenticator))
-      .serverLogic { entity =>
+      .zServerSecurityLogic(SecurityLogic.walletAccessContextFrom(_)(authenticator))
+      .serverLogic { wac =>
         { case (ctx: RequestContext, request: RequestPresentationInput) =>
           presentProofController
             .requestPresentation(request)(ctx)
-            .provideSomeLayer(ZLayer.succeed(entity.walletAccessContext))
+            .provideSomeLayer(ZLayer.succeed(wac))
         }
       }
 
   private val getAllPresentationsEndpoint: ZServerEndpoint[Any, Any] =
     getAllPresentations
-      .zServerSecurityLogic(ApiKeyEndpointSecurityLogic.securityLogic(_)(authenticator))
-      .serverLogic { entity =>
+      .zServerSecurityLogic(SecurityLogic.walletAccessContextFrom(_)(authenticator))
+      .serverLogic { wac =>
         { case (ctx: RequestContext, paginationInput: PaginationInput, thid: Option[String]) =>
           presentProofController
             .getPresentations(paginationInput, thid)(ctx)
-            .provideSomeLayer(ZLayer.succeed(entity.walletAccessContext))
+            .provideSomeLayer(ZLayer.succeed(wac))
         }
       }
 
   private val getPresentationEndpoint: ZServerEndpoint[Any, Any] =
     getPresentation
-      .zServerSecurityLogic(ApiKeyEndpointSecurityLogic.securityLogic(_)(authenticator))
-      .serverLogic { entity =>
+      .zServerSecurityLogic(SecurityLogic.walletAccessContextFrom(_)(authenticator))
+      .serverLogic { wac =>
         { case (ctx: RequestContext, presentationId: UUID) =>
           presentProofController
             .getPresentation(presentationId)(ctx)
-            .provideSomeLayer(ZLayer.succeed(entity.walletAccessContext))
+            .provideSomeLayer(ZLayer.succeed(wac))
         }
       }
 
   private val updatePresentationEndpoint: ZServerEndpoint[Any, Any] =
     updatePresentation
-      .zServerSecurityLogic(ApiKeyEndpointSecurityLogic.securityLogic(_)(authenticator))
-      .serverLogic { entity =>
+      .zServerSecurityLogic(SecurityLogic.walletAccessContextFrom(_)(authenticator))
+      .serverLogic { wac =>
         { case (ctx: RequestContext, presentationId: UUID, action: RequestPresentationAction) =>
           presentProofController
             .updatePresentation(presentationId, action)(ctx)
-            .provideSomeLayer(ZLayer.succeed(entity.walletAccessContext))
+            .provideSomeLayer(ZLayer.succeed(wac))
         }
       }
 
