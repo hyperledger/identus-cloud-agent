e753147a0de53ec012b49930ea1695eb1d62b6d1
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2023-10-16 14:33:30 +0700

    feat: add real and client creation to the tests
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- shared-test/src/main/scala/io/iohk/atala/sharedtest/containers/KeycloakTestContainerSupport.scala
+++ shared-test/src/main/scala/io/iohk/atala/sharedtest/containers/KeycloakTestContainerSupport.scala
@@ -1,5 +1,6 @@
 package io.iohk.atala.sharedtest.containers
 import org.keycloak.admin.client.Keycloak
+import org.keycloak.representations.idm.{ClientRepresentation, RealmRepresentation}
 import zio.*
 
 type KeycloakAdminClient = Keycloak
@@ -10,4 +11,23 @@ trait KeycloakTestContainerSupport {
 
   protected val keycloakAdminClientLayer: URLayer[KeycloakContainerCustom, KeycloakAdminClient] =
     ZLayer.fromZIO(ZIO.service[KeycloakContainerCustom].map(_.container.getKeycloakAdminClient))
+
+  protected val adminClientZIO = ZIO.service[KeycloakAdminClient]
+
+  protected val realName = "atala-test"
+  protected val realmRepresentation = {
+    val rr = new RealmRepresentation()
+    rr.setRealm(realName)
+    rr
+  }
+
+  protected val agentClientSecret = "prism-agent-demo-secret"
+  protected val agentClientRepresentation: ClientRepresentation = {
+    val acr = new ClientRepresentation()
+    acr.setClientId("prism-agent")
+    acr.setDirectAccessGrantsEnabled(true)
+    acr.setServiceAccountsEnabled(true)
+    acr.setSecret(agentClientSecret)
+    acr
+  }
 }
--- shared-test/src/test/scala/io/iohk/atala/sharedtest/containers/KeycloakTestContainerSupportSpec.scala
+++ shared-test/src/test/scala/io/iohk/atala/sharedtest/containers/KeycloakTestContainerSupportSpec.scala
@@ -1,8 +1,9 @@
 package io.iohk.atala.sharedtest.containers
 
-import zio.test.TestAspect.*
+import zio.ZIO
 import zio.test.*
-import zio.{Scope, ZIO}
+import zio.test.TestAspect.*
+import scala.jdk.CollectionConverters._
 
 import scala.util.Try
 object KeycloakTestContainerSupportSpec extends ZIOSpecDefault with KeycloakTestContainerSupport {
@@ -13,11 +14,30 @@ object KeycloakTestContainerSupportSpec extends ZIOSpecDefault with KeycloakTest
         keycloakContainer <- ZIO.service[KeycloakContainerCustom]
       } yield assertTrue(keycloakContainer.container.isRunning)
     },
-    test("Keycloak admin-client works") {
+    test("Keycloak admin-client is initialized") {
       for {
-        adminClient <- ZIO.service[KeycloakAdminClient]
+        adminClient <- adminClientZIO
         usersCount <- ZIO.fromTry(Try(adminClient.realm("master").users().count()))
       } yield assertCompletes
+    },
+    test("`atala-test` realm is created") {
+      for {
+        adminClient <- adminClientZIO
+        _ = adminClient.realms().create(realmRepresentation)
+        realmCreated = adminClient.realms().findAll().asScala.exists(_.getRealm == realmRepresentation.getRealm)
+      } yield assertTrue(realmCreated)
+    },
+    test("The Agent client is created") {
+      for {
+        adminClient <- adminClientZIO
+        _ = adminClient.realm(realmRepresentation.getRealm).clients().create(agentClientRepresentation)
+        clientCreated = adminClient
+          .realm(realmRepresentation.getRealm)
+          .clients()
+          .findAll()
+          .asScala
+          .exists(_.getClientId == agentClientRepresentation.getClientId)
+      } yield assertTrue(clientCreated)
     }
   ).provideLayerShared(keycloakContainerLayer >+> keycloakAdminClientLayer) @@ sequential
 }
