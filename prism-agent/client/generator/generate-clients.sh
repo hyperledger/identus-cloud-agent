9cfccb4210a5b1106dac6cc5307e2c309d09d295
Allain Magyar <allain.magyar@iohk.io>
2023-10-24 07:25:17 -0300

    ci: fix client generation (#766)
    Signed-off-by: Allain Magyar <allain.magyar@iohk.io>
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
    Co-authored-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- prism-agent/client/generator/generate-clients.sh
+++ prism-agent/client/generator/generate-clients.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
+# open api cli generator is not compatible with 3.1.0
+yq e -i '.openapi = "3.0.3"' ../../service/api/http/prism-agent-openapi-spec.yaml
+
 # generate kotlin models
 yarn openapi-generator-cli generate \
     -g kotlin \
     -i ../../service/api/http/prism-agent-openapi-spec.yaml \
     -o ../kotlin \
     --ignore-file-override ../kotlin/.openapi-generator-ignore \
-    --additional-properties=packageName=io.iohk.atala.prism,serializationLibrary=gson
+    --additional-properties=packageName=io.iohk.atala.prism,serializationLibrary=gson,enumPropertyNaming=UPPERCASE
 
 # generate typescript models
 yarn openapi-generator-cli generate \
