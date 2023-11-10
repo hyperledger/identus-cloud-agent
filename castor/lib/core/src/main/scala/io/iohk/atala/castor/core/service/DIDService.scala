b99a73718a06f4b97d933ba2e3220593f8d4e825
patlo-iog <pat.losoponkul@iohk.io>
2023-11-10 15:33:32 +0700

    fix(prism-agent): more descriptive error response for validateDID in issue flow (#783)
    Signed-off-by: Pat Losoponkul <pat.losoponkul@iohk.io>

--- castor/lib/core/src/main/scala/io/iohk/atala/castor/core/service/DIDService.scala
+++ castor/lib/core/src/main/scala/io/iohk/atala/castor/core/service/DIDService.scala
@@ -53,6 +53,7 @@ private class DIDServiceImpl(didOpValidator: DIDOperationValidator, nodeClient: 
         .mapError(DIDOperationError.ValidationError.apply)
       operationOutput <- ZIO
         .fromFuture(_ => nodeClient.scheduleOperations(operationRequest))
+        .logError("Error scheduling Node operation")
         .mapBoth(DIDOperationError.DLTProxyError.apply, _.outputs.toList)
         .map {
           case output :: Nil => Right(output)
@@ -81,6 +82,7 @@ private class DIDServiceImpl(didOpValidator: DIDOperationValidator, nodeClient: 
     for {
       result <- ZIO
         .fromFuture(_ => nodeClient.getOperationInfo(node_api.GetOperationInfoRequest(operationId.toProto)))
+        .logError("Error getting Node operation information")
         .mapError(DIDOperationError.DLTProxyError.apply)
       detail <- ZIO
         .fromEither(result.toDomain)
@@ -98,6 +100,7 @@ private class DIDServiceImpl(didOpValidator: DIDOperationValidator, nodeClient: 
       }
       result <- ZIO
         .fromFuture(_ => nodeClient.getDidDocument(request))
+        .logError("Error resolving DID document from Node")
         .mapError(DIDResolutionError.DLTProxyError.apply)
       publishedDidData <- ZIO
         .fromOption(result.document)
