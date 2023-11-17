9e7501c3e910d4114a61ba6ae6039dc185e5c8e4
Anton Baliasnikov <anton.baliasnikov@iohk.io>
2023-11-17 15:17:01 +0000

    test: add configurations to integration tests
    Signed-off-by: Anton Baliasnikov <anton.baliasnikov@iohk.io>

--- tests/integration-tests/src/test/kotlin/config/Config.kt
+++ tests/integration-tests/src/test/kotlin/config/Config.kt
@@ -2,8 +2,10 @@ package config
 
 data class Config(
     val global: GlobalConf,
+    val admin: AgentConf,
     val issuer: AgentConf,
     val holder: AgentConf,
     val verifier: AgentConf,
-    val admin: AgentConf
+    val agents: List<AgentInitConf>,
+    val services: ServicesConf
 )
