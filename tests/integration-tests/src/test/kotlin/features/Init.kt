da3212150c3a3256a6b533506fd38f39005f9fa5
Anton Baliasnikov <Anton.baliasnikov@iohk.io>
2023-11-24 10:34:23 +0000

    test: improve configuration for integration tests (#796)
    Signed-off-by: Anton Baliasnikov <anton.baliasnikov@iohk.io>
    Signed-off-by: Shota Jolbordi <shota.jolbordi@iohk.io>

---
+++ tests/integration-tests/src/test/kotlin/features/Init.kt
@@ -0,0 +1,120 @@
+package features
+
+import com.sksamuel.hoplite.ConfigException
+import com.sksamuel.hoplite.ConfigLoader
+import common.ListenToEvents
+import common.TestConstants
+import config.Config
+import interactions.Post
+import io.cucumber.java.AfterAll
+import io.cucumber.java.BeforeAll
+import io.iohk.atala.prism.models.CreateWalletRequest
+import io.iohk.atala.prism.models.CreateWebhookNotification
+import net.serenitybdd.screenplay.Actor
+import net.serenitybdd.screenplay.actors.Cast
+import net.serenitybdd.screenplay.actors.OnStage
+import net.serenitybdd.screenplay.rest.abilities.CallAnApi
+import org.apache.http.HttpStatus
+import java.util.*
+
+val config = ConfigLoader().loadConfigOrThrow<Config>(TestConstants.TESTS_CONFIG)
+
+/**
+ * This function starts all services and actors before all tests.
+ */
+fun initServices() {
+    config.services?.keycloak?.start(
+        config.roles.filter { it.name != "Admin" }.map { it.name }
+    )
+    config.services?.prismNode?.start()
+    config.services?.vault?.start()
+    config.agents?.forEach { agent ->
+        agent.start()
+    }
+}
+
+/**
+ * This function initializes all actors and sets the stage.
+ */
+fun initActors() {
+    /**
+     * This function initializes a wallet for an actor when Keycloak is used.
+     *
+     * @param actor The actor for which the wallet should be initialized.
+     */
+    fun initializeWallet(actor: Actor) {
+        Post("")
+            .specWithAuthHeaders(actor)
+            .body(
+                CreateWalletRequest(
+                    name = UUID.randomUUID().toString()
+                )
+            )
+            .post("/wallets")
+            .then().statusCode(HttpStatus.SC_CREATED)
+    }
+
+    /**
+     * This function registers a webhook for an actor.
+     *
+     * @param actor The actor for which the webhook should be registered.
+     * @param webhookUrl The url of the webhook.
+     */
+    fun registerWebhook(actor: Actor, webhookUrl: String) {
+        Post("")
+            .specWithAuthHeaders(actor)
+            .body(CreateWebhookNotification(url = webhookUrl))
+            .post("/events/webhooks")
+            .then().statusCode(HttpStatus.SC_OK)
+    }
+
+    val cast = Cast()
+    config.roles.forEach { role ->
+        cast.actorNamed(
+            role.name,
+            CallAnApi.at(role.url.toExternalForm())
+        )
+    }
+    if (config.services?.keycloak != null) {
+        cast.actors.filter { it.name != "Admin" }.forEach { actor ->
+            try {
+                actor.remember("BEARER_TOKEN", config.services.keycloak.getKeycloakAuthToken(actor.name, actor.name))
+            } catch (e: NullPointerException) {
+                throw ConfigException("Keycloak is configured, but no token found for user ${actor.name}!")
+            }
+            initializeWallet(actor)
+        }
+    }
+    config.roles.forEach { role ->
+        println(role.name)
+        val actor = cast.actorNamed(role.name)
+        if (role.apikey != null) {
+            actor.remember("AUTH_KEY", role.apikey)
+            actor.remember("AUTH_HEADER", role.authHeader)
+        }
+        if (role.webhook != null) {
+            actor.whoCan(ListenToEvents.at(role.webhook.url))
+            if (role.webhook.initRequired) {
+                registerWebhook(actor, role.webhook.url.toExternalForm())
+            }
+        }
+    }
+    OnStage.setTheStage(cast)
+}
+
+@BeforeAll
+fun init() {
+    initServices()
+    initActors()
+}
+
+@AfterAll
+fun clearStage() {
+    OnStage.drawTheCurtain()
+    config.agents?.forEach { agent ->
+        agent.stop()
+    }
+    config.services?.keycloak?.stop()
+    config.services?.prismNode?.stop()
+    config.services?.vault?.stop()
+}
