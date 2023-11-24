45c4da2c157badcf76f5b173bddd3448c9d12e49
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-11-24 10:11:50 +0100

    feat(pollux): add VC status list 2021 generation support
    Signed-off-by: Benjamin Voiturier <benjamin.voiturier@iohk.io>

---
+++ pollux/lib/vc-jwt/src/main/scala/io/iohk/atala/pollux/vc/jwt/revocation/VCStatusList2021.scala
@@ -0,0 +1,48 @@
+package io.iohk.atala.pollux.vc.jwt.revocation
+
+import io.circe.syntax.*
+import io.circe.{Json, JsonObject}
+import io.iohk.atala.pollux.vc.jwt.revocation.BitStringError.EncodingError
+import io.iohk.atala.pollux.vc.jwt.{Issuer, JWT, W3CCredential, W3cCredentialPayload}
+import zio.IO
+
+import java.time.Instant
+
+object VCStatusList2021 {
+
+  def generateRevocationVC(
+      vcId: String,
+      claimsId: String,
+      jwtIssuer: Issuer,
+      revocationData: BitString
+  ): IO[EncodingError, JWT] = {
+    for {
+      encodedBitString <- revocationData.encoded
+    } yield {
+      val claims = JsonObject()
+        .add("id", claimsId.asJson)
+        .add("type", "StatusList2021".asJson)
+        .add("statusPurpose", "revocation".asJson)
+        .add("encodedList", encodedBitString.asJson)
+      val w3Credential = W3cCredentialPayload(
+        `@context` = Set(
+          "https://www.w3.org/2018/credentials/v1",
+          "https://w3id.org/vc/status-list/2021/v1"
+        ),
+        maybeId = Some(vcId),
+        `type` = Set("VerifiableCredential", "StatusList2021Credential"),
+        issuer = jwtIssuer.did,
+        issuanceDate = Instant.now,
+        maybeExpirationDate = None,
+        maybeCredentialSchema = None,
+        credentialSubject = claims.asJson,
+        maybeCredentialStatus = None,
+        maybeRefreshService = None,
+        maybeEvidence = None,
+        maybeTermsOfUse = None
+      )
+      W3CCredential.toEncodedJwt(w3Credential, jwtIssuer)
+    }
+  }
+
+}
