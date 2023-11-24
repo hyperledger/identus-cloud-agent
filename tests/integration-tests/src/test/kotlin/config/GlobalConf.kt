8b3e9601fef02263be46ca08c607cb43b8dc8a59
Anton Baliasnikov <Anton.baliasnikov@iohk.io>
2023-11-24 10:34:23 +0000

    test: improve configuration for integration tests (#796)
    Signed-off-by: Anton Baliasnikov <anton.baliasnikov@iohk.io>
    Signed-off-by: Shota Jolbordi <shota.jolbordi@iohk.io>

--- tests/integration-tests/src/test/kotlin/config/GlobalConf.kt
+++
@@ -1,8 +0,0 @@
-package config
-
-import com.sksamuel.hoplite.ConfigAlias
-
-data class GlobalConf(
-    @ConfigAlias("auth_header") val authHeader: String = "apikey",
-    @ConfigAlias("admin_auth_header") val adminAuthHeader: String = "x-admin-api-key",
-)
