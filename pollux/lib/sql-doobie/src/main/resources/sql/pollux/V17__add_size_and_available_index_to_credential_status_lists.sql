8b1f0c1a4a373b1a6b3e4ed790c7724b331942e8
Shota Jolbordi <shota.jolbordi@iohk.io>
2023-11-27 20:23:10 +0400

    fix migration
    Signed-off-by: Shota Jolbordi <shota.jolbordi@iohk.io>

--- pollux/lib/sql-doobie/src/main/resources/sql/pollux/V17__add_size_and_available_index_to_credential_status_lists.sql
+++ pollux/lib/sql-doobie/src/main/resources/sql/pollux/V17__add_size_and_available_index_to_credential_status_lists.sql
@@ -4,10 +4,10 @@ ALTER TABLE public.credential_status_lists
     ADD COLUMN size            INTEGER NOT NULL DEFAULT 131072,
     ADD COLUMN last_used_index INTEGER NOT NULL DEFAULT 0;
 
--- Rename the column 'encoded_list' to 'encoded_list_credential'
+-- Rename the column 'encoded_list' to 'status_list_jwt_credential'
 ALTER TABLE public.credential_status_lists
-    RENAME COLUMN encoded_list TO status_list_credential;
+    RENAME COLUMN encoded_list TO status_list_jwt_credential;
 
 -- Change the data type of the 'encoded_list_credential' column to JSON
 ALTER TABLE public.credential_status_lists
-    ALTER COLUMN status_list_credential TYPE JSON USING status_list_credential::JSON;
+    DROP COLUMN proof
