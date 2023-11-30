c49c8136451ae9d0de30c19a5724b4ec1b17ad62
Anton Baliasnikov <Anton.baliasnikov@iohk.io>
2023-11-30 08:27:14 +0000

    test: enable remote envs testing in integration tests (#801)
    Signed-off-by: Anton Baliasnikov <anton.baliasnikov@iohk.io>

--- tests/integration-tests/src/test/kotlin/config/Webhook.kt
+++ tests/integration-tests/src/test/kotlin/config/Webhook.kt
@@ -5,5 +5,6 @@ import java.net.URL
 
 data class Webhook(
     val url: URL,
+    @ConfigAlias("local_port") val localPort: Int? = null,
     @ConfigAlias("init_required") val initRequired: Boolean = true
 )
