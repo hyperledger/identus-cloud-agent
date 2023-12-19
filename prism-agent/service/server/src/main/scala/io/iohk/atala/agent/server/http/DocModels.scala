2580cc82260ad39649adafa3a365e0569b09cf79
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-12-19 13:48:20 +0100

    docs(prism-agent): improve 'connections management' OpenAPI documentation
    Signed-off-by: Benjamin Voiturier <benjamin.voiturier@iohk.io>

--- prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/DocModels.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/DocModels.scala
@@ -1,5 +1,6 @@
 package io.iohk.atala.agent.server.http
 
+import io.iohk.atala.connect.controller.ConnectionEndpoints
 import sttp.apispec.openapi.*
 import sttp.apispec.{SecurityScheme, Tag}
 import sttp.model.headers.AuthenticationScheme
@@ -96,6 +97,27 @@ object DocModels {
           "apiKeyAuth" -> Vector.empty[String],
           "adminApiKeyAuth" -> Vector.empty[String],
           "jwtAuth" -> Vector.empty[String]
+        )
+      )
+      .tags(
+        List(
+          Tag(
+            ConnectionEndpoints.TAG,
+            Some(
+              s"""
+                 |The '${ConnectionEndpoints.TAG}' endpoints facilitate the initiation of connection flows between the current agent and peer agents, regardless of whether they reside in cloud or edge environments.
+                 |<br>
+                 |This implementation adheres to the DIDComm Messaging v2.0 - [Out of Band Messages](https://identity.foundation/didcomm-messaging/spec/v2.0/#out-of-band-messages) specification [section 9.5.4](https://identity.foundation/didcomm-messaging/spec/v2.0/#invitation) - to generate invitations.
+                 |The <b>from</b> field of the out-of-band invitation message contains a freshly generated Peer DID that complies with the [did:peer:2](https://identity.foundation/peer-did-method-spec/#generating-a-didpeer2) specification.
+                 |This Peer DID includes the 'uri' location of the DIDComm messaging service, essential for the invitee's subsequent execution of the connection flow.
+                 |<br>
+                 |Upon accepting an invitation, the invitee sends a connection request to the inviter's DIDComm messaging service endpoint.
+                 |The connection request's 'type' attribute must be specified as "https://atalaprism.io/mercury/connections/1.0/request".
+                 |The inviter agent responds with a connection response message, indicated by a 'type' attribute of "https://atalaprism.io/mercury/connections/1.0/response".
+                 |Both request and response types are proprietary to the Open Enterprise Agent ecosystem.
+                 |""".stripMargin
+            )
+          )
         )
       )
 
