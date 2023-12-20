71ba7169b7ce142652ed7e61c4b419d608e78af4
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-12-20 11:25:55 +0100

    fix(pollux): add error handling implementation in HttpURIDereferencerImpl
    Signed-off-by: Benjamin Voiturier <benjamin.voiturier@iohk.io>

--- pollux/lib/core/src/main/scala/io/iohk/atala/pollux/core/service/HttpURIDereferencerImpl.scala
+++ pollux/lib/core/src/main/scala/io/iohk/atala/pollux/core/service/HttpURIDereferencerImpl.scala
@@ -1,28 +1,33 @@
 package io.iohk.atala.pollux.core.service
 
 import io.iohk.atala.pollux.core.service.URIDereferencerError.{ConnectionError, ResourceNotFound, UnexpectedError}
-import zio.http.*
 import zio.*
+import zio.http.*
+import zio.stream.ZSink
 
 import java.net.URI
+import java.nio.charset.StandardCharsets
 
 class HttpURIDereferencerImpl(client: Client) extends URIDereferencer {
 
   override def dereference(uri: URI): IO[URIDereferencerError, String] = {
     val result = for {
-      response <- Client.request(Request(url = URL(path = Path(uri.toString)))).mapError(t => ConnectionError(t.getMessage))
+      url <- ZIO.fromOption(URL.fromURI(uri)).mapError(_ => ConnectionError(s"Invalid connection URI: $uri"))
+      response <- Client.request(Request(url = url)).mapError(t => ConnectionError(t.getMessage))
       body <- response.status match {
         case Status.Ok =>
           response.body.asString.mapError(t => UnexpectedError(t.getMessage))
-        case Status.NotFound if !response.status.isError => ZIO.fail(ResourceNotFound(uri))
-//        case _ if response.status.isError =>
-//          val err = response match {
-//            case Response.GetError(error) => Some(error)
-//            case _                        => None
-//          }
-//          ZIO.fail(UnexpectedError(s"HTTP response error: $err"))
-        case _ =>
-          ZIO.fail(UnexpectedError("Unknown error"))
+        case Status.NotFound =>
+          ZIO.fail(ResourceNotFound(uri))
+        case status if status.isError =>
+          response.body.asStream
+            .take(1024) // Only take the first 1024 bytes from the response body (if any).
+            .runFold(Array.empty[Byte])(_ :+ _)
+            .map(new String(_, StandardCharsets.UTF_8))
+            .orDie
+            .flatMap(errorMessage => ZIO.fail(UnexpectedError(s"HTTP response error: $status - $errorMessage")))
+        case status =>
+          ZIO.fail(UnexpectedError(s"Unexpected response status: $status"))
       }
     } yield body
     result.provide(ZLayer.succeed(client) ++ Scope.default)
