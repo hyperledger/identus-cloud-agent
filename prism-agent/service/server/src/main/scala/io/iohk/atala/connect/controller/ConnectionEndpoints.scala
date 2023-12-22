45eac892d16a643bb0d77a3660aaf8c65c4cd8dc
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2023-12-22 13:44:27 +0100

    docs(prism-agent): improve endpoints descriptions
    Signed-off-by: Benjamin Voiturier <benjamin.voiturier@iohk.io>

--- prism-agent/service/server/src/main/scala/io/iohk/atala/connect/controller/ConnectionEndpoints.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/connect/controller/ConnectionEndpoints.scala
@@ -49,14 +49,17 @@ object ConnectionEndpoints {
           )
       )
       .out(jsonBody[Connection])
-      .description("The created connection record.")
+      .description("The newly created connection record.")
       .errorOut(basicFailuresAndForbidden)
       .name("createConnection")
-      .summary("Creates a new connection record and returns an Out of Band invitation.")
+      .summary("Create a new connection invitation that can be delivered out-of-band to a peer agent.")
       .description("""
-         |Generates a new Peer DID and creates an [Out of Band 2.0](https://identity.foundation/didcomm-messaging/spec/v2.0/#out-of-band-messages) invitation.
-         |It returns a new connection record in `InvitationGenerated` state.
-         |The request body may contain a `label` that can be used as a human readable alias for the connection, for example `{'label': "Bob"}`
+         |Create a new connection invitation that can be delivered out-of-band to a peer agent, regardless of whether it resides in cloud or edge environment.
+         |The generated invitation adheres to the DIDComm Messaging v2.0 - [Out of Band Messages](https://identity.foundation/didcomm-messaging/spec/v2.0/#out-of-band-messages) specification [section 9.5.4](https://identity.foundation/didcomm-messaging/spec/v2.0/#invitation).
+         |The <b>from</b> field of the out-of-band invitation message contains a freshly generated Peer DID that complies with the [did:peer:2](https://identity.foundation/peer-did-method-spec/#generating-a-didpeer2) specification.
+         |This Peer DID includes the 'uri' location of the DIDComm messaging service, essential for the invitee's subsequent execution of the connection flow.
+         |In the agent database, the created connection record has an initial state set to `InvitationGenerated`.
+         |The request body may contain a `label` that can be used as a human readable alias for the connection, for example `{'label': "Connection with Bob"}`
          |""".stripMargin)
       .tag(TAG)
 
@@ -68,14 +71,20 @@ object ConnectionEndpoints {
       .in(extractFromRequest[RequestContext](RequestContext.apply))
       .in(
         "connections" / path[UUID]("connectionId").description(
-          "The unique identifier of the connection record."
+          "The `connectionId` uniquely identifying the connection flow record."
         )
       )
-      .out(jsonBody[Connection].description("The connection record."))
+      .out(jsonBody[Connection].description("The specific connection flow record."))
       .errorOut(basicFailureAndNotFoundAndForbidden)
       .name("getConnection")
-      .summary("Gets an existing connection record by its unique identifier.")
-      .description("Gets an existing connection record by its unique identifier")
+      .summary(
+        "Retrieves a specific connection flow record from the agent's database based on its unique `connectionId`."
+      )
+      .description("""
+          |Retrieve a specific connection flow record from the agent's database based in its unique `connectionId`.
+          |The API returns a comprehensive collection of connection flow records within the system, regardless of their state.
+          |The returned connection item includes essential metadata such as connection ID, thread ID, state, role, participant information, and other relevant details.
+          |""".stripMargin)
       .tag(TAG)
 
   val getConnections: Endpoint[
@@ -91,12 +100,23 @@ object ConnectionEndpoints {
       .in(extractFromRequest[RequestContext](RequestContext.apply))
       .in("connections")
       .in(paginationInput)
-      .in(query[Option[String]]("thid").description("The thid of a DIDComm communication."))
-      .out(jsonBody[ConnectionsPage].description("The list of connection records."))
+      .in(
+        query[Option[String]]("thid").description(
+          "The `thid`, shared between the inviter and the invitee, that uniquely identifies a connection flow."
+        )
+      )
+      .out(
+        jsonBody[ConnectionsPage].description("The list of connection flow records available from the agent's database")
+      )
       .errorOut(basicFailuresAndForbidden)
       .name("getConnections")
-      .summary("Gets the list of connection records.")
-      .description("Get the list of connection records paginated")
+      .summary("Retrieves the list of connection flow records available from the agent's database.")
+      .description("""
+          |Retrieve of a list containing connections available from the agent's database.
+          |The API returns a comprehensive collection of connection flow records within the system, regardless of their state.
+          |Each connection item includes essential metadata such as connection ID, thread ID, state, role, participant information, and other relevant details.
+          |Pagination support is available, allowing for efficient handling of large datasets.
+          |""".stripMargin)
       .tag(TAG)
 
   val acceptConnectionInvitation: Endpoint[
@@ -123,14 +143,16 @@ object ConnectionEndpoints {
           )
       )
       .out(jsonBody[Connection])
-      .description("The created connection record.")
+      .description("The newly connection record.")
       .errorOut(basicFailuresAndForbidden)
       .name("acceptConnectionInvitation")
-      .summary("Accepts an Out of Band invitation.")
+      .summary("Accept a new connection invitation received out-of-band from another peer agent.")
       .description("""
-          |Accepts an [Out of Band 2.0](https://identity.foundation/didcomm-messaging/spec/v2.0/#out-of-band-messages) invitation, generates a new Peer DID,
-          |and submits a Connection Request to the inviter.
-          |It returns a connection object in `ConnectionRequestPending` state, until the Connection Request is eventually sent to the inviter by the prism-agent's background process. The connection object state will then automatically move to `ConnectionRequestSent`.
+          |Accept an new connection invitation received out-of-band from another peer agent.
+          |The invitation must be compliant with the DIDComm Messaging v2.0 - [Out of Band Messages](https://identity.foundation/didcomm-messaging/spec/v2.0/#out-of-band-messages) specification [section 9.5.4](https://identity.foundation/didcomm-messaging/spec/v2.0/#invitation).
+          |A new connection record with state `ConnectionRequestPending` will be created in the agent database and later processed by a background job to send a connection request to the peer agent.
+          |The created record will contain a newly generated pairwise Peer DID used for that connection.
+          |A connection request will then be sent to the peer agent to actually establish the connection, moving the record state to `ConnectionRequestSent`, and waiting the connection response from the peer agent.
           |""".stripMargin)
       .tag(TAG)
 
