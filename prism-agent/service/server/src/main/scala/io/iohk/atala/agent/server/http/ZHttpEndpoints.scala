b48fe3ccfd1917d07f763a47666347be3afcc8e8
Yurii Shynbuiev - IOHK <yurii.shynbuiev@iohk.io>
2024-01-10 16:05:44 +0700

    docs(prism-agent): add verification policy OpenAPI description (#835)
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/ZHttpEndpoints.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/agent/server/http/ZHttpEndpoints.scala
@@ -1,15 +1,12 @@
 package io.iohk.atala.agent.server.http
 
-import sttp.apispec.SecurityScheme
-import sttp.apispec.openapi.{OpenAPI, Server}
-import sttp.model.headers.AuthenticationScheme
+import io.iohk.atala.agent.server.buildinfo.BuildInfo
+import sttp.apispec.openapi.OpenAPI
 import sttp.tapir.redoc.RedocUIOptions
 import sttp.tapir.redoc.bundle.RedocInterpreter
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.swagger.SwaggerUIOptions
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
-
-import scala.collection.immutable.ListMap
 
 object ZHttpEndpoints {
 
@@ -21,11 +18,11 @@ object ZHttpEndpoints {
 
   def swaggerEndpoints[F[_]](apiEndpoints: List[ServerEndpoint[Any, F]]): List[ServerEndpoint[Any, F]] =
     SwaggerInterpreter(swaggerUIOptions = swaggerUIOptions, customiseDocsModel = DocModels.customiseDocsModel)
-      .fromServerEndpoints[F](apiEndpoints, "Prism Agent", "1.0.0")
+      .fromServerEndpoints[F](apiEndpoints, "Prism Agent", BuildInfo.version)
 
   def redocEndpoints[F[_]](apiEndpoints: List[ServerEndpoint[Any, F]]): List[ServerEndpoint[Any, F]] =
     RedocInterpreter(redocUIOptions = redocUIOptions, customiseDocsModel = DocModels.customiseDocsModel)
-      .fromServerEndpoints[F](apiEndpoints, "Prism Agent", "1.0.0")
+      .fromServerEndpoints[F](apiEndpoints, "Prism Agent", BuildInfo.version)
 
   def withDocumentations[F[_]](apiEndpoints: List[ServerEndpoint[Any, F]]): List[ServerEndpoint[Any, F]] = {
     apiEndpoints ++ swaggerEndpoints[F](apiEndpoints) ++ redocEndpoints[F](apiEndpoints)
--- prism-agent/service/server/src/main/scala/io/iohk/atala/pollux/credentialschema/controller/VerificationPolicyPageRequestLogic.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/pollux/credentialschema/controller/VerificationPolicyPageRequestLogic.scala
@@ -2,28 +2,28 @@ package io.iohk.atala.pollux.credentialschema.controller
 
 import io.iohk.atala.api.http.RequestContext
 import io.iohk.atala.api.http.model.{CollectionStats, Pagination}
-import io.iohk.atala.pollux.credentialschema.http.{VerificationPolicy, VerificationPolicyPage}
+import io.iohk.atala.pollux.credentialschema.http.{VerificationPolicyResponse, VerificationPolicyResponsePage}
 import sttp.model.Uri
 import io.iohk.atala.api.util.PaginationUtils
 
 case class VerificationPolicyPageRequestLogic(
     ctx: RequestContext,
     pagination: Pagination,
-    items: List[VerificationPolicy],
+    items: List[VerificationPolicyResponse],
     stats: CollectionStats
 ) {
   def composeNextUri(uri: Uri): Option[Uri] = PaginationUtils.composeNextUri(uri, items, pagination, stats)
 
   def composePreviousUri(uri: Uri): Option[Uri] = PaginationUtils.composePreviousUri(uri, items, pagination, stats)
 
-  def result: VerificationPolicyPage = {
+  def result: VerificationPolicyResponsePage = {
     val self = ctx.request.uri.toString
     val pageOf = ctx.request.uri.copy(querySegments = Seq.empty).toString
     val next = composeNextUri(ctx.request.uri).map(_.toString)
     val previous = composePreviousUri(ctx.request.uri).map(_.toString)
     val baseUri = ctx.request.uri.copy(querySegments = Seq.empty)
 
-    val pageResult = VerificationPolicyPage(
+    val pageResult = VerificationPolicyResponsePage(
       self = self,
       kind = "VerificationPolicyPage",
       pageOf = pageOf,
