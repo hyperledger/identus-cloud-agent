c41d7deb2d94856dd6a9126d3f1086243b46853e
shotexa <shota.jolbordi@iohk.io>
2024-02-21 00:17:26 +0400

    feat(pollux): add endpoint to revoke a credential (#899)
    Signed-off-by: Shota Jolbordi <shota.jolbordi@iohk.io>

--- prism-agent/service/server/src/main/scala/io/iohk/atala/credentialstatus/controller/CredentialStatusEndpoints.scala
+++ prism-agent/service/server/src/main/scala/io/iohk/atala/credentialstatus/controller/CredentialStatusEndpoints.scala
@@ -5,8 +5,13 @@ import io.iohk.atala.api.http.EndpointOutputs.*
 import io.iohk.atala.credentialstatus.controller.http.StatusListCredential
 import sttp.tapir.*
 import sttp.tapir.json.zio.jsonBody
+import io.iohk.atala.iam.authentication.apikey.ApiKeyEndpointSecurityLogic.apiKeyHeader
+import io.iohk.atala.iam.authentication.oidc.JwtSecurityLogic.jwtAuthHeader
 import java.util.UUID
-
+import io.iohk.atala.pollux.core.model.DidCommID
+import io.iohk.atala.api.http.codec.DidCommIDCodec.given
+import io.iohk.atala.iam.authentication.apikey.ApiKeyCredentials
+import io.iohk.atala.iam.authentication.oidc.JwtCredentials
 object CredentialStatusEndpoints {
 
   val getCredentialStatusListEndpoint: PublicEndpoint[
@@ -29,5 +34,25 @@ object CredentialStatusEndpoints {
       .description(
         "Fetch credential status list by its ID"
       )
+      .tag("Credential status list")
+
+  val revokeCredentialByIdEndpoint: Endpoint[
+    (ApiKeyCredentials, JwtCredentials),
+    (RequestContext, DidCommID),
+    ErrorResponse,
+    Unit,
+    Any
+  ] =
+    endpoint.patch
+      .securityIn(apiKeyHeader)
+      .securityIn(jwtAuthHeader)
+      .in(extractFromRequest[RequestContext](RequestContext.apply))
+      .in(
+        "credential-status" / "revoke-credential" / path[DidCommID]("id").description("Revoke a credential by its ID")
+      )
+      .out(statusCode(sttp.model.StatusCode.Ok))
+      .errorOut(basicFailuresAndNotFound)
+      .summary("Revoke a credential by its ID")
+      .description("Marks credential to be ready for revocation, it will be revoked automatically")
       .tag("Credential status list")
 }
