e6829c7e0d869c7ef2ca285259965672f43d9c7b
mineme0110 <shailesh.patil@iohk.io>
2024-04-24 15:48:45 +0100

    minor cleanup
    Signed-off-by: mineme0110 <shailesh.patil@iohk.io>

--- infrastructure/dev/get-versions.sh
+++ infrastructure/dev/get-versions.sh
@@ -5,8 +5,8 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 # Set working directory
 cd ${SCRIPT_DIR}
 
-export PRISM_AGENT_VERSION=$(cd ../../ && sbt "project agent" -Dsbt.supershell=false -error "print version")
-echo "prism-agent server version: ${PRISM_AGENT_VERSION}"
+export AGENT_VERSION=$(cd ../../ && sbt "project agent" -Dsbt.supershell=false -error "print version")
+echo "prism-agent server version: ${AGENT_VERSION}"
 
 export PRISM_NODE_VERSION=v2.1.1
 echo "prism node version: ${PRISM_NODE_VERSION}"
