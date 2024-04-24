789c2d513876cc2e9a0f8fcdfe84b6aee7b1479e
mineme0110 <shailesh.patil@iohk.io>
2024-04-24 15:34:26 +0100

    minor cleanup
    Signed-off-by: mineme0110 <shailesh.patil@iohk.io>

--- infrastructure/local/update_env.sh
+++ infrastructure/local/update_env.sh
@@ -5,6 +5,6 @@ ENV_FILE="${SCRIPT_DIR}/.env"
 
 pip install ${SCRIPT_DIR}/../utils/python/github-helpers > /dev/null 2>&1
 
-PRISM_AGENT_VERSION=$(github get-latest-package-version --package prism-agent --package-type container)
+AGENT_VERSION=$(github get-latest-package-version --package prism-agent --package-type container)
 
-sed -i.bak "s/PRISM_AGENT_VERSION=.*/PRISM_AGENT_VERSION=${PRISM_AGENT_VERSION}/" ${ENV_FILE} && rm -f ${ENV_FILE}.bak
+sed -i.bak "s/AGENT_VERSION=.*/AGENT_VERSION=${AGENT_VERSION}/" ${ENV_FILE} && rm -f ${ENV_FILE}.bak
