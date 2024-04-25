18eb8ab9ccf6ec7b1917d6ac32ba228dfe51b3c9
mineme0110 <shailesh.patil@iohk.io>
2024-04-25 20:02:11 +0100

    yaml lint fix for javascript
    Signed-off-by: mineme0110 <shailesh.patil@iohk.io>

--- tests/performance-tests/agent-performance-tests-k6/webpack.config.js
+++ tests/performance-tests/agent-performance-tests-k6/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     ],
   },
   target: 'web',
-  externals: /^(k6|https?\:\/\/)(\/.*)?/,
+  externals: /^(k6|https?:\/\/)(\/.*)?/,
   // Generate map files for compiled scripts
   devtool: "source-map",
   stats: {
