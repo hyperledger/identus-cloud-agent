9e9a1221461e47e8d2bedc06b67bf56ddf2ee19d
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2024-04-29 18:45:46 +0700

    ci: fix the dependency in e2e tests and python package
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- tests/integration-tests/build.gradle.kts
+++ tests/integration-tests/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     testImplementation("io.ktor:ktor-server-netty:2.3.0")
     testImplementation("io.ktor:ktor-client-apache:2.3.0")
     // RestAPI client
-    testImplementation("org.hyperledger.identus:identus-kotlin-client:1.32.0")
+    testImplementation("org.hyperledger.identus:cloud-agent-client-kotlin:1.32.0")
     // Test helpers library
     testImplementation("io.iohk.atala:atala-automation:0.3.2")
     // Hoplite for configuration
