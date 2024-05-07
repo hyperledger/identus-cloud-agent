6ff710ab1f4fd2a182f1479261f9ae0a842437b8
Yurii Shynbuiev - IOHK <yurii.shynbuiev@iohk.io>
2024-05-07 19:52:27 +0700

    chore: update identus-client-kotlin dependency
    Signed-off-by: Yurii Shynbuiev - IOHK <yurii.shynbuiev@iohk.io>

--- tests/integration-tests/build.gradle.kts
+++ tests/integration-tests/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     testImplementation("io.ktor:ktor-server-netty:2.3.0")
     testImplementation("io.ktor:ktor-client-apache:2.3.0")
     // RestAPI client
-    testImplementation("org.hyperledger.identus:cloud-agent-client-kotlin:1.32.0")
+    testImplementation("org.hyperledger.identus:cloud-agent-client-kotlin:1.32.1")
     // Test helpers library
     testImplementation("io.iohk.atala:atala-automation:0.4.0")
     // Hoplite for configuration
