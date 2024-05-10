3e12e98ff1fa43224f21dd455382ff5a622878b5
patlo-iog <pat.losoponkul@iohk.io>
2024-05-10 17:03:12 +0700

    test: add tests for oid4vci credential configuration CRUD (#1027)
    Signed-off-by: Pat Losoponkul <pat.losoponkul@iohk.io>

--- pollux/core/src/main/scala/org/hyperledger/identus/pollux/core/model/oid4vci/CredentialIssuer.scala
+++ pollux/core/src/main/scala/org/hyperledger/identus/pollux/core/model/oid4vci/CredentialIssuer.scala
@@ -3,12 +3,18 @@ package org.hyperledger.identus.pollux.core.model.oid4vci
 import java.net.URL
 import java.time.Instant
 import java.util.UUID
+import java.time.temporal.ChronoUnit
 
-case class CredentialIssuer(id: UUID, authorizationServer: URL, createdAt: Instant, updatedAt: Instant)
+case class CredentialIssuer(id: UUID, authorizationServer: URL, createdAt: Instant, updatedAt: Instant) {
+  def withTruncatedTimestamp(unit: ChronoUnit = ChronoUnit.MICROS): CredentialIssuer = copy(
+    createdAt = createdAt.truncatedTo(unit),
+    updatedAt = updatedAt.truncatedTo(unit),
+  )
+}
 
 object CredentialIssuer {
   def apply(authorizationServer: URL): CredentialIssuer = {
     val now = Instant.now
-    CredentialIssuer(UUID.randomUUID(), authorizationServer, now, now)
+    CredentialIssuer(UUID.randomUUID(), authorizationServer, now, now).withTruncatedTimestamp()
   }
 }
