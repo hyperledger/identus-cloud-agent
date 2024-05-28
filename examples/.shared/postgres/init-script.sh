c50fb397855f93798888b4a067702a0496eaf61b
Pat Losoponkul <pat.losoponkul@iohk.io>
2024-05-28 13:54:35 +0700

    chore: fix linter warnings
    Signed-off-by: Pat Losoponkul <pat.losoponkul@iohk.io>

--- examples/.shared/postgres/init-script.sh
+++ examples/.shared/postgres/init-script.sh
@@ -17,8 +17,8 @@ function create_user_and_database() {
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-		create_user_and_database $db
+	for db in $(echo "$POSTGRES_MULTIPLE_DATABASES" | tr ',' ' '); do
+		create_user_and_database "$db"
 	done
 	echo "Multiple databases created"
 fi
