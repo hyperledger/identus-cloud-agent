3b4a98d877fbabf54e727bd851ef057bf60cc701
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2024-05-29 10:46:26 +0200

    feat: create additional DIDComm controller errors
    Signed-off-by: Benjamin Voiturier <benjamin.voiturier@iohk.io>

--- cloud-agent/service/server/src/main/scala/org/hyperledger/identus/didcomm/controller/DIDCommControllerError.scala
+++ cloud-agent/service/server/src/main/scala/org/hyperledger/identus/didcomm/controller/DIDCommControllerError.scala
@@ -1,5 +1,6 @@
 package org.hyperledger.identus.didcomm.controller
 
+import org.hyperledger.identus.mercury.model.DidId
 import org.hyperledger.identus.shared.models.{Failure, StatusCode}
 
 sealed trait DIDCommControllerError(
@@ -22,5 +23,23 @@ object DIDCommControllerError {
       extends DIDCommControllerError(
         statusCode,
         "An unexpected error occurred while processing your request"
+      )
+
+  final case class RequestBodyParsingError(cause: String)
+      extends DIDCommControllerError(
+        StatusCode.BadRequest,
+        s"Unable to parse the request body as a valid DIDComm message: $cause"
+      )
+
+  final case class PeerDIDNotFoundError(did: DidId)
+      extends DIDCommControllerError(
+        StatusCode.UnprocessableContent,
+        s"The Peer DID was not found in this agent: ${did.value}"
+      )
+
+  final case class PeerDIDKeyNotFoundError(did: DidId, keyId: String)
+      extends DIDCommControllerError(
+        StatusCode.UnprocessableContent,
+        s"The Peer DID does not contain the required key: DID=$did, keyId=$keyId"
       )
 }
