8be5e48ed188cacafe775dfb83ced8a143abbe11
Bassam <bassam.riman@iohk.io>
2024-05-31 11:27:28 -0400

    fix: VC Verification API Doc (#1118)
    Signed-off-by: Bassam Riman <bassam.riman@iohk.io>
    Co-authored-by: Yurii Shynbuiev - IOHK <yurii.shynbuiev@iohk.io>
    Signed-off-by: Pat Losoponkul <pat.losoponkul@iohk.io>

--- cloud-agent/service/server/src/main/scala/org/hyperledger/identus/verification/controller/http/VcVerificationResult.scala
+++ cloud-agent/service/server/src/main/scala/org/hyperledger/identus/verification/controller/http/VcVerificationResult.scala
@@ -2,10 +2,20 @@ package org.hyperledger.identus.verification.controller.http
 
 import org.hyperledger.identus.pollux.core.service.verification.VcVerificationResult as ServiceVcVerificationResult
 import sttp.tapir.Schema
+import sttp.tapir.Schema.annotations.description
 import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
 
+/** Represents the result of a verification performed on a verifiable credential.
+  *
+  * @param verification
+  *   The type of verification that was performed.
+  * @param success
+  *   Indicates whether the verification was successful.
+  */
 final case class VcVerificationResult(
+    @description("The type of verification that was performed.")
     verification: VcVerification,
+    @description("Indicates whether the verification was successful.")
     success: Boolean
 )
 object VcVerificationResult {
