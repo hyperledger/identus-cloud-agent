e4d1b2f680a718ebb2104e64cf2da895c84eb9e3
Shota Jolbordi <shota.jolbordi@iohk.io>
2024-06-10 03:37:38 +0400

    Use derived decoder for json web key
    Signed-off-by: Shota Jolbordi <shota.jolbordi@iohk.io>

--- pollux/vc-jwt/src/main/scala/org/hyperledger/identus/pollux/vc/jwt/JsonWebKey.scala
+++ pollux/vc-jwt/src/main/scala/org/hyperledger/identus/pollux/vc/jwt/JsonWebKey.scala
@@ -1,6 +1,7 @@
 package org.hyperledger.identus.pollux.vc.jwt
 
 import io.circe.*
+import io.circe.generic.semiauto.*
 import io.circe.syntax.*
 
 case class JsonWebKey(
@@ -19,54 +20,8 @@ case class JsonWebKey(
 )
 
 object JsonWebKey {
-  given jsonWebKeyEncoder: Encoder[JsonWebKey] =
-    (jsonWebKey: JsonWebKey) =>
-      Json
-        .obj(
-          ("alg", jsonWebKey.alg.asJson),
-          ("crv", jsonWebKey.crv.asJson),
-          ("e", jsonWebKey.e.asJson),
-          ("d", jsonWebKey.d.asJson),
-          ("ext", jsonWebKey.ext.asJson),
-          ("key_ops", jsonWebKey.key_ops.asJson),
-          ("kid", jsonWebKey.kid.asJson),
-          ("kty", jsonWebKey.kty.asJson),
-          ("n", jsonWebKey.n.asJson),
-          ("use", jsonWebKey.use.asJson),
-          ("x", jsonWebKey.x.asJson),
-          ("y", jsonWebKey.y.asJson),
-        )
+  given jsonWebKeyEncoder: Encoder[JsonWebKey] = deriveEncoder[JsonWebKey]
 
-  given jsonWebKeyDecoder: Decoder[JsonWebKey] =
-    (c: HCursor) =>
-      for {
-        alg <- c.downField("alg").as[Option[String]]
-        crv <- c.downField("crv").as[Option[String]]
-        e <- c.downField("e").as[Option[String]]
-        d <- c.downField("d").as[Option[String]]
-        ext <- c.downField("ext").as[Option[Boolean]]
-        key_ops <- c.downField("key_ops").as[Vector[String]]
-        kid <- c.downField("kid").as[Option[String]]
-        kty <- c.downField("kty").as[String]
-        n <- c.downField("n").as[Option[String]]
-        use <- c.downField("use").as[Option[String]]
-        x <- c.downField("x").as[Option[String]]
-        y <- c.downField("y").as[Option[String]]
-      } yield {
-        JsonWebKey(
-          alg = alg,
-          crv = crv,
-          e = e,
-          d = d,
-          ext = ext,
-          key_ops = key_ops,
-          kid = kid,
-          kty = kty,
-          n = n,
-          use = use,
-          x = x,
-          y = y
-        )
-      }
+  given jsonWebKeyDecoder: Decoder[JsonWebKey] = deriveDecoder[JsonWebKey]
 
 }
