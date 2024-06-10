0ba5829b8480b1ebfcfb540071c6ae15a3e6cb8b
Pat Losoponkul <pat.losoponkul@iohk.io>
2024-06-10 15:27:41 +0700

    chore: remove keycloak plugin code
    Signed-off-by: Pat Losoponkul <pat.losoponkul@iohk.io>

--- extensions/keycloak-oid4vci/src/main/java/io/iohk/atala/keycloak/OID4VCIConstants.java
+++
@@ -1,7 +0,0 @@
-package io.iohk.atala.keycloak;
-
-public class OID4VCIConstants {
-    public static final String ISSUER_STATE = "issuer_state";
-    public static final String C_NONCE = "c_nonce";
-    public static final String C_NONCE_EXPIRE = "c_nonce_expires_in";
-}
