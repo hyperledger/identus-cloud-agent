07247d1b7c39b3afa69d5fc5e0b644831ec96dbb
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2024-06-17 21:02:59 +0700

    fix: make init-script.sh idempotent #1173
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- infrastructure/shared/postgres/init-script.sh
+++ infrastructure/shared/postgres/init-script.sh
@@ -6,12 +6,39 @@ set -u
 function create_user_and_database() {
 	local database=$1
 	local app_user=${database}-application-user
-	echo "  Creating user and database '$database'"
+	echo "Creating user and database '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-		CREATE USER "$app_user" WITH PASSWORD 'password';
-		CREATE DATABASE $database;
-		\c $database
-		ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "$app_user";
+        DO \$\$
+        BEGIN
+            IF NOT EXISTS (
+                SELECT FROM pg_catalog.pg_roles
+                WHERE rolname = '$app_user') THEN
+                CREATE USER "$app_user" WITH PASSWORD 'password';
+            END IF;
+        END
+        \$\$;
+
+        DO \$\$
+        BEGIN
+            IF NOT EXISTS (
+                SELECT FROM pg_database
+                WHERE datname = '$database') THEN
+                CREATE DATABASE $database;
+            END IF;
+        END
+        \$\$;
+
+        \c $database
+
+        DO \$\$
+        BEGIN
+            IF NOT EXISTS (
+                SELECT FROM pg_catalog.pg_roles
+                WHERE rolname = '$app_user') THEN
+                ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "$app_user";
+            END IF;
+        END
+        \$\$;
 	EOSQL
 }
 
