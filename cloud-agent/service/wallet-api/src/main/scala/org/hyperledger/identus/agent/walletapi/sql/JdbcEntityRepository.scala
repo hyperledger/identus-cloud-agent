74508a78845a2503c7a17554542473a2e8921f15
Benjamin Voiturier <benjamin.voiturier@iohk.io>
2024-06-18 11:17:56 +0200

    chore: refactor entity repository
    Signed-off-by: Benjamin Voiturier <benjamin.voiturier@iohk.io>

--- cloud-agent/service/wallet-api/src/main/scala/org/hyperledger/identus/agent/walletapi/sql/JdbcEntityRepository.scala
+++ cloud-agent/service/wallet-api/src/main/scala/org/hyperledger/identus/agent/walletapi/sql/JdbcEntityRepository.scala
@@ -3,14 +3,8 @@ package org.hyperledger.identus.agent.walletapi.sql
 import doobie.*
 import doobie.implicits.*
 import org.hyperledger.identus.agent.walletapi.model.error.EntityServiceError
-import org.hyperledger.identus.agent.walletapi.model.error.EntityServiceError.{
-  EntityAlreadyExists,
-  EntityNotFound,
-  EntityStorageError,
-  EntityWalletNotFound
-}
 import org.hyperledger.identus.agent.walletapi.model.Entity
-import org.postgresql.util.PSQLException
+import org.hyperledger.identus.shared.db.Implicits.ensureOneAffectedRowOrDie
 import zio.*
 import zio.interop.catz.*
 
@@ -18,87 +12,49 @@ import java.util.UUID
 
 class JdbcEntityRepository(xa: Transactor[Task]) extends EntityRepository {
   import EntityStorageSql.*
-  override def insert(entity: Entity): IO[EntityServiceError, Entity] = {
+  override def insert(entity: Entity): UIO[Entity] = {
     EntityStorageSql
       .insert(model2db(entity))
       .transact(xa)
-      .logError(s"Insert entity failed: $entity")
-      .mapError {
-        case sqlException: PSQLException
-            if sqlException.getMessage.contains("duplicate key value violates unique constraint") =>
-          EntityAlreadyExists(entity.id, sqlException.getMessage)
-        case sqlException: PSQLException
-            if sqlException.getMessage
-              .contains("violates foreign key constraint \"entity_wallet_id_fkey\"") =>
-          EntityWalletNotFound(entity.id, entity.walletId)
-        case other: Throwable =>
-          EntityStorageError(other.getMessage)
-      }
       .map(db2model)
+      .orDie
   }
-  override def getById(id: UUID): IO[EntityServiceError, Entity] = {
+  override def getById(id: UUID): UIO[Entity] = {
     EntityStorageSql
       .getById(id)
       .transact(xa)
       .map(_.headOption.map(db2model))
-      .logError(s"Get entity by id=$id failed")
-      .mapError(throwable => EntityStorageError(throwable.getMessage))
-      .flatMap(
-        _.fold[ZIO[Any, EntityServiceError, Entity]](ZIO.fail(EntityNotFound(id, s"Get entity by id=$id failed")))(
-          ZIO.succeed
-        )
-      )
+      .someOrElseZIO(ZIO.dieMessage(s"Entity not found: id=$id"))
+      .orDie
   }
 
-  override def updateName(entityId: UUID, name: String): IO[EntityServiceError, Unit] = {
+  override def updateName(entityId: UUID, name: String): UIO[Unit] = {
     EntityStorageSql
       .updateName(entityId, name)
       .transact(xa)
-      .logError(s"Update entity name=$name by id=$entityId failed")
-      .mapError(throwable => EntityStorageError(throwable.getMessage))
-      .flatMap { updatedCount =>
-        if updatedCount == 1 then ZIO.unit
-        else ZIO.fail(EntityNotFound(entityId, s"Update entity name=$name by id=$entityId failed"))
-      }
+      .ensureOneAffectedRowOrDie
   }
 
-  override def updateWallet(entityId: UUID, walletId: UUID): IO[EntityServiceError, Unit] = {
+  override def updateWallet(entityId: UUID, walletId: UUID): UIO[Unit] = {
     EntityStorageSql
       .updateWallet(entityId, walletId)
       .transact(xa)
-      .logError(s"Update entity walletId=$walletId by id=$entityId failed")
-      .mapError {
-        case sqlException: PSQLException
-            if sqlException.getMessage
-              .contains("violates foreign key constraint \"entity_wallet_id_fkey\"") =>
-          EntityWalletNotFound(entityId, walletId)
-        case other: Throwable => EntityStorageError(other.getMessage)
-      }
-      .flatMap(updatedCount =>
-        if updatedCount == 1 then ZIO.unit
-        else ZIO.fail(EntityNotFound(entityId, s"Update entity walletId=$walletId by id=$entityId failed"))
-      )
+      .ensureOneAffectedRowOrDie
   }
 
-  override def delete(entityId: UUID): IO[EntityServiceError, Unit] = {
+  override def delete(entityId: UUID): UIO[Unit] = {
     EntityStorageSql
       .delete(entityId)
       .transact(xa)
-      .logError(s"Delete entity failed: id=$entityId")
-      .mapError(throwable => EntityStorageError(throwable.getMessage))
-      .flatMap(deletedCount =>
-        if deletedCount == 1 then ZIO.unit
-        else ZIO.fail(EntityNotFound(entityId, s"Delete entity failed: id=$entityId"))
-      )
+      .ensureOneAffectedRowOrDie
   }
 
-  override def getAll(offset: Index, limit: Index): IO[EntityServiceError, List[Entity]] = {
+  override def getAll(offset: Index, limit: Index): UIO[List[Entity]] = {
     EntityStorageSql
       .getAll(offset, limit)
       .transact(xa)
-      .logError("Get all entities failed")
-      .mapError(throwable => EntityStorageError(throwable.getMessage))
       .map(_.map(db2model))
+      .orDie
   }
 }
 
