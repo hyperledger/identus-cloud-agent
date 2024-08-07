c582c14e20310556fca9d77110c99dfa56116a05
Allain Magyar <allain.magyar@iohk.io>
2024-08-07 15:40:29 -0300

    ci: changes to new release process
    Signed-off-by: Allain Magyar <allain.magyar@iohk.io>

---
+++ release.config.mjs
@@ -0,0 +1,71 @@
+let productionRelease = process.env.PRODUCTION_RELEASE ? true : false
+
+let branches = []
+let extraPlugins = []
+
+if (productionRelease) {
+    branches = [
+        'main',
+        '+([0-9])?(.{+([0-9]),x}).x',
+    ]
+    extraPlugins = [
+        '@semantic-release/release-notes-generator',
+        ["@semantic-release/changelog", {
+            "changelogFile": "CHANGELOG.md"
+        }],
+        ["@semantic-release/exec", {
+            "prepareCmd": "sbt \"release release-version ${nextRelease.version} next-version ${nextRelease.version}-SNAPSHOT with-defaults\""
+        }],
+        ["@semantic-release/exec", {
+            "prepareCmd": "npm version ${nextRelease.version} --git-tag-version false"
+        }],
+        ["@semantic-release/exec", {
+            "prepareCmd": "sbt dumpLicenseReportAggregate && cp ./target/license-reports/root-licenses.md ./DEPENDENCIES.md"
+        }],
+        ["@semantic-release/exec", {
+            "prepareCmd": "sed -i.bak \"s/AGENT_VERSION=.*/AGENT_VERSION=${nextRelease.version}/\" ./infrastructure/local/.env && rm -f ./infrastructure/local/.env.bak"
+        }],
+        ["@semantic-release/exec", {
+            "prepareCmd": "sbt \"cloudAgentServer/test:runMain org.hyperledger.identus.api.util.Tapir2StaticOAS ${process.env.PWD}/cloud-agent/service/api/http/cloud-agent-openapi-spec.yaml ${nextRelease.version}\""
+        }],
+        ["@semantic-release/git", {
+            "assets": [
+                "version.sbt",
+                "CHANGELOG.md",
+                "DEPENDENCIES.md",
+                "package.json",
+                "package-lock.json",
+                "cloud-agent/service/api/http/cloud-agent-openapi-spec.yaml",
+                "infrastructure/local/.env"
+            ],
+            "message": "chore(release): cut Identus Cloud agent ${nextRelease.version} release\n\n${nextRelease.notes}\n\nSigned-off-by: Allain Magyar <allain.magyar@iohk.io>"
+        }],
+        ["semantic-release-slack-bot", {
+            "notifyOnSuccess": true,
+            "notifyOnFail": true,
+            "markdownReleaseNotes": true,
+            "onSuccessTemplate": {
+                "text": "A new version of Identus Cloud Agent successfully released!\nVersion: `$npm_package_version`\nTag: $repo_url/releases/tag/cloud-agent-v$npm_package_version\n\nRelease notes:\n$release_notes"
+            }
+        }],
+    ]
+} else {
+    branches = [
+        '+([0-9])?(.{+([0-9]),x}).x',
+        'base',
+        { name: 'main', prerelease: 'rc' }
+    ]
+    extraPlugins = []
+}
+
+export default {
+    branches: branches,
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        ["@semantic-release/exec", {
+            "prepareCmd": "docker buildx build --platform=linux/arm64,linux/amd64 --push -t ghcr.io/hyperledger/identus-cloud-agent:${nextRelease.version} ./cloud-agent/service/server/target/docker/stage"
+        }],
+        ...extraPlugins
+    ],
+    tagFormat: "cloud-agent-v${version}"
+}
