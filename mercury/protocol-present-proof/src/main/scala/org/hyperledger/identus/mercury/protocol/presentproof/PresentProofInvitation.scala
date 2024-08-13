f20f87b58de084242b4456ee79bd424f8dc4d535
mineme0110 <shailesh.patil@iohk.io>
2024-08-13 16:08:34 +0100

    WIP feat: connection less presentation
    Signed-off-by: mineme0110 <shailesh.patil@iohk.io>

---
+++ mercury/protocol-present-proof/src/main/scala/org/hyperledger/identus/mercury/protocol/presentproof/PresentProofInvitation.scala
@@ -0,0 +1,27 @@
+package org.hyperledger.identus.mercury.protocol.presentproof
+
+import org.hyperledger.identus.mercury.model.DidId
+import org.hyperledger.identus.mercury.protocol.invitation.v2.Invitation
+import org.hyperledger.identus.mercury.model.AttachmentDescriptor
+object PresentProofInvitation {
+  def makeInvitation(
+      from: DidId,
+      goalCode: Option[String],
+      goal: Option[String],
+      invitationId: String,
+      requestPresentation: RequestPresentation
+  ): Invitation = {
+    val attachmentDescriptor = AttachmentDescriptor.buildJsonAttachment(payload = requestPresentation)
+    Invitation(
+      id = invitationId,
+      from = from,
+      body = Invitation.Body(
+        goal_code = goalCode,
+        goal = goal,
+        Nil
+      ),
+      attachments = Some(Seq(attachmentDescriptor))
+    )
+  }
+
+}
