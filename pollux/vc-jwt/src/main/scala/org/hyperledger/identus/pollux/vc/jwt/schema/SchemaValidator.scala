6f8c9ffb6283d8da2bf7b92672142a3b27195c4d
patlo-iog <pat.losoponkul@iohk.io>
2024-08-22 23:50:09 +0700

    chore: remove dead code SchemaValidator (#1296)
    Signed-off-by: Pat Losoponkul <pat.losoponkul@iohk.io>

--- pollux/vc-jwt/src/main/scala/org/hyperledger/identus/pollux/vc/jwt/schema/SchemaValidator.scala
+++
@@ -1,8 +0,0 @@
-package org.hyperledger.identus.pollux.vc.jwt.schema
-
-import io.circe.Json
-import zio.prelude.Validation
-
-trait SchemaValidator {
-  def validate(payloadToValidate: Json): Validation[String, Json]
-}
