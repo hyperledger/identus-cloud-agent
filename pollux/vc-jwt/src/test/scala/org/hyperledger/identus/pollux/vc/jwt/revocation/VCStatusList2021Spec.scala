1ed2a14651b6411add88f96208db6037b49ebd4a
Bassam <bassam.riman@iohk.io>
2024-09-10 12:17:48 -0400

    fix: Remove type DID (#1327)
    Signed-off-by: Bassam Riman <bassam.riman@iohk.io>

--- pollux/vc-jwt/src/test/scala/org/hyperledger/identus/pollux/vc/jwt/revocation/VCStatusList2021Spec.scala
+++ pollux/vc-jwt/src/test/scala/org/hyperledger/identus/pollux/vc/jwt/revocation/VCStatusList2021Spec.scala
@@ -1,6 +1,7 @@
 package org.hyperledger.identus.pollux.vc.jwt.revocation
 
-import org.hyperledger.identus.pollux.vc.jwt.{DID, ES256KSigner, Issuer, JwtCredential}
+import org.hyperledger.identus.castor.core.model.did.DID
+import org.hyperledger.identus.pollux.vc.jwt.{ES256KSigner, Issuer, JwtCredential}
 import org.hyperledger.identus.shared.crypto.KmpSecp256k1KeyOps
 import zio.{UIO, ZIO}
 import zio.test.{assertTrue, Spec, ZIOSpecDefault}
@@ -17,7 +18,7 @@ object VCStatusList2021Spec extends ZIOSpecDefault {
 
     ZIO.succeed(
       Issuer(
-        did = DID("did:issuer:MDP8AsFhHzhwUvGNuYkX7T"),
+        did = DID.fromString("did:issuer:MDP8AsFhHzhwUvGNuYkX7T").toOption.get,
         signer = ES256KSigner(javaSKey),
         publicKey = javaPKey
       )
