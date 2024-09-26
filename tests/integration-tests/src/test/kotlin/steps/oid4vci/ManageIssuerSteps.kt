5d4e6fb1a9507738f3ec12a3fc6b38db4e66592f
Allain Magyar <allain.magyar@iohk.io>
2024-09-26 08:46:22 -0300

    test: add negative scenarios for oid4vci integration tests
    Signed-off-by: Allain Magyar <allain.magyar@iohk.io>

--- tests/integration-tests/src/test/kotlin/steps/oid4vci/ManageIssuerSteps.kt
+++ tests/integration-tests/src/test/kotlin/steps/oid4vci/ManageIssuerSteps.kt
@@ -1,13 +1,20 @@
 package steps.oid4vci
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonNull
+import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
 import interactions.Delete
 import interactions.Get
 import interactions.Patch
 import interactions.Post
+import interactions.body
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
 import io.iohk.atala.automation.extensions.get
+import io.iohk.atala.automation.extensions.toJsonPath
 import io.iohk.atala.automation.serenity.ensure.Ensure
 import net.serenitybdd.rest.SerenityRest
 import net.serenitybdd.screenplay.Actor
@@ -17,7 +24,6 @@ import org.apache.http.HttpStatus.SC_OK
 import org.hyperledger.identus.client.models.AuthorizationServer
 import org.hyperledger.identus.client.models.CreateCredentialIssuerRequest
 import org.hyperledger.identus.client.models.CredentialIssuer
-import org.hyperledger.identus.client.models.CredentialIssuer1
 import org.hyperledger.identus.client.models.CredentialIssuerPage
 import org.hyperledger.identus.client.models.IssuerMetadata
 import org.hyperledger.identus.client.models.PatchAuthorizationServer
@@ -30,7 +36,9 @@ class ManageIssuerSteps {
 
     @Given("{actor} has an existing oid4vci issuer")
     fun issuerHasExistingCredentialIssuer(issuer: Actor) {
-        issuerCreateCredentialIssuer(issuer)
+        if (!issuer.recallAll().containsKey("oid4vciCredentialIssuer")) {
+            issuerCreateCredentialIssuer(issuer)
+        }
     }
 
     @When("{actor} creates an oid4vci issuer")
@@ -56,7 +64,7 @@ class ManageIssuerSteps {
 
     @Then("{actor} sees the oid4vci issuer exists on the agent")
     fun issuerSeesCredentialIssuerExists(issuer: Actor) {
-        val credentialIssuer = issuer.recall<CredentialIssuer1>("oid4vciCredentialIssuer")
+        val credentialIssuer = issuer.recall<CredentialIssuer>("oid4vciCredentialIssuer")
         issuer.attemptsTo(
             Get("/oid4vci/issuers"),
             Ensure.thatTheLastResponse().statusCode().isEqualTo(SC_OK),
@@ -81,19 +89,29 @@ class ManageIssuerSteps {
     fun issuerUpdateCredentialIssuer(issuer: Actor) {
         val credentialIssuer = issuer.recall<CredentialIssuer>("oid4vciCredentialIssuer")
         issuer.attemptsTo(
-            Patch.to("/oid4vci/issuers/${credentialIssuer.id}")
-                .with {
-                    it.body(
-                        PatchCredentialIssuerRequest(
-                            authorizationServer = PatchAuthorizationServer(
-                                url = UPDATE_AUTH_SERVER_URL,
-                                clientId = UPDATE_AUTH_SERVER_CLIENT_ID,
-                                clientSecret = UPDATE_AUTH_SERVER_CLIENT_SECRET,
-                            ),
-                        ),
-                    )
-                },
-            Ensure.thatTheLastResponse().statusCode().isEqualTo(HttpStatus.SC_OK),
+            Patch.to("/oid4vci/issuers/${credentialIssuer.id}").body(
+                PatchCredentialIssuerRequest(
+                    authorizationServer = PatchAuthorizationServer(
+                        url = UPDATE_AUTH_SERVER_URL,
+                        clientId = UPDATE_AUTH_SERVER_CLIENT_ID,
+                        clientSecret = UPDATE_AUTH_SERVER_CLIENT_SECRET,
+                    ),
+                ),
+            ),
+            Ensure.thatTheLastResponse().statusCode().isEqualTo(SC_OK)
+        )
+    }
+
+    @When("{actor} tries to update the oid4vci issuer '{}' property using '{}' value")
+    fun issuerTriesToUpdateTheOID4VCIIssuer(issuer: Actor, property: String, value: String) {
+        val credentialIssuer = issuer.recall<CredentialIssuer>("oid4vciCredentialIssuer")
+        val body = JsonObject()
+        val propertyValue = if (value == "null") { null } else { value }
+        body.addProperty(property, propertyValue)
+
+        val gson = GsonBuilder().serializeNulls().create()
+        issuer.attemptsTo(
+            Patch.to("/oid4vci/issuers/${credentialIssuer.id}").body(gson.toJson(body))
         )
     }
 
@@ -109,23 +127,60 @@ class ManageIssuerSteps {
     @When("{actor} tries to create oid4vci issuer with '{}', '{}', '{}' and '{}'")
     fun issuerTriesToCreateOIDCIssuer(
         issuer: Actor,
-        id: String?,
-        url: String?,
-        clientId: String?,
-        clientSecret: String?
+        id: String,
+        url: String,
+        clientId: String,
+        clientSecret: String
     ) {
-        println("$issuer $id $url $clientId $clientSecret")
-        println(url == null)
+        val idProperty = if (id == "null") {
+            null
+        } else {
+            id
+        }
+        val urlProperty = if (url == "null") {
+            null
+        } else {
+            url
+        }
+        val clientIdProperty = if (clientId == "null") {
+            null
+        } else {
+            clientId
+        }
+        val clientSecretProperty = if (clientSecret == "null") {
+            null
+        } else {
+            clientSecret
+        }
+
+        val body = JsonObject()
+        val authorizationServer = JsonObject()
+
+        body.addProperty("id", idProperty)
+        body.add("authorizationServer", authorizationServer)
+
+        authorizationServer.addProperty("url", urlProperty)
+        authorizationServer.addProperty("clientId", clientIdProperty)
+        authorizationServer.addProperty("clientSecret", clientSecretProperty)
+
+        val gson = GsonBuilder().serializeNulls().create()
+        issuer.attemptsTo(
+            Post.to("/oid4vci/issuers").body(gson.toJson(body))
+        )
     }
 
-    @Then("{actor} should see the oid4vci error '{}'")
-    fun issuerShouldSeeTheOIDC4VCIError(issuer: Actor, error: String) {
-
+    @Then("{actor} should see the oid4vci '{}' http status response with '{}' detail")
+    fun issuerShouldSeeTheOIDC4VCIError(issuer: Actor, httpStatus: Int, errorDetail: String) {
+        SerenityRest.lastResponse().body.prettyPrint()
+        issuer.attemptsTo(
+            Ensure.that(SerenityRest.lastResponse().statusCode).isEqualTo(httpStatus),
+            Ensure.that(SerenityRest.lastResponse().body.asString()).contains(errorDetail)
+        )
     }
 
     @Then("{actor} sees the oid4vci issuer updated with new values")
     fun issuerSeesUpdatedCredentialIssuer(issuer: Actor) {
-        val credentialIssuer = issuer.recall<CredentialIssuer1>("oid4vciCredentialIssuer")
+        val credentialIssuer = issuer.recall<CredentialIssuer>("oid4vciCredentialIssuer")
         issuer.attemptsTo(
             Get("/oid4vci/issuers"),
             Ensure.thatTheLastResponse().statusCode().isEqualTo(HttpStatus.SC_OK),
@@ -152,7 +207,7 @@ class ManageIssuerSteps {
 
     @Then("{actor} cannot see the oid4vci issuer on the agent")
     fun issuerCannotSeeCredentialIssuer(issuer: Actor) {
-        val credentialIssuer = issuer.recall<CredentialIssuer1>("oid4vciCredentialIssuer")
+        val credentialIssuer = issuer.recall<CredentialIssuer>("oid4vciCredentialIssuer")
         issuer.attemptsTo(
             Get("/oid4vci/issuers"),
             Ensure.thatTheLastResponse().statusCode().isEqualTo(HttpStatus.SC_OK),
@@ -171,5 +226,11 @@ class ManageIssuerSteps {
             Get("/oid4vci/issuers/${credentialIssuer.id}/.well-known/openid-credential-issuer"),
             Ensure.thatTheLastResponse().statusCode().isEqualTo(HttpStatus.SC_NOT_FOUND),
         )
+    }
+
+    @Then("{actor} should see the update oid4vci issuer returned '{}' http status")
+    fun issuerShouldSeeTheUpdateOID4VCIIssuerReturnedHttpStatus(issuer: Actor, httpStatus: Int) {
+        println("STATUS: ${SerenityRest.lastResponse().statusCode}")
+        SerenityRest.lastResponse().body.prettyPrint()
     }
 }
--- tests/integration-tests/src/test/resources/features/oid4vci/manage_issuer.feature
+++ tests/integration-tests/src/test/resources/features/oid4vci/manage_issuer.feature
@@ -19,12 +19,24 @@ Feature: Manage OID4VCI credential issuer
     And Issuer cannot see the oid4vci IssuerMetadata endpoint
 
   @test
-  Scenario Outline: Create issuer with wrong data should not work
+  Scenario Outline: Create issuer with <description> expect <httpStatus> response
     When Issuer tries to create oid4vci issuer with '<id>', '<url>', '<clientId>' and '<clientSecret>'
-    Then Issuer should see the oid4vci error '<error>'
+    Then Issuer should see the oid4vci '<httpStatus>' http status response with '<errorDetail>' detail
     Examples:
-      | id    | url   | clientId | clientSecret | error |
-      | null  | null  | null     | null         |       |
-      |       |       |          |              |       |
-      | empty | empty | empty    | empty        |       |
-      | 1     | {}    | null     | null         |       |
+      | id                                   | url                | clientId | clientSecret | httpStatus | errorDetail                      | description        |
+      | null                                 | null               | null     | null         | 400        | authorizationServer.url          | null values        |
+      | null                                 | malformed          | id       | secret       | 400        | Invalid URL                      | malformed url      |
+      | null                                 | http://example.com | id       | null         | 400        | authorizationServer.clientSecret | null client secret |
+      | null                                 | http://example.com | null     | secret       | 400        | authorizationServer.clientId     | null client id     |
+      | null                                 | null               | id       | secret       | 400        | authorizationServer.url          | null url           |
+      | 4048ef76-749d-4296-8c6c-07c8a20733a0 | http://example.com | id       | secret       | 201        |                                  | right values       |
+      | 4048ef76-749d-4296-8c6c-07c8a20733a0 | http://example.com | id       | secret       | 500        |                                  | duplicated id      |
+
+  @test
+  Scenario Outline: Update issuer with <description> expect <httpStatus> response
+    Given Issuer has an existing oid4vci issuer
+    When Issuer tries to update the oid4vci issuer '<property>' property using '<value>' value
+    Then Issuer should see the oid4vci '<httpStatus>' http status response with '<errorDetail>' detail
+    Examples:
+      | property | value     | httpStatus | errorDetail | description |
+      | url      | malformed | 200        |             | Invalid URL |
