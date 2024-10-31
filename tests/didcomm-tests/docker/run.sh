e78b362a58eb241ce27520f311ed449babb23aa2
Allain Magyar <allain.magyar@iohk.io>
2024-10-31 09:42:11 -0300

    test: fixes docker script
    Signed-off-by: Allain Magyar <allain.magyar@iohk.io>

--- tests/didcomm-tests/docker/run.sh
+++ tests/didcomm-tests/docker/run.sh
@@ -7,5 +7,5 @@ CLOUD_AGENT_VERSION=$1
 PORT="$PRISM_PORT" \
 CLOUD_AGENT_VERSION="$CLOUD_AGENT_VERSION" \
 docker compose \
-  -f "docker-compose.yml" \
+  -f "docker/docker-compose.yml" \
   up -d --wait
