13375d07c3aba83c9c2a0c03ebbf4e9640f15b22
mineme0110 <shailesh.patil@iohk.io>
2024-11-27 12:35:36 +0000

    fix the performance test with single docker compose for 3 agents
    Signed-off-by: mineme0110 <shailesh.patil@iohk.io>

--- tests/performance-tests/agent-performance-tests-k6/run.sh
+++ tests/performance-tests/agent-performance-tests-k6/run.sh
@@ -6,15 +6,28 @@ set -e
 ENV_FILE=".env"
 PERF_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 AGENT_DIR="$PERF_DIR/../../.."
-DOCKERFILE_NODE="$AGENT_DIR/infrastructure/shared/docker-compose-node.yml"
-DOCKERFILE_AGENT="$AGENT_DIR/infrastructure/shared/docker-compose-agent.yml"
+DOCKERFILE="$AGENT_DIR/infrastructure/shared/docker-compose-combined.yml"
 K6_URL="https://github.com/grafana/k6/releases/download/v0.45.0/k6-v0.45.0-macos-arm64.zip"
 K6_ZIP_FILE="$(basename ${K6_URL})"
 
 # Functions
-function startAgent() {
-	echo "Starting [$NAME] agent"
-	PORT="${PORT}" \
+function startAllAgents() {
+	echo "Starting agents"
+
+	local DOCKERHOST="host.docker.internal"
+  local NODE_REFRESH_AND_SUBMIT_PERIOD="1s"
+  local NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD="1s"
+  local NODE_WALLET_MAX_TPS="1000"
+	local ADMIN_TOKEN=admin
+	local DEFAULT_WALLET_ENABLED=true
+	local DEFAULT_WALLET_AUTH_API_KEY=default
+	local API_KEY_AUTO_PROVISIONING=false
+	local API_KEY_ENABLED=true
+
+  echo "Issuer Port [$ISSUER_PORT]"
+  echo "Holder Port [$HOLDER_PORT]"
+  echo "VerifierPort [$VERIFIER_PORT]"
+
 		ADMIN_TOKEN="${ADMIN_TOKEN}" \
 		DEFAULT_WALLET_ENABLED="${DEFAULT_WALLET_ENABLED}" \
 		DEFAULT_WALLET_AUTH_API_KEY="${DEFAULT_WALLET_AUTH_API_KEY}" \
@@ -25,151 +38,19 @@ function startAgent() {
 		NODE_REFRESH_AND_SUBMIT_PERIOD="${NODE_REFRESH_AND_SUBMIT_PERIOD}" \
 		NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD="${NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD}" \
 		NODE_WALLET_MAX_TPS="${NODE_WALLET_MAX_TPS}" \
-		docker compose -p "${NAME}" -f "${DOCKERFILE_AGENT}" \
+		docker compose -f "${DOCKERFILE}" \
 		--env-file "${ENV_FILE}" up -d --wait 2>/dev/null
-	echo "Agent [$NAME] healthy"
+	echo "Agents healthy"
 }
 
-function startPrismNode() {
-	echo "Starting [$NAME]"
-		NODE_REFRESH_AND_SUBMIT_PERIOD="${NODE_REFRESH_AND_SUBMIT_PERIOD}" \
-		NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD="${NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD}" \
-		NODE_WALLET_MAX_TPS="${NODE_WALLET_MAX_TPS}" \
-		docker compose -p "${NAME}" -f "${DOCKERFILE_NODE}" \
-		--env-file "${ENV_FILE}" up -d --wait 2>/dev/null
-	echo "[$NAME] healthy"
-}
-
-function stopAgent() {
-	echo "Stopping [${NAME}] agent"
-	PORT="${PORT}" \
-		DOCKERHOST="${DOCKERHOST}" \
+function stopAllAgents() {
+	echo "Stopping  agents"
 		docker compose \
-		-p "${NAME}" \
 		-f "${DOCKERFILE}" \
 		--env-file "${ENV_FILE}" down -v 2>/dev/null
-	echo "Agent [${NAME}] stopped"
+	echo "Agents  stopped"
 }
 
-function stopPrismNode() {
-	echo "Stopping [${NAME}] agent"
-		docker compose \
-		-p "${NAME}" \
-		-f "${DOCKERFILE}" \
-		--env-file "${ENV_FILE}" down -v 2>/dev/null
-	echo "Agent [${NAME}] stopped"
-}
-
-function createPrismNode1() {
-    local NAME="prism-node"
-    local PG_PORT=5432
-    local NODE_REFRESH_AND_SUBMIT_PERIOD="1s"
-    local NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD="1s"
-    local NODE_WALLET_MAX_TPS="1000"
-
-    echo "Creating PrismNode with Name: $NAME"
-    docker network create agent-network || echo "Network agent-network already exists"
-    echo "Starting Docker compose for PrismNode"
-    NODE_REFRESH_AND_SUBMIT_PERIOD="${NODE_REFRESH_AND_SUBMIT_PERIOD}" \
-    NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD="${NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD}" \
-    NODE_WALLET_MAX_TPS="${NODE_WALLET_MAX_TPS}" \
-    docker compose -p "${NAME}" -f "${DOCKERFILE_NODE}" \
-    --env-file "${ENV_FILE}" up -d --wait 2>/dev/null || {
-        echo "Failed to start PrismNode Docker containers"
-        exit 1
-    }
-
-    echo "PrismNode [$NAME] is now healthy"
-}
-
-function createPrismNode() {
-	local NAME="prism-node"
-	local PG_PORT=5432
-	local NODE_REFRESH_AND_SUBMIT_PERIOD="1s"
-	local NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD="1s"
-	local NODE_WALLET_MAX_TPS="1000"
-	startPrismNode
-}
-
-function createIssuer() {
-	local NAME="issuer"
-	local PORT=8080
-	local ADMIN_TOKEN=admin
-	local DEFAULT_WALLET_ENABLED=true
-	local DEFAULT_WALLET_AUTH_API_KEY=default
-	local API_KEY_AUTO_PROVISIONING=false
-	local API_KEY_ENABLED=true
-	local DOCKERHOST="host.docker.internal"
-	local PG_PORT=5432
-	local NODE_REFRESH_AND_SUBMIT_PERIOD="1s"
-	local NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD="1s"
-	local NODE_WALLET_MAX_TPS="1000"
-
-	startAgent
-}
-
-function createHolder() {
-	local NAME="holder"
-	local PORT=8200
-	local ADMIN_TOKEN=admin
-	local DEFAULT_WALLET_ENABLED=true
-	local DEFAULT_WALLET_AUTH_API_KEY=default
-	local API_KEY_AUTO_PROVISIONING=false
-	local API_KEY_ENABLED=true
-	local DOCKERHOST="host.docker.internal"
-	local PG_PORT=5432
-	local NODE_REFRESH_AND_SUBMIT_PERIOD="1s"
-	local NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD="1s"
-	local NODE_WALLET_MAX_TPS="1000"
-
-	startAgent
-}
-
-function createVerifier() {
-	local NAME="verifier"
-	local PORT=8100
-	local ADMIN_TOKEN=admin
-	local DEFAULT_WALLET_ENABLED=true
-	local DEFAULT_WALLET_AUTH_API_KEY=default
-	local API_KEY_AUTO_PROVISIONING=false
-	local API_KEY_ENABLED=true
-	local DOCKERHOST="host.docker.internal"
-	local PG_PORT=5432
-	local NODE_REFRESH_AND_SUBMIT_PERIOD="1s"
-	local NODE_MOVE_SCHEDULED_TO_PENDING_PERIOD="1s"
-	local NODE_WALLET_MAX_TPS="1000"
-
-	startAgent
-}
-
-function removeIssuer() {
-	local NAME="issuer"
-	local PORT=8080
-	local DOCKERHOST="host.docker.internal"
-
-	stopAgent
-}
-
-function removeVerifier() {
-	local NAME="verifier"
-	local PORT=8100
-	local DOCKERHOST="host.docker.internal"
-
-	stopAgent
-}
-
-function removeHolder() {
-	local NAME="holder"
-	local PORT=8200
-	local DOCKERHOST="host.docker.internal"
-
-	stopAgent
-}
-
-function removePrismNode() {
-	local NAME="prism-node"
-	stopPrismNode
-}
 
 # clean up on finish
 function cleanup() {
@@ -182,19 +63,9 @@ function cleanup() {
 	echo "Removing K6 binaries"
 	rm k6
 	rm "$K6_ZIP_FILE"
-
-  removePrismNode &
-	removeIssuer &
-	removeVerifier &
-	removeHolder &
-	wait
-  # Ensure no containers are left connected to the network
-  echo "Forcefully stopping all containers on agent-network"
-  docker ps -q --filter network=agent-network | xargs -r docker stop || echo "No containers to stop"
-  docker ps -a -q --filter network=agent-network | xargs -r docker rm || echo "No containers to remove"
-
-  echo "Removing agent-network"
-  docker network rm agent-network || echo "Failed to remove agent-network"
+	echo "Stopping All Agents"
+  stopAllAgents
+  echo "cleanup complete"
 }
 
 trap 'cleanup' EXIT
@@ -218,13 +89,19 @@ cd "$PERF_DIR"
 sed -i.bak "s/AGENT_VERSION=.*/AGENT_VERSION=${AGENT_VERSION}/" "${ENV_FILE}" && rm -f "${ENV_FILE}.bak"
 
 # create docker  agent-network
-docker network create agent-network
 # create Prism Node
-createPrismNode
+export HOLDER_PORT=8300
+export ISSUER_PORT=8080
+export VERIFIER_PORT=8100
+export ISSUER_AGENT_API_KEY=default
+export HOLDER_AGENT_API_KEY=default
+export VERIFIER_AGENT_API_KEY=default
+export ISSUER_AGENT_URL="http://localhost:${ISSUER_PORT}/cloud-agent"
+export HOLDER_AGENT_URL="http://localhost:${HOLDER_PORT}/cloud-agent"
+export VERIFIER_AGENT_URL="http://localhost:${VERIFIER_PORT}/cloud-agent"
 
-createIssuer
-createHolder
-createVerifier
+startAllAgents
+
 
 # yarn install
 echo "Installing dependencies"
@@ -235,10 +112,6 @@ yarn webpack >/dev/null
 # start perf test
 echo "Starting performance testing"
 
-export ISSUER_AGENT_API_KEY=default
-export HOLDER_AGENT_API_KEY=default
-export VERIFIER_AGENT_API_KEY=default
-
 ./k6 run -e SCENARIO_LABEL=create-prism-did-smoke ./dist/create-prism-did-test.js
 ./k6 run -e SCENARIO_LABEL=credential-offer-smoke ./dist/credential-offer-test.js
 ./k6 run -e SCENARIO_LABEL=credential-definition-smoke ./dist/credential-definition-test.js
@@ -247,4 +120,3 @@ export VERIFIER_AGENT_API_KEY=default
 ./k6 run -e SCENARIO_LABEL=connection-flow-smoke ./dist/connection-flow-test.js
 ./k6 run -e SCENARIO_LABEL=issuance-flow-smoke ./dist/issuance-flow-test.js
 ./k6 run -e SCENARIO_LABEL=present-proof-flow-smoke ./dist/present-proof-flow-test.js
-
