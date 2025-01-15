ca495d28191795b8db2e5a7f452986166400e5a3
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2025-01-15 18:45:46 +0700

    test: stabilize e2e build
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- tests/integration-tests/build.gradle.kts
+++ tests/integration-tests/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     testImplementation("io.ktor:ktor-server-netty:2.3.0")
     testImplementation("io.ktor:ktor-client-apache:2.3.0")
     // RestAPI client
-    testImplementation("org.hyperledger.identus:cloud-agent-client-kotlin:1.40.1-01bb43a")
+    testImplementation("org.hyperledger.identus:cloud-agent-client-kotlin:1.40.1-16c17ce")
     // Test helpers library
     testImplementation("io.iohk.atala:atala-automation:0.4.0")
     // Hoplite for configuration
