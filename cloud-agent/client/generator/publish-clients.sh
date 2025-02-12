b1876e6ff2f8972dc60fd76977393e05ea55e93a
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2025-02-13 03:14:56 +0800

    build: fix yarn publishing to NPMJS
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- cloud-agent/client/generator/publish-clients.sh
+++ cloud-agent/client/generator/publish-clients.sh
@@ -7,20 +7,10 @@ echo version=${AGENT_VERSION}
 # install dependencies
 yarn
 
-# kotlin
-# Determine if the version is a snapshot or a release
-if [[ "$AGENT_VERSION" == *-* ]]; then
-  echo "Publishing snapshot version"
-  # kotlin
-  gradle -p ../kotlin -Pversion=${AGENT_VERSION} publishToSonatype
-else
-  echo "Publishing release version"
-  # kotlin
-  gradle -p ../kotlin -Pversion=${AGENT_VERSION} publishToSonatype closeAndReleaseSonatypeStagingRepository
-fi
+gradle -p ../kotlin -Pversion=${AGENT_VERSION} publishToSonatype closeAndReleaseSonatypeStagingRepository
 
 # typescript
 yarn --cwd ../typescript
-yarn --cwd ../typescript publish --new-version ${AGENT_VERSION} --no-git-tag-version
+yarn --cwd ../typescript publish --new-version ${AGENT_VERSION} --no-git-tag-version --non-interactive
 
 # python
