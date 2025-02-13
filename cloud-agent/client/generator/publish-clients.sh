0639b9d1d97129c9db9156ff330b448d0c61f263
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2025-02-13 17:15:02 +0800

    rollback: migration to npmjs and maven central
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- cloud-agent/client/generator/publish-clients.sh
+++ cloud-agent/client/generator/publish-clients.sh
@@ -7,10 +7,11 @@ echo version=${AGENT_VERSION}
 # install dependencies
 yarn
 
-gradle -p ../kotlin -Pversion=${AGENT_VERSION} publishToSonatype --debug
+# kotlin
+gradle -p ../kotlin -Pversion=${AGENT_VERSION} publish
 
 # typescript
 yarn --cwd ../typescript
-yarn --cwd ../typescript publish --new-version ${AGENT_VERSION} --no-git-tag-version --non-interactive --access public
+yarn --cwd ../typescript publish --new-version ${AGENT_VERSION} --no-git-tag-version
 
 # python
